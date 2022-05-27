6997d5a90025c4f46c369f81b1c27c12baded916
Anthony <anthony@immutableholdings.com>
2022-05-27 15:48:33 -0600

    adding back in segment, react-dom version matching

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -43,6 +43,26 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap" rel="stylesheet" />
           <link href='https://cdn.nft.com/StretchPro.otf' rel='stylesheet' />
           <link href='https://cdn.nft.com/BEBAS___.ttf' rel='stylesheet' />
+
+          <Script id='google-tag-manager' strategy='afterInteractive'>
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PLBC5X8');
+            `}
+          </Script>
+
+          <Script id="segment-js" strategy='afterInteractive'>
+            {`
+              !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="ey8FEiTC7Von8zKLqIe1ju6rGLzeG4A5";;analytics.SNIPPET_VERSION="4.15.3";
+              analytics.load("ey8FEiTC7Von8zKLqIe1ju6rGLzeG4A5");
+              analytics.page();
+              }}();
+            `}
+          </Script>
+
         </Head>
         <body>
           <noscript>You need to enable JavaScript to run this app.</noscript>
