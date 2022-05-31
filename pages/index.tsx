485bc0b71dd04de1481b2d3177b8f474ed13ecd3
Anthony <anthony@immutableholdings.com>
2022-05-31 09:32:28 -0600

    remove layout from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,6 @@
 import { HeroPage } from 'components/templates/HeroPage';
 
-import { ReactElement } from 'react';
-
 export default function Main() {
   return <HeroPage />;
 }
-
-Main.getLayout = function getLayout(page: ReactElement) {
-  return (
-    { page }
-  );
-};
 
