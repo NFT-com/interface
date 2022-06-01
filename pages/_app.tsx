ed69916d948d8738f61a0f41a8471a72f476c3f0
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-06-01 13:34:44 -0500

    fix: import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,7 @@ import {
 } from '@rainbow-me/rainbowkit';
 import { AnimatePresence } from 'framer-motion';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 import { rainbowDark } from 'styles/RainbowKitThemes';
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
@@ -58,20 +59,40 @@ const wagmiClient = createClient({
 
 export default function MyApp({ Component, pageProps, router }: AppProps) {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider
-        appInfo={{
-          appName: 'NFT.com',
-          learnMoreUrl: 'https://docs.nft.com/',
-        }}
-        theme={rainbowDark}
-        chains={chains}>
-        <AnimatePresence exitBeforeEnter>
-          <GraphQLProvider>
-            <Component {...pageProps} key={router.pathname} />
-          </GraphQLProvider>
-        </AnimatePresence>
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <>
+      <Script id='google-tag-manager' strategy='afterInteractive'>
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PLBC5X8');
+        `}
+      </Script>
+
+      <Script id="segment-js" strategy='afterInteractive'>
+        {`
+          !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="ey8FEiTC7Von8zKLqIe1ju6rGLzeG4A5";;analytics.SNIPPET_VERSION="4.15.3";
+          analytics.load("ey8FEiTC7Von8zKLqIe1ju6rGLzeG4A5");
+          analytics.page();
+          }}();
+        `}
+      </Script>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider
+          appInfo={{
+            appName: 'NFT.com',
+            learnMoreUrl: 'https://docs.nft.com/',
+          }}
+          theme={rainbowDark}
+          chains={chains}>
+          <AnimatePresence exitBeforeEnter>
+            <GraphQLProvider>
+              <Component {...pageProps} key={router.pathname} />
+            </GraphQLProvider>
+          </AnimatePresence>
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </>
   );
 }
