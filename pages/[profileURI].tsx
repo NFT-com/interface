0bf1ffdb7f1fe8de22c8647c2baceb3d8cc84f32
just-toby <ejd4@protonmail.com>
2022-06-03 15:34:45 -0700

    fix: show 404 for invalid profile names

--- pages/[profileURI].tsx
+++ pages/[profileURI].tsx
@@ -1,5 +1,7 @@
-import { NullState } from 'components/elements/NullState';
 import { ProfilePage } from 'components/modules/Profile/ProfilePage';
+import { PROFILE_URI_LENGTH_LIMIT } from 'constants/misc';
+
+import NotFoundPage from './404';
 
 import { useRouter } from 'next/router';
 
@@ -9,8 +11,14 @@ import { useRouter } from 'next/router';
 export default function ProfileURI() {
   const router = useRouter();
   const { profileURI } = router.query;
-  if( profileURI === null || profileURI === undefined ) {
-    return <NullState />;
+  const validReg = /^[a-z0-9_]*$/;
+  if(
+    profileURI === null ||
+    profileURI === undefined ||
+    !validReg.test(profileURI as string ?? '-') ||
+    profileURI.length > PROFILE_URI_LENGTH_LIMIT
+  ) {
+    return <NotFoundPage />;
   }
   
   return <ProfilePage uri={profileURI} />;
