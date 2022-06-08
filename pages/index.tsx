e9a6a56ba5896cf3cbe67dea506bdffff5fa3a72
Anthony <anthony@immutableholdings.com>
2022-06-08 10:40:46 -0600

    code cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
-  return <div>Content</div>;
+  return (<main>
+    <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      Content Here
+      <div className="px-4 py-6 sm:px-0">
+        <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+      </div>
+      {/* <!-- /End replace --> */}
+    </div>
+  </main>);
 };
 
 export default HomePage;
