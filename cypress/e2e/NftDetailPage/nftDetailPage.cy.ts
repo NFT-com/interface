bb361e45a9223424dd813cba0802776bf42f4ca5
just-toby <ejd4@protonmail.com>
2022-06-09 11:40:32 -0700

    fix: cypress test runs

--- cypress/e2e/NftDetailPage/nftDetailPage.cy.ts
+++ cypress/e2e/NftDetailPage/nftDetailPage.cy.ts
@@ -2,7 +2,7 @@
 
 describe('nft detail page tests', () => {
   beforeEach(() => {
-    cy.visit('localhost:3000/app/nft/0x530E404f51778F38249413264ac7807A16b88603/330');
+    cy.visit('/app/nft/0x530E404f51778F38249413264ac7807A16b88603/330');
   });
   
   it('displays title and owner', () => {
