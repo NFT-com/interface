8c3e77742bbafe7860273b0f07c6babbfe4ce384
Anthony <anthony@immutableholdings.com>
2022-06-17 14:19:34 -0600

    add more e2e tests, fix logic in id

--- cypress/e2e/NftDetailPage/gkGalleryDetailPage.cy.ts
+++ cypress/e2e/NftDetailPage/gkGalleryDetailPage.cy.ts
@@ -1,35 +1,47 @@
 /// <reference types="cypress" />
 
 describe('gk gallery detail page tests', () => {
+  const generateRandomNumber = (min: number, max: number) => {
+    const _min: number = Math.ceil(min);
+    const _max: number = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+}
+
   it('id > 10000', () => {
-    cy.visit('/app/gallery/10001');
-    cy.contains('Looking for a NFT.com profile?').should('exist').and('be.visible');
-    cy.contains('Return to NFT.com').should('exist').and('be.visible');
-    cy.get('.drop-shadow-md > .flex').should('exist').and('be.visible').click();
-    cy.url().should('not.include', 'app/gallery/10000');
+    cy.visit('/app/gallery/100001');
+    cy.get('.text-primary-txt').should('exist').and('include.text', 'This Genesis Key');
+    cy.contains('Back to Gallery').should('exist');
+    cy.get('.drop-shadow-md > .flex').should('exist').click();
+    cy.url().should('not.include', '100001');
   });
 
   it('id === 0', () => {
     cy.visit('/app/gallery/0');
-    cy.contains('Looking for a NFT.com profile?').should('exist').and('be.visible');
-    cy.contains('Return to NFT.com').should('exist').and('be.visible');
-    cy.get('.drop-shadow-md > .flex').should('exist').and('be.visible').click();
-    cy.url().should('not.include', 'app/gallery/0');
+    cy.get('.text-primary-txt').should('exist').and('include.text', 'This Genesis Key');
+    cy.contains('Back to Gallery').should('exist');
+    cy.get('.drop-shadow-md > .flex').should('exist').click();
+    cy.url().should('not.include', '/0');
   });
 
   it('id < 0', () => {
     cy.visit('/app/gallery/-1');
-    cy.contains('Looking for a NFT.com profile?').should('exist').and('be.visible');
-    cy.contains('Return to NFT.com').should('exist').and('be.visible');
-    cy.get('.drop-shadow-md > .flex').should('exist').and('be.visible').click();
+    cy.get('.text-primary-txt').should('exist').and('include.text', 'This Genesis Key');
+    cy.contains('Back to Gallery').should('exist');
+    cy.get('.drop-shadow-md > .flex').should('exist').click();
     cy.url().should('not.include', '-1');
   });
 
   it('id NaN', () => {
     cy.visit('/app/gallery/notanumber');
-    cy.contains('Looking for a NFT.com profile?').should('exist').and('be.visible');
-    cy.contains('Return to NFT.com').should('exist').and('be.visible');
-    cy.get('.drop-shadow-md > .flex').should('exist').and('be.visible').click();
+    cy.get('.text-primary-txt').should('exist').and('include.text', 'This Genesis Key');
+    cy.contains('Back to Gallery').should('exist');
+    cy.get('.drop-shadow-md > .flex').should('exist').click();
     cy.url().should('not.include', 'notanumber');
+  })
+
+  it('visit valid gk', () => {
+    const id = generateRandomNumber(1, 10000);
+    cy.visit(`/app/gallery/${id}`);
+    cy.get('.text-5xl').should('exist').and('include.text', `${id}`);
   })
 });
--- pages/app/gallery/[id].tsx
+++ pages/app/gallery/[id].tsx
@@ -1,9 +1,8 @@
 import { Footer } from 'components/elements/Footer';
 import { PageWrapper } from 'components/layouts/PageWrapper';
 import { GenesisKeyDetailContent } from 'components/modules/Gallery/GenesisKeyDetailContent';
-import NotFoundPage from 'pages/404';
 
-import { BigNumber } from 'ethers';
+import { NullState } from 'instrumented/components/elements/NullState';
 import { useRouter } from 'next/router';
 
 /**
@@ -13,8 +12,8 @@ export default function GalleryDetailPage() {
   const router = useRouter();
   const { id } = router.query;
 
-  if (id === null || id === undefined || !BigNumber.isBigNumber(id) || !BigNumber.from(id).isZero || BigNumber.from(id).isNegative || BigNumber.from(id).gt(10000)) {
-    return <NotFoundPage />;
+  if (id === null || id === undefined) {
+    return <NullState />;
   }
   
   return (
