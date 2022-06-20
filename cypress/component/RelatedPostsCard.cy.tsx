6d51f2d23816ee37f12c2ef9172eb3a7dcd7d466
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-06-20 14:25:42 -0700

    added component test, mock data, support for tailwind

---
+++ cypress/component/RelatedPostsCard.cy.tsx
@@ -0,0 +1,27 @@
+import RelatedPostCard from '../../components/modules/BlogPage/RelatedPostsCard'
+import * as testPost from '../fixtures/blog_post.json'
+import '../plugins/tailwind'
+
+describe('RelatedPostCard', () => {
+  it('mounts with valid post', () => {
+    cy.mount(
+      <RelatedPostCard post={testPost} />
+    );
+    cy.get('h3').should("have.text", "test blog post")
+    cy.get('h3').should('have.css', 'font-family').and('match', /Rubik, sans-serif/)
+
+    cy.get('p').should('have.css', 'font-weight').and('match', /400/)
+
+    cy.get('.mt-1').should('have.text', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lobortis diam m...')
+    cy.get('.mt-1').invoke('text').then((text) => {
+      expect(text.length).to.be.below(79)
+    })
+
+    cy.get('p').contains("John Doe")
+
+    cy.get('p.text-sm').should('have.text', 'Sep 1st, 2003')
+
+    cy.get('img').should('have.attr', 'src')
+    cy.get('img').should('have.attr', 'alt')
+  });
+});
