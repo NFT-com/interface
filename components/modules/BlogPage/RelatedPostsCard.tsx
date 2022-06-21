48d7bc084b936560ac4755516191ce96e4b40ff1
lucas <50972198+robitaille1@users.noreply.github.com>
2022-06-21 08:11:06 -0700

    Feat/blog template page (#63)
    * Updated styling to include header, updated share component to use current href, added checks in related cards, updated tooltip for mouse leave delay

--- components/modules/BlogPage/RelatedPostsCard.tsx
+++ components/modules/BlogPage/RelatedPostsCard.tsx
@@ -11,30 +11,30 @@ type RelatedPostProps = {
 export default function RelatedPostCard({ post }: RelatedPostProps) {
   const result = readingTime(post?.body);
   return (
-    <Link href={post.slug}>
+    <Link href={post?.slug}>
       <div className="text-left	hover:cursor-pointer">
         <div className="aspect-4/3 w-full relative">
           <Image
-            src={post.heroImage.url}
-            alt={post.heroImage.description}
+            src={post?.heroImage.url}
+            alt={post?.heroImage.description}
             layout="fill"
             objectFit="cover"
             className="rounded-md"
           />
         </div>
         <h3 className="dark:text-white text-xl lg:text-lg md:text-base sm:text-sm font-medium mt-2">
-          {post.title}
+          {post?.title}
         </h3>
         <p className="text-base md:text-xs mt-1 leading-5.5 md:leading-4 text-blog-text dark:text-gray-400">
-          {post.description.length > 75
+          {post?.description.length > 75
             ? post.description.substring(0, 75) + '...'
             : post.description}
         </p>
         <div className="flex mt-3">
           <div className="h-9 md:h-7 w-9 md:w-7 mr-2 mt-0.5">
             <Image
-              src={post.author.image.url}
-              alt={post.author.image.description}
+              src={post?.author.image.url}
+              alt={post?.author.image.description}
               className="rounded-full"
               width="100%"
               height="100%"
@@ -42,7 +42,7 @@ export default function RelatedPostCard({ post }: RelatedPostProps) {
           </div>
           <div>
             <p className="dark:text-white text-base md:text-xs">
-              {post.author.name}
+              {post?.author.name}
             </p>
             <div className="flex text-blog-text dark:text-gray-400">
               <p className="text-sm md:text-xxs3">
