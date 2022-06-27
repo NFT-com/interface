9277c814198fd277ae262429bcb65d1094850213
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-06-27 14:17:03 -0700

    Created api endpoint for fetching contentful paginated posts

---
+++ utils/contentful.ts
@@ -0,0 +1,16 @@
+import { Doppler,getEnv } from './env';
+
+export async function getPaginatedPosts(
+  skip: number,
+  pageSize: number,
+  preview: boolean
+) {
+  const url = new URL(getEnv(Doppler.NEXT_PUBLIC_BASE_URL) + 'api/contentful');
+  url.searchParams.set('skip', String(skip));
+  url.searchParams.set('pageSize', String(pageSize));
+  url.searchParams.set('preview', String(preview));
+
+  const result = await fetch(url.toString()).then(res => res.json());
+  return result;
+}
+
