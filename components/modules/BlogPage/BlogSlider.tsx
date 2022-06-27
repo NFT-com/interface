66d1d1fdea4a9bd24c8f15ee76e3d47805c65a76
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-06-27 08:54:36 -0700

    Added carousel components and cards

---
+++ components/modules/BlogPage/BlogSlider.tsx
@@ -0,0 +1,68 @@
+import BlogSliderCard from './BlogSliderCard';
+
+import Autoplay from 'embla-carousel-autoplay';
+import useEmblaCarousel from 'embla-carousel-react';
+import { useCallback,useEffect, useState } from 'react';
+import { PostData } from 'types/blogs';
+
+type BlogSliderProps = {
+  posts: PostData[];
+};
+
+export default function BlogSlider({ posts }:BlogSliderProps) {
+  const options = { delay: 5000 };
+  const autoplayRoot = (emblaRoot) => emblaRoot.parentElement;
+  const autoplay = Autoplay(options, autoplayRoot);
+  const [emblaRef, embla] = useEmblaCarousel({
+    align: 'start',
+    loop: true,
+    skipSnaps: false,
+    inViewThreshold: 0.7,
+  }, [autoplay]);
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState([]);
+
+  const scrollTo = useCallback(
+    (index) => embla && embla.scrollTo(index),
+    [embla]
+  );
+
+  const onSelect = useCallback(() => {
+    if (!embla) return;
+    setSelectedIndex(embla.selectedScrollSnap());
+  }, [embla, setSelectedIndex]);
+
+  useEffect(() => {
+    if (!embla) return;
+    onSelect();
+    setScrollSnaps(embla.scrollSnapList());
+    embla.on('select', onSelect);
+  }, [embla, setScrollSnaps, onSelect]);
+
+  return (
+    <div className="mx-auto px-5 bg-[#DCF2FF] p-3 rounded-2xl">
+      <div className="overflow-hidden" ref={emblaRef}>
+        <div className="flex">
+          {posts?.map((post) => (
+            <BlogSliderCard key={post?.sys.id} post={post} />
+          ))}
+        </div>
+      </div>
+      <div className="flex items-center justify-center mt-5 space-x-2">
+        {scrollSnaps.map((_, idx) => (
+          <div className={`w-3 h-3 bg-[#DCF2FF] border rounded-full flex justify-center items-center ${
+            idx === selectedIndex ? 'border-[#0077BA]' : 'none'
+          }`} key={idx} >
+            <button
+              className={`w-2 h-2 rounded-full ${
+                idx === selectedIndex ? 'bg-[#0077BA]' : 'bg-[#B7C6CE]'
+              }`}
+              onClick={() => scrollTo(idx)}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ components/modules/BlogPage/BlogSliderCard.tsx
@@ -0,0 +1,96 @@
+import { tw } from 'utils/tw';
+
+import moment from 'moment';
+import Image from 'next/image';
+import Link from 'next/link';
+import { readingTime } from 'reading-time-estimator';
+import { PostData } from 'types/blogs';
+
+type BlogSliderPostProps = {
+  post: PostData;
+};
+
+export default function BlogSliderPost({ post }: BlogSliderPostProps) {
+  return (
+    <div
+      className="relative flex flex-row md:flex-col flex-none flex-nowrap w-full mx-10 p-3 bg-white rounded-2xl"
+      key={post.title}
+    >
+      <div className="relative overflow-hidden cursor-pointer w-2/5 md:w-full aspect-square md:aspect-video sm:aspect-square object-cover object-center">
+        <Link href={`/articles/${post.slug}`}>
+          <a>
+            <Image
+              src={post?.heroImage.url}
+              className="rounded-lg"
+              alt="cover image"
+              placeholder="blur"
+              layout='fill'
+              objectFit="cover"
+              blurDataURL={post?.heroImage.url}
+            />
+          </a>
+        </Link>
+      </div>
+      <div className="flex flex-col w-3/5 md:w-full ml-8 md:ml-0 pt-12 lg:pt-2 md:pt-4 px-1">
+        <Link href={`/articles/${post.slug}`}>
+          <a className="cursor-pointer">
+            <h2 className="font-bold text-black text-4xl lg:text-3xl md:text-lg md:w-4/5 sm:w-full">
+              {post.title}
+            </h2>
+          </a>
+        </Link>
+        <Link href={`/articles/${post.slug}`}>
+          <a className="cursor-pointer">
+            <p className="text-blog-text text-justify text-xl lg:text-lg md:text-sm w-11/12 md:w-10/12 sm:w-full leading-8 mt-3 md:mt-2">
+              {post?.description.length > 230
+                ? post.description.substring(0, 230) + '...'
+                : post.description}
+                        Webhooks notify you, another person or system when resources have changed by calling a given HTTP endpoint.
+            </p>
+          </a>
+        </Link>
+        <div className="flex mt-4 md:mt-2">
+          <div className="h-12 md:h-7 w-12 md:w-7 mr-2 mt-0.5">
+            <Image
+              src={post?.author.image.url}
+              alt={post?.author.image.description}
+              className="rounded-full"
+              width="100%"
+              height="100%"
+            />
+          </div>
+          <div className='pt-0.5'>
+            <p className="text-xl lg:text-lg md:text-sm">
+              {post?.author.name}
+            </p>
+            <div className="flex text-blog-text dark:text-gray-400">
+              <p className="text-sm md:text-xs">
+                {moment(post?.publishDate).format('MMM Do, YYYY')}
+              </p>
+              <span className="mx-1 block md:hidden text-sm md:text-xxs3 ">
+                .
+              </span>
+              {post?.body && (
+                <p className="block md:hidden text-sm md:text-xxs3">
+                  {readingTime(post?.body).minutes} min read
+                </p>
+              )}
+            </div>
+          </div>
+                  
+        </div>
+        <Link href={`/articles/${post.slug}`}>
+          <button
+            className={tw(
+              'font-medium bg-primary-button-bckg rounded-xl text-white block',
+              'flex flex-row items-center cursor-pointer hover:opacity-80 font-rubik w-max mt-8 lg:mt-6 md:mt-3 mb-9 md:mb-6 sm:mb-4',
+              'py-2 px-5'
+            )}
+            type="button">
+                View Post
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
