d8e1ee706b9e294dbb01bae16374c7a3d561fcb4
lucas <50972198+robitaille1@users.noreply.github.com>
2022-06-29 09:11:30 -0700

    Added container, set max width for content (#91)
    * Added container, set max width for content
    
    * Added container and max width to blog template page

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -57,30 +57,32 @@ export default function Post({ post, preview }: PostProps) {
         <Header />
         <Sidebar />
       </ClientOnly>
-      <div className="bg-white dark:bg-modal-overlay-dk  relative text-center px-4 w-full pt-28">
-        <BlogHeader post={post} />
+      <div className='bg-white dark:bg-modal-overlay-dk'>
+        <div className="relative text-center px-4 w-full pt-28  max-w-7xl mx-auto">
+          <BlogHeader post={post} />
 
-        <BlogHeroImage
-          src={post?.heroImage.url}
-          alt={post?.heroImage.description}
-        />
-        <div className="text-left mt-12 md:mt-8 mx-auto pb-5 w-8/12 lg:w-3/4 md:w-full">
-          <Markdown content={post?.body} />
-        </div>
-        {post?.relatedPostsCollection?.items.length
-          ? (
-            <div className='border-t-2 border-share-icon'>
-              <h2 className="dark:text-white text-left mb-6 sm:mb-3 font-medium mt-8 md:mt-4 sm:mt-1.5 text-3xll md:text-xl sm:text-sm">
+          <BlogHeroImage
+            src={post?.heroImage.url}
+            alt={post?.heroImage.description}
+          />
+          <div className="text-left mt-12 md:mt-8 mx-auto pb-5 w-8/12 lg:w-3/4 md:w-full">
+            <Markdown content={post?.body} />
+          </div>
+          {post?.relatedPostsCollection?.items.length
+            ? (
+              <div className='border-t-2 border-share-icon'>
+                <h2 className="dark:text-white text-left mb-6 sm:mb-3 font-medium mt-8 md:mt-4 sm:mt-1.5 text-3xll md:text-xl sm:text-sm">
                 Related Posts
-              </h2>
-              <div className="grid gap-x-4 sm:gap-x-3 gap-y-7 grid-cols-3 md:grid-cols-2 pb-24 lg:pb-12">
-                {post.relatedPostsCollection.items.map((post) => (
-                  post && <RelatedPostCard key={post?.sys.id} post={post} />
-                ))}
+                </h2>
+                <div className="grid gap-x-4 sm:gap-x-3 gap-y-7 grid-cols-3 md:grid-cols-2 pb-24 lg:pb-12">
+                  {post.relatedPostsCollection.items.map((post) => (
+                    post && <RelatedPostCard key={post?.sys.id} post={post} />
+                  ))}
+                </div>
               </div>
-            </div>
-          )
-          : null}
+            )
+            : null}
+        </div>
       </div>
       <Footer />
       {preview && <PreviewBanner />}
