c05528ec5dbc3eed2db9766f82a3f257ad97009b
just-toby <ejd4@protonmail.com>
2022-06-30 17:00:41 -0400

    feat: update nft allowance hook

--- hooks/balances/useNftCollectionAllowance.ts
+++ hooks/balances/useNftCollectionAllowance.ts
@@ -1,13 +1,18 @@
-import { AssetClass } from 'graphql/generated/types';
 import { use721Contract } from 'hooks/contracts/use721Contract';
-import { useTransferProxy } from 'hooks/contracts/useTransferProxy';
 
 import { useCallback } from 'react';
 import useSWR from 'swr';
 import { useAccount, useNetwork, useProvider, useSigner } from 'wagmi';
+
+export enum TransferProxyTarget {
+  LooksRare = '0xf42aa99F011A1fA7CDA90E5E98b277E306BcA83e',
+  Opensea = '0x1e0049783f008a0085193e00003d00cd54003c71',
+}
+
 export function useNftCollectionAllowance(
   collectionAddress: string,
   account: string,
+  target: TransferProxyTarget
 ): {
     allowedAll: boolean,
     mutate: () => void ,
@@ -18,16 +23,12 @@ export function useNftCollectionAllowance(
   const { activeChain } = useNetwork();
   const provider = useProvider({ chainId: activeChain?.id });
   const collection = use721Contract(collectionAddress, provider);
-  const nftTransferProxy = useTransferProxy(AssetClass.Erc721);
 
-  const keyString = `${activeChain?.id}_${collectionAddress}_allowance_${account}_${nftTransferProxy}`;
+  const keyString = `${activeChain?.id}_${collectionAddress}_allowance_${account}_${target}`;
 
   const { data, mutate } = useSWR(keyString, async () => {
     try {
-      if (nftTransferProxy == null) {
-        return false;
-      }
-      const allowed = await collection.isApprovedForAll(account, nftTransferProxy);
+      const allowed = await collection.isApprovedForAll(account, target);
       return allowed ?? false;
     } catch (e) {
       console.log(
@@ -39,15 +40,15 @@ export function useNftCollectionAllowance(
   });
 
   const requestAllowance = useCallback(async () => {
-    if (acctData?.connector == null || nftTransferProxy == null || signer == null) {
+    if (acctData?.connector == null || target == null || signer == null) {
       console.log('Failed to set approvals, please connect wallet');
       return;
     }
     const tx = await collection
       .connect(signer)
-      .setApprovalForAll(nftTransferProxy, true);
+      .setApprovalForAll(target, true);
     await tx.wait(1);
-  }, [acctData, collection, nftTransferProxy, signer]);
+  }, [acctData, collection, target, signer]);
 
   return {
     allowedAll: data ?? false,
