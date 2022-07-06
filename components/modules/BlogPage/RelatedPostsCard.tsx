9f92085f6f642c616e0c29fea0f4cdb86cca6550
lucas <50972198+robitaille1@users.noreply.github.com>
2022-07-06 14:03:46 -0700

    Fix/7 6 blog fixes (#119)
    * Added check for null author image and post description
    
    * added 'back to blog' link and icon

--- components/modules/BlogPage/RelatedPostsCard.tsx
+++ components/modules/BlogPage/RelatedPostsCard.tsx
@@ -27,21 +27,24 @@ export default function RelatedPostCard({ post }: RelatedPostProps) {
         <h3 className="dark:text-white text-xl lg:text-lg md:text-base sm:text-sm font-medium mt-2">
           {post?.title}
         </h3>
-        <p className="text-base md:text-xs mt-1 leading-5.5 md:leading-4 text-blog-text dark:text-gray-400">
-          {post?.description.length > 75
-            ? post.description.substring(0, 75) + '...'
-            : post.description}
-        </p>
+        {post?.description && (
+          <p className="text-base md:text-xs mt-1 leading-5.5 md:leading-4 text-blog-text dark:text-gray-400">
+            {post.description.length > 75
+              ? post.description.substring(0, 75) + '...'
+              : post.description}
+          </p>
+        )}
         <div className="flex mt-3">
-          <div className="h-9 md:h-7 w-9 md:w-7 mr-2 mt-0.5">
-            <Image
-              src={post?.author.image.url}
-              alt={post?.author.image.description}
-              className="rounded-full"
-              width="100%"
-              height="100%"
-            />
-          </div>
+          {post?.author?.image?.url && (
+            <div className="h-9 md:h-7 w-9 md:w-7 mr-2 mt-0.5">
+              <Image
+                src={post.author.image.url}
+                alt={post?.author?.image?.description || 'Author Image'}
+                className="rounded-full"
+                width="100%"
+                height="100%"
+              />
+            </div>)}
           <div>
             <p className="dark:text-white text-base md:text-xs">
               {post?.author.name}
