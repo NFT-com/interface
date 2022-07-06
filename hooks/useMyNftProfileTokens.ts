687d9aa536efbcf5a57cf203ef526bdabe63f50d
just-toby <ejd4@protonmail.com>
2022-07-06 12:29:35 -0700

    fix: remove hardcoded address for testing

--- hooks/useMyNftProfileTokens.ts
+++ hooks/useMyNftProfileTokens.ts
@@ -5,9 +5,7 @@ import { useAccount } from 'wagmi';
 export function useMyNftProfileTokens(): ProfileTokenResults {
   const { data: account } = useAccount();
   
-  // TODO: UNDO THIS CHANGE BEFORE LANDING
-  // const result = useNftProfileTokens(account?.address);
-  const result = useNftProfileTokens('0xf14C166CEFEA0e53Af3B70BB38850ECf4B5A80A5');
+  const result = useNftProfileTokens(account?.address);
 
   return result;
 }
