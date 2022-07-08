ad9d56273caf98f47de8bf5da94d889425761a68
just-toby <ejd4@protonmail.com>
2022-07-08 16:37:24 -0700

    feat: looksrare listings working

---
+++ hooks/contracts/useLooksrareStrategyContract.ts
@@ -0,0 +1,13 @@
+import { IExecutionStrategy } from 'constants/typechain/looksrare';
+import { IExecutionStrategy__factory } from 'constants/typechain/looksrare/factories/IExecutionStrategy__factory';
+
+import { Provider } from '@ethersproject/providers';
+import { Addresses, addressesByNetwork } from '@looksrare/sdk';
+import { useNetwork } from 'wagmi';
+
+export function useLooksrareStrategyContract(provider: Provider): IExecutionStrategy {
+  const { activeChain } = useNetwork();
+  const addresses: Addresses = addressesByNetwork[activeChain?.id];
+  // todo: generalize this hook to different strategies.
+  return IExecutionStrategy__factory.connect(addresses.STRATEGY_STANDARD_SALE, provider);
+}
--- pages/api/looksrare.ts
+++ pages/api/looksrare.ts
@@ -8,16 +8,35 @@ const looksrareHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   const action = req.query['action'];
 
   switch(action) {
-  case 'listNFT':
+  case 'getNonce':
     try {
-      const signature = req.query['signature'];
-      const parameters = JSON.parse(req.query['parameters'] as string);
-      if (isNullOrEmpty(signature)) {
-        res.status(400).json({ message: 'listNFT: Invalid Signature' });
+      const address = req.query['address'];
+      if (isNullOrEmpty(address)) {
+        res.status(400).json({ message: 'listNFT: missing required param "order' });
         return;
       }
-      if (isNullOrEmpty(parameters)) {
-        res.status(400).json({ message: 'listNFT: Invalid Parameters' });
+      const options = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'X-Looks-Api-Key': process.env.LOOKSRARE_API_KEY,
+        }
+      };
+      const result = await fetch(
+        'https://api.looksrare.org/api/v1/orders/nonce?address=' + address,
+        options
+      ).then(res => res.json());
+      res.status(200).json(result);
+    } catch (e) {
+      res.status(500).json({ message: 'Failed to get nonce', error: e });
+    }
+    break;
+  case 'listNFT':
+    try {
+      const order = JSON.parse(req.query['order'] as string);
+      if (isNullOrEmpty(order)) {
+        res.status(400).json({ message: 'listNFT: missing required param "order' });
         return;
       }
       const options = {
@@ -25,12 +44,9 @@ const looksrareHandler = async (req: NextApiRequest, res: NextApiResponse) => {
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json',
-          'X-Looks-Api-Key': process.env.OPENSEA_API_KEY
+          'X-Looks-Api-Key': process.env.LOOKSRARE_API_KEY,
         },
-        body: JSON.stringify({
-          signature,
-          parameters
-        })
+        body: JSON.stringify(order)
       };
       const result = await fetch(
         'https://api.looksrare.org/api/v1/orders',
@@ -38,7 +54,7 @@ const looksrareHandler = async (req: NextApiRequest, res: NextApiResponse) => {
       ).then(res => res.json());
       res.status(200).json( result );
     } catch (e) {
-      res.status(500).json({ message: 'Failed to list NFT on Seaport', error: e });
+      res.status(500).json({ message: 'Failed to list NFT on Looksrare', error: e });
     }
     break;
   default:
