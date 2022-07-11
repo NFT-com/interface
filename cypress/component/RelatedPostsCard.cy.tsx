50bc0b2c575c4bb185725e66e2cfd54e9a2b4a11
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-07-11 15:24:47 -0700

    Updated component test font

--- cypress/component/RelatedPostsCard.cy.tsx
+++ cypress/component/RelatedPostsCard.cy.tsx
@@ -1,27 +1,28 @@
-import RelatedPostCard from '../../components/modules/BlogPage/RelatedPostsCard'
-import * as testPost from '../fixtures/blog_post.json'
-import '../plugins/tailwind'
+import '../plugins/tailwind';
+
+import RelatedPostCard from '../../components/modules/BlogPage/RelatedPostsCard';
+import * as testPost from '../fixtures/blog_post.json';
 
 describe('RelatedPostCard', () => {
   it('mounts with valid post and styling', () => {
     cy.mount(
       <RelatedPostCard post={testPost} />
     );
-    cy.get('h3').should("have.text", "test blog post")
-    cy.get('h3').should('have.css', 'font-family').and('match', /Rubik, sans-serif/)
+    cy.get('h3').should('have.text', 'test blog post');
+    cy.get('h3').should('have.css', 'font-family').and('match', /Grotesk/);
 
-    cy.get('p').should('have.css', 'font-weight').and('match', /400/)
+    cy.get('p').should('have.css', 'font-weight').and('match', /400/);
 
-    cy.get('.mt-1').should('have.text', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lobortis diam m...')
+    cy.get('.mt-1').should('have.text', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lobortis diam m...');
     cy.get('.mt-1').invoke('text').then((text) => {
-      expect(text.length).to.be.below(79)
-    })
+      expect(text.length).to.be.below(79);
+    });
 
-    cy.get('p').contains("John Doe")
+    cy.get('p').contains('John Doe');
 
-    cy.get('p.text-sm').should('contain.text', 'Sep 1st, 2003')
+    cy.get('p.text-sm').should('contain.text', 'Sep 1st, 2003');
 
-    cy.get('img').should('have.attr', 'src')
-    cy.get('img').should('have.attr', 'alt')
+    cy.get('img').should('have.attr', 'src');
+    cy.get('img').should('have.attr', 'alt');
   });
-}); 
+});
