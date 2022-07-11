1b001cdd09c5b91cf7370c700034cd6ab2f45412
just-toby <ejd4@protonmail.com>
2022-07-11 13:51:26 -0700

    feat: add more tests, wip

--- utils/seaportHelpers.ts
+++ utils/seaportHelpers.ts
@@ -1,15 +1,22 @@
+import { NULL_ADDRESS } from 'constants/addresses';
+import { Nft } from 'graphql/generated/types';
+
 import { BigNumber, BigNumberish, ethers } from 'ethers';
 import { _TypedDataEncoder } from 'ethers/lib/utils';
+import { PartialDeep } from 'type-fest';
 import {
   CROSS_CHAIN_SEAPORT_ADDRESS,
   EIP_712_ORDER_TYPE,
   Fee,
   ItemType,
   ONE_HUNDRED_PERCENT_BP,
-  SEAPORT_CONTRACT_NAME,
+  OPENSEA_CONDUIT_KEY, OrderType,SEAPORT_CONTRACT_NAME,
   SEAPORT_CONTRACT_VERSION,
+  SEAPORT_FEE_COLLLECTION_ADDRESS, SEAPORT_ZONE,
+  SEAPORT_ZONE_HASH,
   SeaportConsiderationItem,
-  SeaportOrderComponents
+  SeaportOrderComponents,
+  SeaportOrderParameters
 } from 'types/seaport';
 
 export function getTypedDataDomain(chainId: string | number) {
@@ -85,3 +92,51 @@ export const feeToConsiderationItem = ({
     recipient: fee.recipient,
   };
 };
+
+export function createSeaportParametersForNFTListing(
+  offerer: string,
+  nft: PartialDeep<Nft>,
+  price: BigNumberish,
+  currency: string
+): SeaportOrderParameters {
+  // This is what the seller will accept for their NFT.
+  // For now, we support a single currency.
+  const considerationItems = [{
+    itemType: ItemType.NATIVE,
+    token: currency,
+    identifierOrCriteria: BigNumber.from(0).toString(),
+    startAmount: BigNumber.from(price).toString(),
+    endAmount: BigNumber.from(price).toString(),
+    recipient: offerer,
+  }];
+  const openseaFee: Fee = {
+    recipient: SEAPORT_FEE_COLLLECTION_ADDRESS,
+    basisPoints: 250,
+  };
+  return {
+    offerer: offerer ?? NULL_ADDRESS,
+    zone: SEAPORT_ZONE,
+    offer: [{
+      itemType: ItemType.ERC721,
+      token: nft?.contract,
+      identifierOrCriteria: BigNumber.from(nft?.tokenId).toString(),
+      startAmount: BigNumber.from(1).toString(),
+      endAmount: BigNumber.from(1).toString(),
+    }],
+    consideration: [
+      ...deductFees(considerationItems, [openseaFee]),
+      feeToConsiderationItem({
+        fee: openseaFee,
+        token: currency,
+        baseAmount: price,
+      })
+    ],
+    orderType: OrderType.FULL_RESTRICTED,
+    startTime: BigNumber.from(Date.now()).div(1000).toString(),
+    endTime: BigNumber.from(Date.now()).div(1000).add(604800 /* 1 week in seconds */).toString(),
+    zoneHash: SEAPORT_ZONE_HASH,
+    totalOriginalConsiderationItems: '2',
+    salt: generateRandomSalt(),
+    conduitKey: OPENSEA_CONDUIT_KEY,
+  };
+}
