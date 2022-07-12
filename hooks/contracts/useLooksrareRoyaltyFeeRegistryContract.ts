6dff6c8b696ac74bce9ea16ca1e205dd2f1778fa
just-toby <ejd4@protonmail.com>
2022-07-11 19:10:24 -0700

    fix: one more error. thank you cypress

--- hooks/contracts/useLooksrareRoyaltyFeeRegistryContract.ts
+++ hooks/contracts/useLooksrareRoyaltyFeeRegistryContract.ts
@@ -1,12 +1,18 @@
 import { RoyaltyFeeRegistry } from 'constants/typechain/looksrare';
 import { RoyaltyFeeRegistry__factory } from 'constants/typechain/looksrare/factories/RoyaltyFeeRegistry__factory';
+import { Maybe } from 'graphql/generated/types';
+import { isNullOrEmpty } from 'utils/helpers';
 
 import { Provider } from '@ethersproject/providers';
 import { Addresses, addressesByNetwork } from '@looksrare/sdk';
 import { useNetwork } from 'wagmi';
 
-export function useLooksrareRoyaltyFeeRegistryContractContract(provider: Provider): RoyaltyFeeRegistry {
+export function useLooksrareRoyaltyFeeRegistryContractContract(provider: Provider): Maybe<RoyaltyFeeRegistry> {
   const { activeChain } = useNetwork();
   const addresses: Addresses = addressesByNetwork[activeChain?.id];
-  return RoyaltyFeeRegistry__factory.connect(addresses?.ROYALTY_FEE_REGISTRY, provider);
+  const address = addresses?.ROYALTY_FEE_REGISTRY;
+  if (isNullOrEmpty(address)) {
+    return null;
+  }
+  return RoyaltyFeeRegistry__factory.connect(address, provider);
 }
