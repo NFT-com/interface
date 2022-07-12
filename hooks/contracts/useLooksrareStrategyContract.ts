fe7cb1799f6b83aceae1a0cb7aa006a2812b94fc
just-toby <ejd4@protonmail.com>
2022-07-11 19:10:24 -0700

    fix: one more error. thank you cypress

--- hooks/contracts/useLooksrareStrategyContract.ts
+++ hooks/contracts/useLooksrareStrategyContract.ts
@@ -1,5 +1,6 @@
 import { IExecutionStrategy } from 'constants/typechain/looksrare';
 import { IExecutionStrategy__factory } from 'constants/typechain/looksrare/factories/IExecutionStrategy__factory';
+import { isNullOrEmpty } from 'utils/helpers';
 
 import { Provider } from '@ethersproject/providers';
 import { Addresses, addressesByNetwork } from '@looksrare/sdk';
@@ -8,6 +9,10 @@ import { useNetwork } from 'wagmi';
 export function useLooksrareStrategyContract(provider: Provider): IExecutionStrategy {
   const { activeChain } = useNetwork();
   const addresses: Addresses = addressesByNetwork[activeChain?.id];
+  const address = addresses?.STRATEGY_STANDARD_SALE;
+  if (isNullOrEmpty(address)) {
+    return null;
+  }
   // todo: generalize this hook to different strategies.
-  return IExecutionStrategy__factory.connect(addresses?.STRATEGY_STANDARD_SALE, provider);
+  return IExecutionStrategy__factory.connect(address, provider);
 }
