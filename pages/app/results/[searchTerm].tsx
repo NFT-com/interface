205993eb62426bfc58f9337cf05ebc887a7122eb
fernandoGalarcio <fernando.galarcio.m@gmail.com>
2022-07-12 00:56:12 -0500

    filters and sort by

--- pages/app/results/[searchTerm].tsx
+++ pages/app/results/[searchTerm].tsx
@@ -158,7 +158,7 @@ export default function ResultsPage() {
                         stats(nbHits) {
                           return (
                             <>
-                              <span>{nbHits === 0 ? '' : `${nbHits.toLocaleString()} RESULTS${nbHits > 1? 'S' : ''} FOR `}</span><br/>
+                              <span>{nbHits === 0 ? '' : `${nbHits.toLocaleString()} RESULT${nbHits > 1? 'S' : ''} FOR `}</span><br/>
                               <span className="text-gray-400 dark:text-always-white font-medium text-2xl">{searchTerm}</span>
                             </>);
                         },
