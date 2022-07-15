769f4d61e42046d053eb233ca6887ca1774c216e
just-toby <ejd4@protonmail.com>
2022-07-15 13:08:35 -0700

    fix: react types versions

--- cypress/component/RoundedCornerMedia.cy.tsx
+++ cypress/component/RoundedCornerMedia.cy.tsx
@@ -1,31 +1,30 @@
-import { RoundedCornerMedia, RoundedCornerVariant } from '../../components/elements/RoundedCornerMedia'
+import { RoundedCornerMedia, RoundedCornerVariant } from '../../components/elements/RoundedCornerMedia';
 
 describe('RoundedCornerMedia', () => {
   it('mounts with valid props', () => {
     cy.mount(
-      <RoundedCornerMedia 
-        src="test.png" 
-        variant={RoundedCornerVariant.All} 
+      <RoundedCornerMedia
+        src="test.png"
+        variant={RoundedCornerVariant.All}
       />
     );
-    cy.get('video').should('have.attr', 'src')
-    cy.get('video').should('have.class', 'rounded-3xl')
+    cy.get('video').should('have.attr', 'src');
+    cy.get('video').should('have.class', 'rounded-3xl');
   });
-
 
   it('mounts with all props, top right variant', () => {
     cy.mount(
-      <RoundedCornerMedia 
-        src="test.png" 
+      <RoundedCornerMedia
+        src="test.png"
         variant={RoundedCornerVariant.TopRight}
-        extraClasses="test" 
+        extraClasses="test"
       />
     );
-    cy.get('video').should('have.attr', 'src')
-    cy.get('video').should('have.attr', 'poster')
-    cy.get('video').should('have.attr', 'loop')
-    cy.get('video').should('have.attr', 'autoplay')
-    cy.get('video').should('have.class', 'test')
-    cy.get('video').should('have.class', 'rounded-tr-3xl')
+    cy.get('video').should('have.attr', 'src');
+    cy.get('video').should('have.attr', 'poster');
+    cy.get('video').should('have.attr', 'loop');
+    cy.get('video').should('have.attr', 'autoplay');
+    cy.get('video').should('have.class', 'test');
+    cy.get('video').should('have.class', 'rounded-tr-3xl');
   });
 });
