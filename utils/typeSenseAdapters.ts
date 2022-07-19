8914d46a7db507ef4240502d0ed762d8610161c1
fernandoGalarcio <fernando.galarcio.m@gmail.com>
2022-07-19 10:02:36 -0500

    add collection page place holder view, hide api key typesense request, add carousel for collection in neft result

--- utils/typeSenseAdapters.ts
+++ utils/typeSenseAdapters.ts
@@ -48,13 +48,3 @@ export const getTypesenseInstantsearchAdapter = (QUERY_BY: SearchableFields) => 
   });
   return typesenseInstantSearchAdapter.searchClient;
 };
-
-export const getTypesenseInstantsearchAdapterRaw = new Typesense.Client({
-  'nodes': [{
-    'host': getEnv(Doppler.NEXT_PUBLIC_TYPESENSE_HOST), // For Typesense Cloud use xxx.a1.typesense.net
-    'port': 443, // For Typesense Cloud use 443
-    'protocol': 'https', // For Typesense Cloud use https
-  }],
-  'apiKey': getEnv(Doppler.NEXT_PUBLIC_TYPESENSE_APIKEY),
-  //'connectionTimeoutSeconds': 2
-});
