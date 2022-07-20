9a33c48fc419ae67fdb94d96e4e461aa0b3fdb68
Gavin <gmaijoe@users.noreply.github.com>
2022-07-20 15:21:36 -0400

    Additional small changes from design (#181)
    * fix: latest
    
    * fix: ok
    
    * fix: additional macbook fix
    
    * fix: ok
    
    * fix: width
    
    * fix: latest
    
    * fix: latest
    
    * using swiper carousel, works out of box
    
    * profile feed blur
    
    * fix: ok
    
    * fix: white
    
    * fix: new
    
    * fix: font
    
    * fix: new
    
    * fix: nit
    
    Co-authored-by: Anthony <anthony@immutableholdings.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -168,7 +168,7 @@ const Index: NextPageWithLayout = ({ preview, data }: HomePageProps) => {
             <div className='h-full md:px-6 px-2 py-12 ...'>
               <div className='text-section leading-header font-header justify-center ...'>
                 {data?.feedTitle}
-                <div className='text-[#7F7F7F] text-body leading-body font-normal tracking-wide text-lg py-2 whitespace-nowrap ...'>
+                <div className='text-[#7F7F7F] text-body leading-body font-normal tracking-wide py-2 whitespace-nowrap ...'>
                   {data?.feedDescription}
                 </div>
                 <ProfileFeed profiles={[profileFeed1, profileFeed2, profileFeed3, profileFeed4, profileFeed5, profileFeed6, profileFeed7, profileFeed8]} />
@@ -176,7 +176,7 @@ const Index: NextPageWithLayout = ({ preview, data }: HomePageProps) => {
                   <Button
                     bgColor={'#F9D963'}
                     color={'#4D4412'}
-                    label='Discover More NFT Profiles'
+                    label='Discover more NFT Profiles'
                     stretch={isMobile}
                     onClick={() => {
                       router.push('/app/gallery');
