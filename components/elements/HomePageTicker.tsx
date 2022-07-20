d10af788fdcbbab5b7d867fc1d120d7da13c97fa
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-07-20 12:22:18 -0400

    fix: latest

--- components/elements/HomePageTicker.tsx
+++ components/elements/HomePageTicker.tsx
@@ -14,10 +14,10 @@ export default function HomePageTicker(props: HomePageTickerProps) {
         <>
           <div style={{ display: 'none' }}>{index=index%tickerStatsLength}</div>
           <div className='flex items-center justify-start py-5 flex-col homeTicker'>
-            <div className='text-always-white text-section leading-2 tracking-wider font-header px-9 min-w-[250px] max-w-[250px] ...'>
+            <div className='text-always-white text-section leading-2 tracking-wider font-header px-9 min-w-[235px] max-w-[235px] ...'>
               {props.tickerStats[`tickerStat${index+1}`].value}
             </div>
-            <div className='text-[#B6B6B6] text-body leading-2 tracking-wider whitespace-normal font-header px-9 min-w-[250px] max-w-[250px] ...'>
+            <div className='text-[#B6B6B6] text-body leading-2 tracking-wider whitespace-normal font-header px-9 min-w-[235px] max-w-[235px] ...'>
               {props.tickerStats[`tickerStat${index+1}`].sub}
             </div>
           </div>
