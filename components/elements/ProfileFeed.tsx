5ca6561fc4c3cbdd4a7c871678cca995b8f28c01
fernandoGalarcio <41962103+fernandoGalarcio@users.noreply.github.com>
2022-07-20 16:07:33 -0500

    profile feed size (#182)
    * profile feed size
    
    * typesense upgrade
    
    * add redirection to profile from slides, fixed slides spacing a little, 1.2 slides visible in mobile

--- components/elements/ProfileFeed.tsx
+++ components/elements/ProfileFeed.tsx
@@ -9,6 +9,7 @@ import { tw } from 'utils/tw';
 
 import { RoundedCornerMedia, RoundedCornerVariant } from './RoundedCornerMedia';
 
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { isMobile } from 'react-device-detect';
 import SwiperCore, {
@@ -25,25 +26,29 @@ export const ProfileFeed = ({ profiles }: ProfileFeedProps) => {
   const router = useRouter();
 
   return (
-    <Swiper slidesPerView={isMobile ? 1 : 3.2} centeredSlides={false} loop={true} autoplay={{
+    <Swiper slidesPerView={isMobile ? 1.2 : 3.2} centeredSlides={false} loop={true} autoplay={{
       'delay': 3000,
       'disableOnInteraction': false
     }} className="flex space-x-1 py-4 drop-shadow-2xl">
       {profiles.map((profile, index) => (
-        <SwiperSlide key={profile?.profile?.id ?? index} className='h-full w-3/4 sm:w-full flex-none cursor-pointer'>
-          <RoundedCornerMedia
-            src={profile?.profile?.photoURL}
-            variant={RoundedCornerVariant.All}
-            containerClasses={tw(
-              'h-[390px] w-3/4 sm:w-full flex-none cursor-pointer relative',
-            )}
-            onClick={() => router.push(`/${profile.profile.url}`)}
-          />
-          <div className='relative w-3/4 sm:w-full h-full flex-none rounded-b-3xl'>
-            <div className='absolute md:h-[45px] xl:h-[50px] rounded-b-3xl bottom-0 flex flex-row items-center w-full bg-white/30 backdrop-blur-md'>
-              <p className='text-white md:text-lg text-xl font-grotesk pl-6'>{profile?.profile?.url}</p>
-            </div>
-          </div>
+        <SwiperSlide key={profile?.profile?.id ?? index} className='flex-none cursor-pointer'>
+          <Link href={`/${profile?.profile?.url}`} passHref>
+            <a>
+              <RoundedCornerMedia
+                src={profile?.profile?.photoURL}
+                variant={RoundedCornerVariant.All}
+                containerClasses={tw(
+                  'h-[390px] w-[93%] sm:w-full flex-none cursor-pointer relative',
+                )}
+                onClick={() => router.push(`/${profile.profile.url}`)}
+              />
+              <div className='relative w-full sm:w-full h-full flex-none rounded-b-3xl'>
+                <div className='absolute md:h-[45px] xl:h-[50px] rounded-b-3xl bottom-0 flex flex-row items-center w-full bg-white/30 backdrop-blur-md'>
+                  <p className='text-white md:text-lg text-xl font-grotesk pl-6'>{profile?.profile?.url}</p>
+                </div>
+              </div>
+            </a>
+          </Link>
         </SwiperSlide>
       ))}
     </Swiper>
