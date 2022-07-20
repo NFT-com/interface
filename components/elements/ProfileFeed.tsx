be7cf5151802a6833a384e7091907b38515e9651
fernandoGalarcio <41962103+fernandoGalarcio@users.noreply.github.com>
2022-07-20 18:30:11 -0500

    fix general max width, number of slides to show (#192)
    * fix general max width, number of slides to show
    
    * fix general max width, number of slides to show
    
    * changed hooks to get screen size
    
    * fixed change from main
    
    * fixed cards overlay
    
    * fixed cards overlay

--- components/elements/ProfileFeed.tsx
+++ components/elements/ProfileFeed.tsx
@@ -5,13 +5,13 @@ import 'swiper/components/pagination/pagination.min.css';
 import 'swiper/components/navigation/navigation.min.css';
 
 import { ProfileQuery } from 'graphql/generated/types';
+import useWindowDimensions from 'hooks/useWindowDimensions';
 import { tw } from 'utils/tw';
 
 import { RoundedCornerMedia, RoundedCornerVariant } from './RoundedCornerMedia';
 
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { isMobile } from 'react-device-detect';
 import SwiperCore, {
   Autoplay,Navigation } from 'swiper/core';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -24,9 +24,10 @@ interface ProfileFeedProps {
 
 export const ProfileFeed = ({ profiles }: ProfileFeedProps) => {
   const router = useRouter();
+  const { width: screenWidth } = useWindowDimensions();
 
   return (
-    <Swiper slidesPerView={isMobile ? 1.2 : 3.2} centeredSlides={false} loop={true} autoplay={{
+    <Swiper slidesPerView={screenWidth < 600 ? 1.2 : screenWidth >= 600 && screenWidth < 900 ? 3.2 : 4.2} centeredSlides={false} loop={true} autoplay={{
       'delay': 3000,
       'disableOnInteraction': false
     }} className="flex space-x-1 py-4 drop-shadow-2xl">
@@ -38,11 +39,11 @@ export const ProfileFeed = ({ profiles }: ProfileFeedProps) => {
                 src={profile?.profile?.photoURL}
                 variant={RoundedCornerVariant.All}
                 containerClasses={tw(
-                  'h-[390px] w-[93%] sm:w-full flex-none cursor-pointer relative',
+                  'h-[390px] w-[93%] flex-none cursor-pointer relative',
                 )}
                 onClick={() => router.push(`/${profile.profile.url}`)}
               />
-              <div className='relative w-full sm:w-full h-full flex-none rounded-b-3xl'>
+              <div className='relative w-[93%] h-full flex-none rounded-b-3xl'>
                 <div className='absolute md:h-[45px] xl:h-[50px] rounded-b-3xl bottom-0 flex flex-row items-center w-full bg-white/30 backdrop-blur-md'>
                   <p className='text-white md:text-lg text-xl font-grotesk pl-6'>{profile?.profile?.url}</p>
                 </div>
