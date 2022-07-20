105d456f928518ea188ca726837c4ad54d5b9c1b
Gavin <gmaijoe@users.noreply.github.com>
2022-07-20 08:00:30 -0400

    Better tablet view (#165)
    fix: new

--- components/elements/HomePageTicker.tsx
+++ components/elements/HomePageTicker.tsx
@@ -14,10 +14,10 @@ export default function HomePageTicker(props: HomePageTickerProps) {
         <>
           <div style={{ display: 'none' }}>{index=index%tickerStatsLength}</div>
           <div className='flex items-center py-5 flex-col homeTicker'>
-            <div className='text-always-white text-section leading-2 font-header px-12 w-full ...'>
+            <div className='text-always-white text-section leading-2 tracking-wider font-header px-12 w-full ...'>
               {props.tickerStats[`tickerStat${index+1}`].value}
             </div>
-            <div className='text-[#B6B6B6] text-body leading-2 font-header px-12 w-full ...'>
+            <div className='text-[#B6B6B6] text-body leading-2 tracking-wider font-header px-12 w-full ...'>
               {props.tickerStats[`tickerStat${index+1}`].sub}
             </div>
           </div>
