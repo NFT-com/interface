d10bc912dbbe996c5d38f4dd66cd77368052272f
just-toby <ejd4@protonmail.com>
2022-07-21 10:54:44 -0700

    feat: more component tests

---
+++ cypress/component/Footer.cy.tsx
@@ -0,0 +1,32 @@
+import '../plugins/tailwind';
+
+import { Footer } from '../../components/elements/Footer';
+import { setupWagmiClient } from '../util/wagmi';
+
+import { WagmiConfig } from 'wagmi';
+
+describe('Footer', () => {
+  it('mounts with valid props', () => {
+    cy.viewport(800, 1200);
+    const client = setupWagmiClient();
+    cy.mount(
+      <WagmiConfig client={client}>
+        <Footer />
+      </WagmiConfig>
+    );
+    cy.findByText('Learn').should('exist');
+    cy.findByText('Gallery').should('exist');
+    cy.findByText('Docs').should('exist');
+    cy.findByText('Blog').should('exist');
+    cy.findByText('Policies').should('exist');
+    cy.findByText('Terms of Service').should('exist');
+    cy.findByText('Privacy Policy').should('exist');
+    cy.findByText('Bug Bounty').should('exist');
+    cy.findByText('Resources').should('exist');
+    cy.findByText('Support').should('exist');
+    cy.findByText('Careers').should('exist');
+    cy.findByText('Social').should('exist');
+    cy.findByText('Discord').should('exist');
+    cy.findByText('Twitter').should('exist');
+  });
+});
