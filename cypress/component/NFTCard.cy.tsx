5d9a28920aae21f4cc5f94bdaf20116824f6f6f8
just toby <66155195+just-toby@users.noreply.github.com>
2022-07-25 10:57:40 -0700

    Feat/codecov updates (#205)
    * feat: more component tests
    
    * feat: cypress config changes to enable component test coverage reports
    
    * fix: remove bad import from cypress.config.ts
    
    * feat: more component tests
    
    * feat: cypress config changes to enable component test coverage reports
    
    * fix: remove bad import from cypress.config.ts
    
    * update doppler vars
    
    * fix: add missing env vars to test run
    
    * feat: more component tests
    
    * feat: cypress config changes to enable component test coverage reports
    
    * fix: remove bad import from cypress.config.ts
    
    * fix: add missing env vars to test run
    
    * wip working on component test coverage
    
    * fix: remove unused invalid import
    
    * fix: remove comment from cypress config
    
    * Feat/ocr initial skeletons (#212)
    
    * wip: skeletons and basic functionality for settings page and contract interaction
    
    * continued wip - on chain skeletons
    
    * updated contract and abi
    
    * updated ui
    
    * Updated component to use ignoreAssociations
    
    * made requested updates
    
    * feat: more component tests
    
    * feat: cypress config changes to enable component test coverage reports
    
    * fix: remove bad import from cypress.config.ts
    
    * fix: add missing env vars to test run
    
    * update doppler vars
    
    * wip working on component test coverage
    
    * fix: remove unused invalid import
    
    * fix: remove comment from cypress config
    
    Co-authored-by: lucas <50972198+robitaille1@users.noreply.github.com>

---
+++ cypress/component/NFTCard.cy.tsx
@@ -0,0 +1,50 @@
+import '../plugins/tailwind';
+
+import { NFTCard } from '../../components/elements/NFTCard';
+import { getMockAnalytics } from '../util/analytics';
+import { setupWagmiClient } from '../util/wagmi';
+
+import { WagmiConfig } from 'wagmi';
+
+describe('NFTCard', () => {
+  beforeEach(() => {
+    window.analytics = getMockAnalytics();
+  });
+
+  it('mounts with valid props', () => {
+    const onClick = cy.stub();
+    const client = setupWagmiClient();
+    cy.mount(
+      <WagmiConfig client={client}>
+        <NFTCard
+          title={'test_nft'}
+          images={[]}
+          onClick={onClick}
+        />
+      </WagmiConfig>
+    );
+    cy.findByText('test_nft').should('exist');
+    cy.findByText('test_nft').click().then(() => {
+      expect(onClick).to.be.calledOnce;
+    });
+  });
+
+  it('mounts with more props', () => {
+    const onClick = cy.stub();
+    const client = setupWagmiClient();
+    cy.mount(
+      <WagmiConfig client={client}>
+        <NFTCard
+          title={'test_nft'}
+          subtitle="test_subtitle"
+          images={[]}
+          onClick={onClick}
+          visible={true}
+          constrain
+        />
+      </WagmiConfig>
+    );
+    cy.findByText('test_subtitle').should('exist');
+    cy.get('#eye').should('exist');
+  });
+});
---
+++ cypress/component/Sidebar.cy.tsx
@@ -0,0 +1,60 @@
+import '../plugins/tailwind';
+
+import { Sidebar } from '../../components/elements/Sidebar';
+import { useSidebar } from '../../hooks/state/useSidebar';
+import { rainbowDark } from '../../styles/RainbowKitThemes';
+import { Doppler, getEnv } from '../../utils/env';
+import { setupWagmiClient } from '../util/wagmi';
+
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { chain, configureChains, WagmiConfig } from 'wagmi';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+
+const { chains } = configureChains(
+  getEnv(Doppler.NEXT_PUBLIC_ENV) !== 'PRODUCTION' ?
+    [chain.mainnet, chain.goerli, chain.rinkeby] :
+    [chain.mainnet],
+  [
+    jsonRpcProvider({
+      rpc: (chain) => {
+        const url = new URL(getEnv(Doppler.NEXT_PUBLIC_BASE_URL) + 'api/ethrpc');
+        url.searchParams.set('chainId', String(chain.id));
+        return {
+          http: url.toString(),
+        };
+      }
+    }),
+  ]
+);
+
+const TestComponent = () => {
+  const { toggleSidebar } = useSidebar();
+  return <div>
+    <Sidebar />
+    <button onClick={() => {
+      toggleSidebar();
+    }}>openSidebar</button>
+  </div>;
+};
+
+describe('Sidebar', () => {
+  it('mounts with valid props', () => {
+    cy.viewport(800, 1200);
+    const client = setupWagmiClient();
+    cy.mount(
+      <WagmiConfig client={client}>
+        <RainbowKitProvider
+          appInfo={{
+            appName: 'NFT.com',
+            learnMoreUrl: 'https://docs.nft.com/',
+          }}
+          theme={rainbowDark}
+          chains={chains}>
+          <TestComponent />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    );
+    cy.findByText('openSidebar').click();
+    cy.findByText('Sign In').should('exist');
+  });
+});
