856e5b0fa92520e117eec1cba0b3e25b29f87756
fernandoGalarcio <41962103+fernandoGalarcio@users.noreply.github.com>
2022-07-26 13:44:59 -0500

    Refactor searchbar (#216)
    * remove instant search and use typsense api in searchbar
    
    * fix search bar colors due to forced dark mode
    
    * fix search bar colors due to forced dark mode

--- components/elements/SearchBar.tsx
+++ components/elements/SearchBar.tsx
@@ -1,136 +1,147 @@
 import { useOutsideClickAlerter } from 'hooks/useOutsideClickAlerter';
 import { tw } from 'utils/tw';
-import { MultiIndexTypesenseInstantSearchAdapter } from 'utils/typeSenseAdapters';
+import { getTypesenseInstantsearchAdapterRaw, SearchableFields } from 'utils/typeSenseAdapters';
 
 import { useRouter } from 'next/router';
 import SearchIcon from 'public/search.svg';
-import { useEffect, useRef, useState } from 'react';
-import {
-  Configure,
-  connectStateResults,
-  Highlight,
-  Hits,
-  Index,
-  InstantSearch,
-  SearchBox } from 'react-instantsearch-dom';
+import { useRef, useState } from 'react';
 
-const Hit = (hit) => {
-  const router = useRouter();
-  return (
-    <div
-      className={tw(
-        'flex flex-col text-sm my-1',
-        'font-medium dark:text-always-white hover:cursor-pointer')}
-      onClick={() => {
-        if (hit.hit.url) {
-          router.push(`/${hit.hit.url}`);
-        }
+type SearchBarProps = {
+  bgLight?: boolean
+}
 
-        if (!hit.hit.nftName) {
-          router.push(`/app/collection/${hit.hit.contractAddr}/`);
-        } else {
-          router.push(`/app/nft/${hit.hit.contractAddr}/${hit.hit.tokenId}`);
-        }
-      }}>
-      <Highlight attribute={!hit.hit.nftName ? hit.hit.url ? 'url' : 'contractName' : 'nftName'} nonHighlightedTagName="span" hit={hit.hit} />
-      {hit.hit.contractAddr && <span className="text-[0.7rem]"><Highlight attribute={'contractAddr'} nonHighlightedTagName="span" hit={hit.hit} /></span>}
-      {hit.hit._highlightResult.tokenId && hit.hit._highlightResult.tokenId.matchLevel === 'full' && <span className="text-[0.7rem]"><Highlight attribute={'tokenId'} nonHighlightedTagName="span" hit={hit.hit} /></span>}
-    </div>
-  );
-};
-
-export const SearchBar = () => {
+export const SearchBar = (props: SearchBarProps) => {
   const [showHits, setShowHits] = useState(false);
-  const [displaySearchBar, setDisplaySearchBar] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
   const router = useRouter();
-
   const resultsRef = useRef();
-
-  useEffect(() => {
-    setTimeout(() => {
-      setDisplaySearchBar(true);
-    }, 1000);
-  },[]);
-
-  const searchClient = MultiIndexTypesenseInstantSearchAdapter.searchClient;
+  const cliente = getTypesenseInstantsearchAdapterRaw;
 
   useOutsideClickAlerter(resultsRef, () => {
     setShowHits(false);
   });
 
-  const Results = connectStateResults(
-    ({ searchResults, searchState }) => {
-      if (!searchState.query) {
-        setShowHits(false);
-        return '';
-      }
-
-      return searchResults && searchResults.nbHits !== 0
-        ? (
-          <>
-            <Configure hitsPerPage={5} />
-            <Hits hitComponent={Hit}/>
-          </>
-        )
-        : (
-          <div className="dark:text-always-white text-sm p-3 text-gray-500">
-            No results found
-          </div>
-        );
+  const goTo = (document) => {
+    if (document.url) {
+      router.push(`/${document.url}`);
     }
-  );
+    if (!document.nftName) {
+      router.push(`/app/collection/${document.contractAddr}/`);
+    } else {
+      router.push(`/app/nft/${document.contractAddr}/${document.tokenId}`);
+    }
+  };
+
+  const search = (event) => {
+    const target = event.target as HTMLInputElement;
+
+    if (target.value.length < 3) {
+      setShowHits(false);
+      return;
+    }
+
+    const searchRequests = {
+      'searches': [
+        {
+          'collection': 'nfts',
+          'q': target.value,
+          'query_by': SearchableFields.NFTS_INDEX_FIELDS,
+          'per_page': 4,
+        },
+        {
+          'collection': 'collections',
+          'q': target.value,
+          'query_by': SearchableFields.COLLECTIONS_INDEX_FIELDS,
+          'per_page': 4,
+        },
+        {
+          'collection': 'profiles',
+          'q': target.value,
+          'query_by': SearchableFields.PROFILES_INDEX_FIELDS,
+          'per_page': 4,
+        }
+      ]
+    };
+
+    cliente.multiSearch.perform(searchRequests)
+      .then((data) => {
+        setSearchResults([...data.results]);
+      })
+      .catch((error) => {
+        console.log(error);
+        setShowHits(false);
+      });
+
+    setShowHits(true);
+
+    if (event.keyCode === 13) {
+      router.push(`/app/results/${target.value !== '' ? target.value : '0'}`);
+      setShowHits(false);
+      target.click();
+    }
+  };
 
   return (
     <>
-      {displaySearchBar && <InstantSearch
-        searchClient={searchClient}
-        indexName="collections"
-        onSearchStateChange={() => {
-          // TODO: depending on the data from searchState.query, add behavior
-        }}>
-
-        <div className="relative w-full">
-          <div className="flex items-center border rounded-xl py-2 px-3 w-full dark:text-always-white">
-            <SearchIcon className='mr-2 shrink-0 aspect-square' />
-            <SearchBox
-              submit={null}
-              reset={null}
-              onKeyDown={(event) => {
-                setShowHits(true);
-                const target = event.target as HTMLInputElement;
-                if (event.keyCode === 13) {
-                  setShowHits(false);
-                  router.push(`/app/results/${target.value !== '' ? target.value : '0' }`);
-                }
-              }}/>
+      <div className="flex flex-col w-full">
+        <div className={tw(
+          'relative flex items-center border rounded-xl py-2 px-3',
+          'mr-4 md:hidden block w-full',
+          props.bgLight ? 'text-black':'text-white')}>
+          <SearchIcon className='mr-2 shrink-0 aspect-square' />
+          <div className="w-full">
+            <input
+              type="search"
+              placeholder="Search here…"
+              autoComplete="off"
+              autoCorrect="off"
+              autoCapitalize="off"
+              spellCheck="false"
+              required maxLength={512}
+              className="bg-inherit w-full"
+              onKeyUp={(event) => search(event)}
+              onFocus={(event) => search(event)}/>
           </div>
-          
-          {showHits && (
-            <div
-              ref={resultsRef}
-              className={tw(
-                'absolute bg-always-white dark:bg-always-black',
-                'flex flex-col w-full',
-                'p-4 border border-grey z-50 text-rubik rounded-xl')}>
-              <Index indexName="collections">
-                <span className="text-xs text-gray-400">Collections</span>
-                <Results/>
-              </Index>
-
-              <Index indexName="nfts">
-                <span className="text-xs text-gray-400">NFTs</span>
-                <Results/>
-              </Index>
-
-              <Index indexName="profiles">
-                <span className="text-xs text-gray-400">Profiles</span>
-                <Results/>
-              </Index>
-              <span className="text-xs text-gray-400">Press enter for all results</span>
-            </div>
-          )}
         </div>
-      </InstantSearch>
-      }
+        {showHits && (
+          <div
+            ref={resultsRef}
+            className={tw(
+              'absolute mt-10 max-w-[27rem]',
+              props.bgLight ? 'bg-always-white':'bg-always-black',
+              'flex flex-col w-full p-4 border border-grey z-50 text-rubik rounded-xl')}>
+            {searchResults.length > 0 && searchResults.map((item, index) => {
+              return (
+                <div key={index}>
+                  <span className={tw(
+                    'text-xs text-gray-400',
+                    props.bgLight ? 'text-gray-400':'text-white')}>
+                    {item.request_params.collection_name.toUpperCase()}</span>
+                  {item.found === 0 ?
+                    <div className={tw(
+                      props.bgLight ? 'text-gray-400':'text-white',
+                      'text-sm p-3 text-gray-500')}>
+                        No results found
+                    </div>
+                    : (item.hits.map((hit, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className={tw(
+                            'flex flex-col text-sm my-1 font-medium',
+                            props.bgLight ? 'text-gray-400':'text-white',
+                            'hover:cursor-pointer hover:opacity-70')}
+                          onClick={() => goTo(hit.document)}>
+                          <span>{hit.document.contractName}</span>
+                          <span>{hit.document.url}</span>
+                          <span className="text-[0.7rem]">{hit.document.contractAddr}</span>
+                        </div>
+                      );
+                    }))}
+                </div>);
+            })}
+            <span className="text-xs text-gray-400">Press enter for all results</span>
+          </div>)}
+      </div>
     </>);
 };
