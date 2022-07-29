0c6123a15f408ec4aa83b9a59417a64299261de8
just toby <66155195+just-toby@users.noreply.github.com>
2022-07-28 21:43:05 -0700

    feat: update breakpoints on blog pages and components (#223)
    * feat: update breakpoints on blog pages and components
    
    * fix: test

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -59,11 +59,11 @@ export default function Post({ post, preview }: PostProps) {
         }}
       />
       <div className='bg-white'>
-        <div className="relative text-center px-4 w-full pt-28 sm:pt-24 max-w-7xl mx-auto">
+        <div className="relative text-center px-4 w-full minmd:pt-28 pt-24 max-w-nftcom mx-auto">
           <Link href="/articles">
-            <div className='flex content-center items-center hover:cursor-pointer sm:mb-4'>
+            <div className='flex content-center items-center hover:cursor-pointer mb-4 minmd:mb-0'>
               <CaretLeft className='mr-1 text-black' />
-              <p className='font-grotesk text-black sm:text-sm'>Back to Blog</p>
+              <p className='font-grotesk text-black text-sm minmd:text-base'>Back to Blog</p>
             </div>
           </Link>
           
@@ -73,16 +73,16 @@ export default function Post({ post, preview }: PostProps) {
             src={post?.heroImage.url}
             alt={post?.heroImage.description}
           />
-          <div className="text-left mt-12 md:mt-8 mx-auto pb-5 w-8/12 lg:w-3/4 md:w-full">
+          <div className="text-left minlg:mt-12 mt-8 mx-auto pb-5 minxl:w-8/12 minlg:w-3/4 w-full">
             <Markdown content={post?.body} />
           </div>
           {post?.relatedPostsCollection?.items.length
             ? (
               <div>
-                <h2 className="text-left mb-6 sm:mb-3 font-medium mt-8 md:mt-4 sm:mt-1.5 text-3xll md:text-xl sm:text-sm">
+                <h2 className="text-left minmd:mb-6 mb-3 font-medium minlg:mt-8 minmd:mt-4 mt-1.5 minlg:text-3xll minmd:text-xl text-sm">
                 Related Posts
                 </h2>
-                <div className="grid gap-x-4 sm:gap-x-3 gap-y-7 grid-cols-3 md:grid-cols-2 pb-24 lg:pb-12">
+                <div className="grid minmd:gap-x-4 gap-x-3 gap-y-7 minlg:grid-cols-3 grid-cols-2 minxl:pb-24 pb-12">
                   {post.relatedPostsCollection.items.map((post) => (
                     post && <RelatedPostCard key={post?.sys.id} post={post} />
                   ))}
