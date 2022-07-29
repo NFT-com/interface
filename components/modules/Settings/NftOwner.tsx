5d35348935549562db8cd0b27a27ef6f1241ac16
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-07-29 16:21:34 -0700

    Added new profile card and made updates

--- components/modules/Settings/NftOwner.tsx
+++ components/modules/Settings/NftOwner.tsx
@@ -1,12 +1,18 @@
-export default function NftOwner() {
+import ProfileCard from 'components/modules/Sidebar/ProfileCard';
+import { useMyNftProfileTokens } from 'hooks/useMyNftProfileTokens';
+
+type NftOwnerProps = {
+  selectedProfile: string
+}
+
+export default function NftOwner({ selectedProfile }: NftOwnerProps) {
+  const { profileTokens: myOwnedProfileTokens } = useMyNftProfileTokens();
   return (
     <div id="owner" className='md:mt-10 mt-0 font-grotesk'>
-      <h2 className='text-black mb-2 font-bold md:text-2xl text-4xl tracking-wide'>NFT Owner</h2>
+      <h2 className='text-black mb-2 font-bold md:text-2xlz text-4xl tracking-wide'>NFT Owner</h2>
       <p className='mb-4 text-[#6F6F6F]'>Select which profile will display as the owner for your NFTs and collections.</p>
 
-      <div className='bg-black w-full h-16 rounded-[10px] p-4 flex items-center'>
-        <p className='text-white'>BANANA</p>
-      </div>
+      {selectedProfile !== '' && <ProfileCard showSwitch profile={myOwnedProfileTokens?.find(t => t.title === selectedProfile)} />}
     </div>
   );
 }
--- components/modules/Sidebar/ProfileCard.tsx
+++ components/modules/Sidebar/ProfileCard.tsx
@@ -11,30 +11,36 @@ type ProfileCardProps = {
   }
   onClick?: (profileUrl) => void
   message?: string
+  showSwitch?: boolean
 };
 
-export default function ProfileCard({ profile, onClick, message }: ProfileCardProps) {
+export default function ProfileCard({ profile, onClick, message, showSwitch }: ProfileCardProps) {
   return (
     <motion.div className='mb-4 rounded-[10px]'>
-      <input className="sr-only peer" type="radio" value={profile.title} name="profile" id={`profile_${profile.title}`}></input>
-      <label className="flex cursor-pointer rounded-[10px] peer-checked:ring-[#F9D963] peer-checked:ring-2 peer-checked:border-transparent" htmlFor={`profile_${profile.title}`}>
+      <input className="sr-only peer" type="radio" value={profile?.title} name="profile" id={`profile_${profile?.title}`}></input>
+      <label className="flex cursor-pointer rounded-[10px] peer-checked:ring-[#F9D963] peer-checked:ring-2 peer-checked:border-transparent" htmlFor={`profile_${profile?.title}`}>
         <motion.div
           style={{
-            background: `url("${profile.metadata.header}")`,
+            backgroundImage: `url("${profile?.metadata?.header}")`,
             backgroundPosition: 'center',
             backgroundSize: 'cover',
+            boxShadow: 'inset 0 0 0 1000px rgba(0,0,0,.8)'
           }}
-          className='bg-black rounded-[10px] hover:cursor-pointer w-full'
-          onClick={() => onClick(profile.title)}
+          className=' rounded-[10px] hover:cursor-pointer w-full bg-cover bg-center'
+          onClick={onClick ? () => onClick(profile?.title) : null}
         >
-          <div className='bg-black opacity-80 flex items-center py-4 px-4 rounded-[10px]' >
-            <Image className='rounded-full' width={50} height={50} alt={`profile for ${profile.title}`} src={profile.metadata.image} />
-            <div>
-              {message &&
-                <p className='font-grotesk text-xs text-yellow-400 leading-6 font-bold -mb-1 ml-3 tracking-wider uppercase'>{message}</p>
-              }
-              <p className='font-grotesk text-base text-white leading-6 font-medium ml-3 tracking-wide'>{profile.title}</p>
+          <div className='flex items-center py-4 px-4 rounded-[10px]' >
+            {profile?.metadata?.image && <Image className='rounded-full' width={50} height={50} alt={`profile for ${profile?.title}`} src={profile?.metadata?.image} />}
+            <div className='flex justify-between w-full'>
+              <div>
+                {message &&
+                <p className='font-grotesk text-xs text-[#F9D963] leading-6 font-bold -mb-1 ml-3 tracking-wider uppercase'>{message}</p>
+                }
+                <p className='font-grotesk text-base text-white leading-6 font-medium ml-3 tracking-wide'>{profile?.title}</p>
+              </div>
+              {showSwitch && <p className='text-[#F9D963]'>Switch</p>}
             </div>
+            
           </div>
         </motion.div>
       </label>
