15ba19931b83633d6a6593f5fd736bb38c17a756
Anthony <anthony@immutableholdings.com>
2022-07-31 21:58:39 -0600

    update contract definitions for goerli maxProfiles contract, check claimable on new contract

---
+++ hooks/state/useFreeMintAvailable.ts
@@ -0,0 +1,46 @@
+import { Maybe } from 'graphql/generated/types';
+import { useAllContracts } from 'hooks/contracts/useAllContracts';
+import { isNullOrEmpty } from 'utils/helpers';
+
+import { useState } from 'react';
+import useSWR, { mutate } from 'swr';
+
+export interface FreeMintAvailableResult {
+  freeMintAvailable: Maybe<boolean>;
+  loading: boolean;
+  mutate: () => void;
+}
+
+export function useFreeMintAvailable(address: string): FreeMintAvailableResult {
+  const { maxProfiles } = useAllContracts();
+  const [loading, setLoading] = useState(false);
+
+  const keyString = 'freeMintAvailable ' + address;
+
+  const { data } = useSWR(keyString,
+    async () => {
+      if(
+        isNullOrEmpty(address)
+      ) {
+        return false;
+      }
+      setLoading(true);
+      try {
+        const freeMintAvailable = await maxProfiles.publicMinted(address);
+        setLoading(false);
+        return freeMintAvailable.toNumber() === 0;
+      } catch (e) {
+        setLoading(false);
+        console.log('error getting free mint status');
+        return false;
+      }
+    });
+
+  return {
+    freeMintAvailable: data ?? null,
+    loading,
+    mutate: () => {
+      mutate(keyString);
+    }
+  };
+}
