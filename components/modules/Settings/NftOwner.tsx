d095985be0aa04e2c4ccde51ba7dedfcd452af9b
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-08-02 11:34:27 -0700

    wip: NFT owner section

--- components/modules/Settings/NftOwner.tsx
+++ components/modules/Settings/NftOwner.tsx
@@ -1,5 +1,10 @@
+import { Modal } from 'components/elements/Modal';
 import ProfileCard from 'components/modules/Sidebar/ProfileCard';
 import { useMyNftProfileTokens } from 'hooks/useMyNftProfileTokens';
+import { Doppler, getEnvBool } from 'utils/env';
+
+import { XCircle } from 'phosphor-react';
+import { useEffect, useState } from 'react';
 
 type NftOwnerProps = {
   selectedProfile: string
@@ -7,12 +12,85 @@ type NftOwnerProps = {
 
 export default function NftOwner({ selectedProfile }: NftOwnerProps) {
   const { profileTokens: myOwnedProfileTokens } = useMyNftProfileTokens();
+  const [visible, setVisible] = useState(false);
+  const [allProfiles, setAllProfiles] = useState([]);
+  const [profilesToShow, setProfilesToShow] = useState([]);
+  const [selected, setSelected] = useState('lucasgoerli'); // Will be new state from endpoint
+  useEffect(() => {
+    setAllProfiles(myOwnedProfileTokens);
+  }, [myOwnedProfileTokens]);
+
+  useEffect(() => {
+    allProfiles.sort((a, b) => a.title.localeCompare(b.title));
+    const index = allProfiles.findIndex((e) => e.title === selected);
+    allProfiles.unshift(...allProfiles.splice(index, 1));
+    setProfilesToShow(allProfiles.slice(0, 3));
+  }, [myOwnedProfileTokens, allProfiles, selected]);
+
+  const searchHandler = (query) => {
+    setAllProfiles(allProfiles.filter((item) => item.title.includes(query)));
+
+    if(query === ''){
+      setAllProfiles(myOwnedProfileTokens);
+    }
+  };
+
+  const LoadMoreHandler = () => {
+    const currentLength = profilesToShow.length;
+    const nextProfiles = allProfiles.slice(currentLength, currentLength + 3);
+    setProfilesToShow([...profilesToShow, ...nextProfiles]);
+  };
+
   return (
     <div id="owner" className='md:mt-10 mt-0 font-grotesk'>
       <h2 className='text-black mb-2 font-bold md:text-2xlz text-4xl tracking-wide'>NFT Owner</h2>
       <p className='mb-4 text-[#6F6F6F]'>Select which profile will display as the owner for your NFTs and collections.</p>
 
-      {selectedProfile !== '' && <ProfileCard showSwitch profile={myOwnedProfileTokens?.find(t => t.title === selectedProfile)} />}
+      {selectedProfile !== '' && <ProfileCard onClick={setVisible} opensModal showSwitch profile={myOwnedProfileTokens?.find(t => t.title === selectedProfile)} />}
+      
+      <Modal
+        visible={visible}
+        loading={false}
+        title={'Request Modal'}
+        onClose={() => {
+          setVisible(false);
+        }}
+        fullModal
+        bgColor='white'
+        pure
+      >
+        <div className='w-full h-screen bg-white text-left px-4'>
+          <XCircle onClick={() => setVisible(false)} className='absolute top-5 right-3 hover:cursor-pointer' size={32} color="black" weight="fill" />
+          <div className='pt-28 font-grotesk lg:max-w-md max-w-lg m-auto'>
+            <div>
+              <h2 className='text-4xl tracking-wide font-bold mb-10'>Set Owner</h2>
+              <p className='text-[#6F6F6F] mb-4'>Select the profile to sign-in with by default.</p>
+              <input onChange={event => searchHandler(event.target.value.toLowerCase())} className="shadow appearance-none border rounded-[10px] w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4 border-[#D5D5D5] placeholder:text-sm" id="currentAddress" type="text" placeholder="Profile Name" />
+            </div>
+            <div>
+              {profilesToShow && profilesToShow?.map((profile) => {
+                return (
+                  <ProfileCard isSelected={selected === profile.title} key={profile?.title} onClick={setSelected} profile={profile} />
+                );
+              })}
+            </div>
+            {getEnvBool(Doppler.NEXT_PUBLIC_PROFILE_FACTORY_ENABLED) && (
+              <button className="bg-black text-base font-bold tracking-normal mb-4 text-[#F9D963] py-2 px-4 rounded-[10px] focus:outline-none focus:shadow-outline w-full" type="button">
+              Get a New Profile
+              </button>
+            )}
+            {allProfiles.length > 3
+              ?
+              (
+                <button onClick={() => LoadMoreHandler()} className="bg-[#F9D963] font-bold tracking-normal hover:bg-[#fcd034] text-base text-black py-2 px-4 rounded-[10px] focus:outline-none focus:shadow-outline w-full" type="button">
+              Load More
+                </button>
+              )
+              : null
+            }
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 }
--- components/modules/Sidebar/ProfileCard.tsx
+++ components/modules/Sidebar/ProfileCard.tsx
@@ -1,5 +1,8 @@
+import { tw } from 'utils/tw';
+
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import { SetStateAction } from 'react';
 
 type ProfileCardProps = {
   profile: {
@@ -9,41 +12,45 @@ type ProfileCardProps = {
     }
     title: string
   }
-  onClick?: (profileUrl) => void
+  onClick?: (value?: string | SetStateAction<boolean> | SetStateAction<string> ) => void
+  onChange?: (value?: string ) => void
+  opensModal?: boolean
   message?: string
   showSwitch?: boolean
+  isSelected?: boolean
 };
 
-export default function ProfileCard({ profile, onClick, message, showSwitch }: ProfileCardProps) {
+export default function ProfileCard({ profile, onClick, message, showSwitch, opensModal, isSelected }: ProfileCardProps) {
   return (
-    <motion.div className='mb-4 rounded-[10px]'>
-      <input className="sr-only peer" type="radio" value={profile?.title} name="profile" id={`profile_${profile?.title}`}></input>
-      <label className="flex cursor-pointer rounded-[10px] peer-checked:ring-[#F9D963] peer-checked:ring-2 peer-checked:border-transparent" htmlFor={`profile_${profile?.title}`}>
-        <motion.div
-          style={{
-            backgroundImage: `url("${profile?.metadata?.header}")`,
-            backgroundPosition: 'center',
-            backgroundSize: 'cover',
-            boxShadow: 'inset 0 0 0 1000px rgba(0,0,0,.8)'
-          }}
-          className=' rounded-[10px] hover:cursor-pointer w-full bg-cover bg-center'
-          onClick={onClick ? () => onClick(profile?.title) : null}
-        >
-          <div className='flex items-center py-4 px-4 rounded-[10px]' >
-            {profile?.metadata?.image && <Image className='rounded-full' width={50} height={50} alt={`profile for ${profile?.title}`} src={profile?.metadata?.image} />}
-            <div className='flex justify-between w-full'>
-              <div>
-                {message &&
+    <motion.div className={tw(
+      'mb-4 rounded-[10px] box-border',
+      isSelected && 'outline-[#F9D963] outline-offset-[-1px] outline-2 outline'
+    )}>
+      <motion.div
+        style={{
+          backgroundImage: `url("${profile?.metadata?.header}")`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          boxShadow: 'inset 0 0 0 1000px rgba(0,0,0,.8)'
+        }}
+        className={tw(
+          'rounded-[10px] hover:cursor-pointer w-full bg-cover bg-center box-border',
+        )}
+        onClick={onClick ? !opensModal ? () => onClick(profile?.title) : () => onClick(true) : null}
+      >
+        <div className='flex items-center py-4 px-4 rounded-[10px]' >
+          {profile?.metadata?.image && <Image className='rounded-full' width={50} height={50} alt={`profile for ${profile?.title}`} src={profile?.metadata?.image} />}
+          <div className='flex justify-between w-full'>
+            <div>
+              {message &&
                 <p className='font-grotesk text-xs text-[#F9D963] leading-6 font-bold -mb-1 ml-3 tracking-wider uppercase'>{message}</p>
-                }
-                <p className='font-grotesk text-base text-white leading-6 font-medium ml-3 tracking-wide'>{profile?.title}</p>
-              </div>
-              {showSwitch && <p className='text-[#F9D963]'>Switch</p>}
+              }
+              <p className='font-grotesk text-base text-white leading-6 font-medium ml-3 tracking-wide'>{profile?.title}</p>
             </div>
-            
+            {showSwitch && <p className='text-[#F9D963]'>Switch</p>}
           </div>
-        </motion.div>
-      </label>
+        </div>
+      </motion.div>
     </motion.div>
   );
 }
