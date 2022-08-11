81662c778872cfc9b12e4a2af39dd9d475bc3135
fernandoGalarcio <fernando.galarcio.m@gmail.com>
2022-08-11 10:27:13 -0500

    results page adjustment

--- cypress/e2e/DiscoverPage/DiscoverPage.cy.ts
+++ cypress/e2e/DiscoverPage/DiscoverPage.cy.ts
@@ -12,7 +12,7 @@ describe('discover page tests', () => {
   it('renders next 2 items after clicking load more button', () => {
     cy.visit('/app/discover/').then(() => {
       cy.get('.buttonContainer').should('exist').click().then(() =>{
-        cy.get('.DiscoverCollectionItem').should('have.length', 8);
+        cy.get('.DiscoverCollectionItem').should('have.length', 12);
       });
     });
   });
