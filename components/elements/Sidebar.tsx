d0eca3907af24c8994471f8d3fe69e1f8ee57447
lucas <50972198+robitaille1@users.noreply.github.com>
2022-08-16 15:27:55 -0700

    Fix for missing scrollbars after modal popup - toggle sidebar close a… (#286)
    Fix for missing scrollbars after modal popup - toggle sidebar close after closing modal

--- components/elements/Sidebar.tsx
+++ components/elements/Sidebar.tsx
@@ -38,7 +38,7 @@ export const Sidebar = () => {
   const { data: balanceData } = useBalance({ addressOrName: currentAddress, watch: true });
 
   const { setSignOutDialogOpen } = useSignOutDialog();
-  const { sidebarOpen, setSidebarOpen, toggleSidebar } = useSidebar();
+  const { sidebarOpen, setSidebarOpen } = useSidebar();
   const randomLabel = useMemo(() => randomLabelGenerator(), []);
   const { getHiddenProfileWithExpiry, user, setCurrentProfileUrl } = useUser();
   const { addFundsDialogOpen } = useAddFundsDialog();
@@ -111,7 +111,6 @@ export const Sidebar = () => {
             onClick={() => {
               disconnect();
               setSignOutDialogOpen(true);
-              toggleSidebar();
               setCurrentProfileUrl('');
             }}>
             Sign out
@@ -168,7 +167,7 @@ export const Sidebar = () => {
         />
       );
     }
-  }, [balanceData?.formatted, balanceData?.symbol, balanceData?.value, currentAddress, disconnect, ethPriceUSD, hiddenProfile, myOwnedProfileTokens, profileValue, randomLabel, router.pathname, setCurrentProfileUrl, setSidebarOpen, setSignOutDialogOpen, toggleSidebar, user?.currentProfileUrl]);
+  }, [balanceData?.formatted, balanceData?.symbol, balanceData?.value, currentAddress, disconnect, ethPriceUSD, hiddenProfile, myOwnedProfileTokens, profileValue, randomLabel, router.pathname, setCurrentProfileUrl, setSidebarOpen, setSignOutDialogOpen, user?.currentProfileUrl]);
 
   const getSidebarPanel = useCallback(() => {
     if(isNullOrEmpty(currentAddress)) {
