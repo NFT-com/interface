eae623040080290db2b1ea874396446b61acb803
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-08-18 14:56:00 -0700

    Updated notification icon styling

--- components/elements/WalletRainbowKitButton.tsx
+++ components/elements/WalletRainbowKitButton.tsx
@@ -110,9 +110,11 @@ export const WalletRainbowKitButton = (props : WalletRainbowKitButtonProps) => {
         return (
           <>
             {(getEnvBool(Doppler.NEXT_PUBLIC_ON_CHAIN_RESOLVER_ENABLED) && getNotificationCount() > 0) && (
-              <span className="flex h-5 w-5 minmd:-mb-3 -mb-1 right-2 minmd:right-0 relative z-50">
-                <span className="animate-ping absolute inline-flex h-5 w-5 rounded-full bg-[#F9D963] opacity-75"></span>
-                <span className="relative bg-[#F9D963] w-5 h-5 flex items-center rounded-full px-[5px]">{getNotificationCount()}</span>
+              <span className="flex h-5 min-w-[20px] minmd:-mb-3 -mb-1 right-2 minmd:right-0 relative z-50">
+                <span className="relative bg-[#F9D963] rounded-full min-w-[20px] h-5 flex justify-center items-center px-[5px] leading-[0px] text-sm font-mono">
+                  {getNotificationCount()}
+                  <span className="animate-ping absolute inline-flex h-5 w-full rounded-full bg-[#F9D963] opacity-75"></span>
+                </span>
               </span>
             )
             }
