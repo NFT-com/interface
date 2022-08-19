46cca3d2ba735650f323b9eb26b2ce34e2ef4124
lucas <50972198+robitaille1@users.noreply.github.com>
2022-08-19 16:39:09 -0700

    Fix/nft 602 (#318)
    * updated styling of cards to hide grey border
    
    * undid changes to wrong component

--- components/elements/NFTCard.tsx
+++ components/elements/NFTCard.tsx
@@ -76,7 +76,7 @@ export function NFTCard(props: NFTCardProps) {
   return (
     <div
       className={tw(
-        'drop-shadow-md rounded-xl flex flex-col',
+        'drop-shadow-md rounded-2xl flex flex-col h-max',
         props.constrain ?
           // constrain self to 2 or 4 per row
           'w-2/5 minlg:w-[23%]' :
