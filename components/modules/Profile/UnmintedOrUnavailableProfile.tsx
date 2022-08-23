23ce2262d60a13a4f5c9fda2dcad0c3da71d09b5
just-toby <ejd4@protonmail.com>
2022-08-22 17:13:27 -0700

    fix: unminted profile layout fixes

--- components/modules/Profile/UnmintedOrUnavailableProfile.tsx
+++ components/modules/Profile/UnmintedOrUnavailableProfile.tsx
@@ -2,8 +2,11 @@
 import { Footer } from 'components/elements/Footer';
 import { BannerWrapper } from 'components/modules/Profile/BannerWrapper';
 import { useProfileBlocked } from 'hooks/useProfileBlocked';
+import { tw } from 'utils/tw';
 
 import { LinksToSection } from './LinksToSection';
+
+import { isMobile } from 'react-device-detect';
 
 export interface UnmintedOrUnavailableProps {
   profileURI: string;
@@ -15,10 +18,14 @@ export function UnmintedOrUnavailableProfile(props: UnmintedOrUnavailableProps) 
   const { blocked: currentURIBlocked } = useProfileBlocked(profileURI, true);
 
   return (
-    <div className="mt-20 h-screen bg-pagebg">
+    <div className="mt-20 h-screen bg-pagebg w-full flex flex-col">
       <BannerWrapper />
       <div
-        className='mb-0 minlg:mb-8 mx-16 flex justify-start max-w-nftcom'
+        className={tw(
+          'mb-0 minlg:mb-8 mx-16 flex justify-start',
+          'max-w-7xl min-w-[60%]',
+          isMobile ? 'mx-2' : 'mx-2 minmd:mx-8 minxl:mx-auto',
+        )}
         style={{
           zIndex: 103,
         }}
@@ -26,12 +33,19 @@ export function UnmintedOrUnavailableProfile(props: UnmintedOrUnavailableProps) 
         <div className="flex items-center md:flex-col">
           <div className="flex items-end mt-[-30px] minlg:mt-[-86px] minxl:mt-[-125px]">
             <section>
-              <div className='relative outline-none'>
+              <div className={tw(
+                'relative outline-none',
+                'h-32 minlg:h-52 w-32 minlg:w-52',
+              )}>
                 <img
                   src={'https://cdn.nft.com/profile-image-default.svg'}
                   alt="profilePicture"
                   draggable={false}
-                  className="object-center rounded-full h-[72px] minlg:h-[160px] minxl:h-60 w-[72px] minlg:w-[160px] minxl:w-60"
+                  className={tw(
+                    'object-center rounded-full',
+                    'h-full w-full',
+                    'shrink-0 aspect-square',
+                  )}
                   style={{ zIndex: 101, }}
                 />
               </div>
@@ -44,16 +58,15 @@ export function UnmintedOrUnavailableProfile(props: UnmintedOrUnavailableProps) 
               </div>
             </div>
           </div>
-
         </div>
       </div>
-      <main className='justify-start space-y-4 w-full mx-16 max-w-nftcom flex flex-col'>
-        <div className='text-sm minxl:text-xl text-primary-txt dark:text-primary-txt-dk w-full flex justify-center flex-col'>
+      <div className='space-y-4 w-full max-w-nftcom flex flex-col items-center self-center'>
+        <div className='text-sm minxl:text-xl text-primary-txt dark:text-primary-txt-dk w-full flex justify-center flex-col mt-8 minmd:mt-0'>
           <div className="text-center font-bold mx-auto w-full">
             {(notAvailable || currentURIBlocked) ? 'This profile is not available.' : 'This profile is available and is ready to be minted!' }
           </div>
         </div>
-        <div className='text-primary-txt dark:text-primary-txt-dk w-full flex justify-center flex-col'>
+        <div className='text-primary-txt dark:text-primary-txt-dk w-full flex justify-center items-center flex-col'>
           <div className="text-sm minxl:text-xl mb-8 minmd:mb-10 mx-0 minxl:mx-auto  text-center">
             <p>
               {`Learn how to claim ${(notAvailable || currentURIBlocked) ? 'a' : 'this'} profile for your own by visiting either NFT.com or our Support knowledge base.`}
@@ -63,8 +76,8 @@ export function UnmintedOrUnavailableProfile(props: UnmintedOrUnavailableProps) 
             <LinksToSection isAddressOwner={false} />
           </div>
         </div>
-      </main>
-      <div className='w-full items-center bottom-0 mt-16 minmd:mt-0'>
+      </div>
+      <div className='w-full mt-16 minmd:mt-0'>
         <Footer />
       </div>
     </div>
