62f4b1d3c52ace63c99543ba4de8288de88418d2
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-08-24 15:33:49 -0700

    fix test

--- cypress/e2e/Settings/Settings.cy.ts
+++ cypress/e2e/Settings/Settings.cy.ts
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Settings Page', () => {
-  it('Navigates to home page when no profile/wallet connected', async () => {
+  it('Navigates to home page when no profile/wallet connected', () => {
     cy.visit('/app/settings');
     cy.wait(5000);
     cy.url().should('not.eq', '/app/settings');
