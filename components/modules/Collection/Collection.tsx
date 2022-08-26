e6dfaba197ed0359f14a3b984b59a5f4f75476a8
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-08-26 11:34:53 -0700

    Added initial styling to collection page + new collection info component

--- components/modules/Collection/Collection.tsx
+++ components/modules/Collection/Collection.tsx
@@ -11,10 +11,12 @@ import { isNullOrEmpty, shortenAddress } from 'utils/helpers';
 import { tw } from 'utils/tw';
 import { getTypesenseInstantsearchAdapterRaw } from 'utils/typeSenseAdapters';
 
+import { CollectionInfo } from './CollectionInfo';
+
 import { Tab } from '@headlessui/react';
 import router from 'next/router';
-import CopyIcon from 'public/arrow_square_out.svg';
 import { useEffect, useState } from 'react';
+import { ExternalLink as LinkIcon } from 'react-feather';
 import useSWR from 'swr';
 import { useNetwork } from 'wagmi';
 
@@ -29,6 +31,7 @@ export function Collection(props: CollectionProps) {
   const [collectionNfts, setCollectionNfts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [found, setFound] = useState(0);
+  const [descriptionExpanded, setDescriptionExpanded] = useState(false);
   const prevVal = usePrevious(currentPage);
   const { data: collectionData } = useCollectionQuery(String( chain ?? getEnv(Doppler.NEXT_PUBLIC_CHAIN_ID)), props.contract?.toString());
   const { data: imgUrl } = useSWR('imageurl', async() => {
@@ -89,17 +92,86 @@ export function Collection(props: CollectionProps) {
     <>
       <div className="mt-20">
         <BannerWrapper
-          imageOverride={imgUrl}/>
+          imageOverride={imgUrl}
+          isCollection
+        />
+      </div>
+      <div className='font-grotesk px-4 mt-9 max-w-nftcom mx-auto'>
+        <h2 className="text-3xl font-bold">
+          {/* {collectionNfts[0].document.contractName} */}
+            NFT.com Genesis Key
+        </h2>
+        <div className="grid grid-cols-2 gap-4 mt-6 minlg:w-1/2">
+          <div className='flex flex-col'>
+            <p className='text-[10px] uppercase text-[#6F6F6F] font-bold'>Creator</p>
+            <div className='flex mt-1 text-[#B59007] font-medium font-mono'>
+              {/* get creator address */}
+              <span>{shortenAddress(props.contract?.toString(), 4)}</span>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={`https://etherscan.io/address/${props.contract?.toString()}`}
+                className='font-bold underline tracking-wide'
+              >
+                <LinkIcon size={20} className='ml-1' />
+              </a>
+            </div>
+          </div>
+
+          <div className='flex flex-col'>
+            <p className='text-[10px] uppercase text-[#6F6F6F] font-bold'>Contract Address</p>
+            <div className='flex mt-1 text-[#B59007] font-medium font-mono'>
+              <span>{shortenAddress(props.contract?.toString(), 4)}</span>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={`https://etherscan.io/address/${props.contract?.toString()}`}
+                className='font-bold underline tracking-wide'
+              >
+                <LinkIcon size={20} className='ml-1' />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className='font-grotesk mt-6 text-black flex flex-col minlg:flex-row'>
+          <div className='minmd:w-1/2'>
+            <h3 className='text-[#6F6F6F] font-semibold'>
+            Description
+            </h3>
+            <div className='mt-1 minlg:pr-4'>
+              {descriptionExpanded ?
+                <>
+                  <p className='inline'>
+                  War pinnacle gains strong disgust. Good god society overcome overcome philosophy battle. Deceptions inexpedient enlightenment victorious grandeur pinnacle value dead ultimate free of contradict intentions chaos. Pinnacle free strong intentions play value law against abstract transvaluation depths strong oneself.
+                  </p>
+                  <p className='text-[#B59007] font-bold inline ml-1 hover:cursor-pointer' onClick={() => setDescriptionExpanded(false)}>Show less</p>
+                </>
+                :
+                <>
+                  <p className='inline'>
+                  War pinnacle gains strong disgust. Good god society overcome overcome philosophy battle.
+                  </p>
+                  <p className='text-[#B59007] font-bold inline ml-1 hover:cursor-pointer' onClick={() => setDescriptionExpanded(true)}>
+                  Show more
+                  </p>
+                </>
+              }
+            </div>
+          </div>
+          <div className='mb-10 w-full minlg:w-1/2'>
+            <CollectionInfo />
+          </div>
+        </div>
       </div>
       <div className={tw(
-        'pt-7 px-8 minmd:px-[5%] minxl:mx-auto pb-16 w-full',
+        'px-4 pb-16 w-full',
       )}
       >
         {collectionNfts.length > 0 ?
           <>
             {getEnvBool(Doppler.NEXT_PUBLIC_ANALYTICS_ENABLED) &&
             <Tab.Group onChange={(index) => {setSelectedTab(tabs[index]);}}>
-              <Tab.List className="flex space-x-1 rounded-3xl bg-[#F6F6F6] font-grotesk">
+              <Tab.List className="flex space-x-1 rounded-3xl bg-[#F6F6F6] font-grotesk mb-6">
                 {Object.keys(tabs).map((tab) => (
                   <Tab
                     key={tab}
@@ -119,15 +191,6 @@ export function Collection(props: CollectionProps) {
             }
             {selectedTab === 'NFTs' &&
             <>
-              <div className="font-grotesk font-black text-black text-4xl max-w-nftcom minxl:mx-auto">{collectionNfts[0].document.contractName}</div>
-              <div className="mb-7 text-4xl flex items-center font-medium text-copy-size text-[#6F6F6F] max-w-nftcom minxl:mx-auto">
-                <span>{shortenAddress(props.contract?.toString())}</span>
-                <a
-                  target="_blank"
-                  rel="noreferrer" href={`https://etherscan.io/address/${props.contract?.toString()}`} className='font-bold underline tracking-wide'>
-                  <CopyIcon />
-                </a>
-              </div>
               <div className="grid grid-cols-2 minmd:grid-cols-3 minlg:grid-cols-4 gap-4 max-w-nftcom minxl:mx-auto ">
                 {collectionNfts.map((nft, index) => {
                   return (
---
+++ components/modules/Collection/CollectionInfo.tsx
@@ -0,0 +1,82 @@
+import { tw } from 'utils/tw';
+
+import { CaretDown, CaretUp, Info } from 'phosphor-react';
+import { useState } from 'react';
+export type CollectionInfoProps = {
+  data?: any[]
+}
+
+export const CollectionInfo = ({ data }: CollectionInfoProps) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  return (
+    <div className="bg-[#F8F8F8] px-6 pt-6 pb-4 minmd:pb-6 rounded-[10px] mt-10 minlg:mt-0 font-grotesk grid minmd:grid-cols-2 minmd:gap-16 minxl:gap-12">
+      <div className='flex flex-col'>
+        <div className="flex justify-between h-10">
+          <p className='self-center font-medium text-[#6F6F6F] flex items-center'><Info className='mr-1' />Floor</p>
+          <div className='text-right relative'>
+            <p className='font-medium'>12 ETH</p>
+            <p className='text-[#B6B6B6] text-xs font-medium absolute right-0'>$19K</p>
+          </div>
+        </div>
+        <div className="flex justify-between mt-3">
+          <p className='font-medium text-[#6F6F6F] flex items-center'><Info className='mr-1' />Volume</p>
+          <p className='self-center font-medium'>12K</p>
+        </div>
+        <div className="flex justify-between mt-4">
+          <p className='font-medium text-[#6F6F6F] flex items-center'><Info className='mr-1' />Supply</p>
+          <p className='font-medium'>10K</p>
+        </div>
+        <div className="flex justify-between mt-4">
+          <p className='font-medium text-[#6F6F6F] flex items-center'><Info className='mr-1' />Owners</p>
+          <p className='font-medium'>976</p>
+        </div>
+      </div>
+      <div className={tw(
+        isExpanded ? 'block' : 'hidden minmd:block'
+      )}>
+        <div className="flex justify-between mt-4 minmd:mt-0">
+          <p className='font-medium text-[#6F6F6F] flex items-center'><Info className='mr-1' />S/O Ratio</p>
+          <p className='font-medium'>80%</p>
+        </div>
+        <div className="flex justify-between h-10 mt-3">
+          <p className='self-center font-medium text-[#6F6F6F] flex items-center minlg:hidden'><Info className='mr-1' />Market Cap</p>
+          <p className='self-center font-medium text-[#6F6F6F] hidden items-center minlg:flex'><Info className='mr-1' />Mkt Cap</p>
+          <div className='text-right relative'>
+            <p className='font-medium'>120 ETH</p>
+            <p className='text-[#B6B6B6] text-xs font-medium absolute right-0'>$190K</p>
+          </div>
+        </div>
+        <div className="flex justify-between h-10 mt-3 minmd:mt-2">
+          <p className='self-center font-medium text-[#6F6F6F] block minlg:hidden'>Average Price</p>
+          <p className='self-center font-medium text-[#6F6F6F] hidden items-center minlg:flex'><Info className='mr-1' />Avg Price</p>
+          <div className='text-right relative'>
+            <p className='font-medium'>3 ETH</p>
+            <p className='text-[#B6B6B6] text-xs font-medium absolute right-0'>$5K</p>
+          </div>
+        </div>
+        <div className="flex justify-between mt-3 minmd:mt-2">
+          <p className='font-medium text-[#6F6F6F] block minlg:hidden'>Contract Type</p>
+          <p className='self-center font-medium text-[#6F6F6F] hidden items-center minlg:flex'><Info className='mr-1' />Type</p>
+          <p className='font-medium'>ERC1155</p>
+        </div>
+      </div>
+      
+      <div className='block minmd:hidden'>
+        {!isExpanded
+          ? (
+            <div className='flex w-full justify-center items-center mt-4'>
+              <p className='text-[#B59007] font-medium' onClick={() => setIsExpanded(true)}>Expand</p>
+              <CaretDown className='ml-1' weight='fill' color='#B59007' />
+            </div>
+          )
+          : (
+            <div className='flex w-full justify-center items-center mt-4'>
+              <p className='text-[#B59007] font-medium' onClick={() => setIsExpanded(false)}>Collapse</p>
+              <CaretUp className='ml-1' weight='fill' color='#B59007' />
+            </div>
+          )}
+      </div>
+    </div>
+    
+  );
+};
