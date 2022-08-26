434b37470c32648c747752ee1719bc01321c2c79
just-toby <ejd4@protonmail.com>
2022-08-26 09:46:20 -0700

    fix: pagelayout fix

--- pages/app/buy.tsx
+++ pages/app/buy.tsx
@@ -1,4 +1,4 @@
-import { PageWrapper } from 'components/layouts/PageWrapper';
+import DefaultLayout from 'components/layouts/DefaultLayout';
 import { PurchaseCheckout } from 'components/modules/Checkout/PurchaseCheckout';
 import NotFoundPage from 'pages/404';
 import { Doppler, getEnv, getEnvBool } from 'utils/env';
@@ -12,17 +12,24 @@ export default function BuyPage() {
     return <NotFoundPage />;
   }
 
-  return <PageWrapper>
-    <div className={tw(
-      'w-full h-full pt-20 flex flex-col items-center text-primary-txt dark:text-primary-txt-dk',
-      getEnvBool(Doppler.NEXT_PUBLIC_FORCE_DARK_MODE) ? 'bg-white' : 'bg-white dark:bg-footer-bg-dk'
-    )}>
-      <div className='max-w-nftcom w-full flex flex-col items-center my-8'>
-        <div className='w-full flex px-8'>
-          <h1 className='text-xl font-bold underline'>Buy Summary</h1>
-        </div>
-        <PurchaseCheckout />
+  return;
+  <div className={tw(
+    'w-full h-full pt-20 flex flex-col items-center text-primary-txt dark:text-primary-txt-dk',
+    getEnvBool(Doppler.NEXT_PUBLIC_FORCE_DARK_MODE) ? 'bg-white' : 'bg-white dark:bg-footer-bg-dk'
+  )}>
+    <div className='max-w-nftcom w-full flex flex-col items-center my-8'>
+      <div className='w-full flex px-8'>
+        <h1 className='text-xl font-bold underline'>Buy Summary</h1>
       </div>
+      <PurchaseCheckout />
     </div>
-  </PageWrapper>;
+  </div>;
 }
+
+BuyPage.getLayout = function getLayout(page) {
+  return (
+    <DefaultLayout>
+      { page }
+    </DefaultLayout>
+  );
+};
