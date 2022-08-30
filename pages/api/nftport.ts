a073a23cd90d2678e6759051d8445b91bd0226b7
Anthony <anthony@immutableholdings.com>
2022-08-30 14:47:44 -0600

    reverting apiUrl with string conversion.

---
+++ pages/api/nftport.ts
@@ -0,0 +1,34 @@
+import { isNullOrEmpty } from 'utils/helpers';
+
+import { withSentry } from '@sentry/nextjs';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const nftportHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const contractAddress = req.query['contractAddress'];
+  if (isNullOrEmpty(contractAddress)) {
+    res.status(400).json({ message: 'nftport handler: Invalid Arguments' });
+  }
+
+  const apiUrl = new URL(`https://api.nftport.xyz/v0/transactions/stats/${contractAddress}?chain=ethereum`);
+
+  try {
+    const result = await fetch(apiUrl.toString(), {
+      method: 'GET',
+      headers: {
+        'Authorization': process.env.NFTPORT_KEY,
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json());
+    res.status(200).json( result );
+  } catch (e) {
+    res.status(500).json({ message: 'ETH RPC: error' });
+  }
+};
+
+export default withSentry(nftportHandler);
+
+export const config = {
+  api: {
+    externalResolver: true,
+  },
+};
