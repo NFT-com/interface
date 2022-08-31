04f8a4dde98db6fcba17743aad60fb760f449dee
Anthony <anthony@immutableholdings.com>
2022-08-31 14:56:39 -0600

    further analytics work. (#361)
    * further analytics work.
    
    * Update nftport.ts
    
    * address specific feedback from PR
    
    * remove properties rarity % from NFTDetail card
    
    * remove tooltip hover on chain info for nft
    
    Co-authored-by: just toby <66155195+just-toby@users.noreply.github.com>
    Co-authored-by: Gavin <gmaijoe@users.noreply.github.com>

--- components/modules/NFTDetail/NFTDetail.tsx
+++ components/modules/NFTDetail/NFTDetail.tsx
@@ -55,9 +55,9 @@ export const NFTDetail = (props: NFTDetailProps) => {
   }, [props, refreshNft, refreshNftOrders]);
   
   return (
-    <div className="flex flex-col w-screen items-center" id="NFTDetailContainer" key={props.nft?.id}>
+    <div className="flex flex-col w-screen" id="NFTDetailContainer" key={props.nft?.id}>
       {props.nft?.metadata?.imageURL &&
-      <div className='flex w-full bg-[#F0F0F0] justify-around minmd:py-[22px] minmd:px-[42px] minlg:py-[119px] minxl:px-[350px]'>
+      <div className='flex w-full bg-[#F0F0F0] justify-around minmd:py-[22px] minmd:px-[42px]'>
         <div className="flex w-full max-w-[600px] h-full aspect-square object-contain drop-shadow-lg rounded-lg">
           <video
             autoPlay
@@ -72,7 +72,7 @@ export const NFTDetail = (props: NFTDetailProps) => {
       </div>
       }
       <div className={tw(
-        'flex items-center w-full mt-8 py-4 px-4 justify-between minmd:px-[17.5px] minlg:px-[128px]',
+        'flex items-center w-full mt-8 py-4 px-4 justify-between minmd:px-[17.5px] minlg:px-[128px] minxl:px-4',
       )}>
         <div className='flex flex-col'>
           <div className="whitespace-nowrap text-lg font-normal font-grotesk leading-6 tracking-wide text-[#1F2127]">
@@ -95,7 +95,7 @@ export const NFTDetail = (props: NFTDetailProps) => {
           </div>
         </div>
       </div>
-      <div className='flex flex-row items-center w-full h-full py-4 px-4 minmd:px-[17.5px] minlg:px-[128px]'>
+      <div className='flex flex-row items-center w-full minxl:w-1/2 h-full py-4 px-4 minmd:px-[17.5px] minlg:px-[128px] minxl:px-4'>
         {//todo: show collection owner pic
         }
         <div className='flex flex-col h-full'>
@@ -107,7 +107,7 @@ export const NFTDetail = (props: NFTDetailProps) => {
             />
           </div>}
         </div>
-        <div className='flex flex-col w-1/2 h-full minlg:-mr-40'>
+        <div className='flex flex-col w-1/2 h-full minlg:-mr-40 minxl:-mr-0'>
           <div className='flex flex-col h-full'>
             <span className='flex flex-col pl-[11px] -mt-2 font-grotesk text-[10px] not-italic font-bold leading-5 tracking-widest text-[#6F6F6F]'>
             CREATOR
--- pages/api/nftport.ts
+++ pages/api/nftport.ts
@@ -4,12 +4,15 @@ import { withSentry } from '@sentry/nextjs';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const nftportHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const category = req.query['category'];
   const contractAddress = req.query['contractAddress'];
+  const tokenId = req.query['tokenId'] ?? '';
+  const type = req.query['type'] ?? '';
   if (isNullOrEmpty(contractAddress)) {
     res.status(400).json({ message: 'nftport handler: Invalid Arguments' });
   }
 
-  const apiUrl = new URL(`https://api.nftport.xyz/v0/transactions/stats/${contractAddress}?chain=ethereum`);
+  const apiUrl = new URL(`https://api.nftport.xyz/v0/transactions/${category}/${contractAddress}${`/${tokenId}` ?? ''}?chain=ethereum${`&type=${type}`?? ''}`);
 
   try {
     const result = await fetch(apiUrl.toString(), {
