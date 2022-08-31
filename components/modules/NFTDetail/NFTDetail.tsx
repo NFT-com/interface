f18107346ac0d97695d153d7f9228ed531aadcee
Gavin <gmaijoe@users.noreply.github.com>
2022-08-31 19:24:45 -0400

    Update NFTDetail.tsx

--- components/modules/NFTDetail/NFTDetail.tsx
+++ components/modules/NFTDetail/NFTDetail.tsx
@@ -8,7 +8,7 @@ import { useNftProfileTokens } from 'hooks/useNftProfileTokens';
 import { Doppler,getEnv } from 'utils/env';
 import { isNullOrEmpty, processIPFSURL, shortenAddress } from 'utils/helpers';
 import { tw } from 'utils/tw';
-import { BigNumber, utils } from 'ethers';
+import { utils } from 'ethers';
 import Link from 'next/link';
 
 import { useRouter } from 'next/router';
