78fdcd8f51bd8307e1a04256b6c449130fb5cb6b
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-09-01 10:32:48 -0700

    wip: updated asset page functionality and styling

--- components/modules/Assets/AssetTableRow.tsx
+++ components/modules/Assets/AssetTableRow.tsx
@@ -1,32 +1,50 @@
+import { DropdownPickerModal } from 'components/elements/DropdownPickerModal';
 import { useCollectionQuery } from 'graphql/hooks/useCollectionQuery';
+import { useListingActivitiesQuery } from 'graphql/hooks/useListingActivitiesQuery';
+// import { useProfilesByDisplayedNft } from 'graphql/hooks/useProfilesByDisplayedNftQuery';
 import { Doppler, getEnv } from 'utils/env';
+import { filterNulls } from 'utils/helpers';
 import { tw } from 'utils/tw';
 
+import { BigNumber } from 'ethers';
+import Link from 'next/link';
 import { DotsThreeVertical } from 'phosphor-react';
 
-export default function AssetTableRow({ item, index }: any) {
+export default function AssetTableRow({ item, index, isVisibile, profile, onChange, isChecked }: any) {
   const { data: collectionData } = useCollectionQuery(String( getEnv(Doppler.NEXT_PUBLIC_CHAIN_ID)), item?.contract);
+  const { data: listings } = useListingActivitiesQuery(
+    item?.contract,
+    item?.tokenId,
+    String(getEnv(Doppler.NEXT_PUBLIC_CHAIN_ID))
+  );
+  // const { data: profiles } = useProfilesByDisplayedNft(
+  //   item?.contract,
+  //   item?.tokenId,
+  //   String(getEnv(Doppler.NEXT_PUBLIC_CHAIN_ID))
+  // );
   return (
     <tr
-      className={tw('cursor-pointer min-w-[5.5rem] h-20',
+      className={tw('min-w-[5.5rem] h-20',
         index > 0 && '',
-        index % 2 !== 0 && 'bg-[#F8F8F8]'
+        index % 2 === 0 && 'bg-[#F8F8F8]'
       )}
     >
       <td className="font-bold minmd:text-body text-sm leading-body" >
-        <div >
-          <input type='checkbox' />
+        <div className='flex justify-center'>
+          <input checked={isChecked} onChange={() => onChange(item)} className='border-2 border-[#6F6F6F]' type='checkbox' />
         </div>
       </td>
-      <td className="font-bold minmd:text-body text-sm leading-body" >
-        <div >
-          <p>{collectionData?.collection.name}</p>
-          <p>{item.metadata.name}</p>
-        </div>
+      <td className="font-bold text-body leading-body" >
+        <Link href={`/app/nft/${item?.contract}/${BigNumber.from(item?.tokenId).toNumber()}`}>
+          <div className='hover:cursor-pointer'>
+            <p className='uppercase text-[#6F6F6F] text-[10px]'>{collectionData?.collection.name}</p>
+            <p className='-mt-1 font-bold text-[#B59007]'>{item.metadata.name}</p>
+          </div>
+        </Link>
       </td>
       <td className="minmd:text-body text-sm leading-body" >
         <div >
-          <div>Listed</div>
+          {listings.length ? <p>Listed</p> : <p>—</p>}
         </div>
       </td>
       <td className="minmd:text-body text-sm leading-body" >
@@ -41,11 +59,29 @@ export default function AssetTableRow({ item, index }: any) {
       </td>
       <td className="minmd:text-body text-sm leading-body" >
         <div >
-          <div>/luc</div>
+          {isVisibile ? <Link href={`/${profile}`}><p className='text-[#B59007] font-bold hover:cursor-pointer'><span className='text-black'>/ </span>{profile}</p></Link> : <p>Hidden</p>}
         </div>
       </td>
       <td >
-        <DotsThreeVertical data-cy="ProfileDropdown" size={25} weight='fill' className='ml-2 hover:cursor-pointer text-black' />
+        <DropdownPickerModal
+          constrain
+          selectedIndex={0}
+          options={filterNulls([
+            {
+              label: 'List NFT',
+              onSelect: () => onChange(item),
+              icon: null,
+            },
+            {
+              label: 'Share on Twitter',
+              onSelect: () => window.open('https://twitter.com/share?url='+ encodeURIComponent(`${getEnv(Doppler.NEXT_PUBLIC_BASE_URL)}/app/nft/${item?.contract}/${BigNumber.from(item?.tokenId).toNumber()}`)+'&text='+document.title, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600'),
+              icon: null,
+            },
+          ])
+          }>
+          <DotsThreeVertical size={25} weight='fill' className='ml-2 hover:cursor-pointer text-black' />
+        </DropdownPickerModal>
+       
       </td>
     </tr>
   );
--- components/modules/Assets/AssetsPage.tsx
+++ components/modules/Assets/AssetsPage.tsx
@@ -1,55 +1,76 @@
-import { ProfileContext, ProfileContextProvider } from 'components/modules/Profile/ProfileContext';
+import { ProfileContext } from 'components/modules/Profile/ProfileContext';
 import { useUser } from 'hooks/state/useUser';
 
 import AssetTableRow from './AssetTableRow';
 
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 export default function AssetsPages() {
   const { getCurrentProfileUrl }= useUser();
   const selectedProfile = getCurrentProfileUrl();
+  const [selectedAssets, setSelectedAssets] = useState([]);
   const {
     allOwnerNfts,
-    // allOwnerNftCount,
-    // loadMoreNfts,
+    publiclyVisibleNfts,
   } = useContext(ProfileContext);
-  console.log('🚀 ~ file: Assets.tsx ~ line 15 ~ Assets ~ allOwnerNfts', allOwnerNfts);
+
+  const onChangeHandler = (asset) => {
+    const index = selectedAssets.findIndex((item) => item.id === asset.id);
+    if(index !== -1) {
+      setSelectedAssets(selectedAssets.filter((_, i) => i !== index));
+    } else {
+      setSelectedAssets([...selectedAssets, asset]);
+    }
+  };
+
+  const selectAllHandler = () => {
+    if(selectedAssets?.length === allOwnerNfts?.length){
+      setSelectedAssets([]);
+    } else {
+      setSelectedAssets(allOwnerNfts);
+    }
+  };
+
   return (
-    <ProfileContextProvider
-      key={selectedProfile}
-      profileURI={selectedProfile}
-    >
-      <div className='min-h-screen flex flex-col justify-between overflow-x-hidden pt-28 px-4 font-grotesk'>
-        <div className='w-full max-w-nftcom mx-auto'>
-          <h2 className='font-bold text-black text-[40px] mb-6'>
-            <span className='text-[#F9D963]'>/</span>
+    <div className='min-h-screen flex flex-col justify-between overflow-x-hidden pt-28 px-4 font-grotesk'>
+      <div className='w-full max-w-nftcom mx-auto relative'>
+        <h2 className='font-bold text-black text-[40px] mb-6'>
+          <span className='text-[#F9D963]'>/</span>
             My Assets
-          </h2>
-          <div className='w-full bg-[#F8F8F8] text-[#6F6F6F] font-grotesk font-bold p-2 rounded-[20px] text-center'>
+        </h2>
+        {selectedAssets.length ?
+          <div className='w-full minlg:w-[200px] bg-[#F9D963] text-[#1F2127] font-grotesk font-bold p-2 rounded-[20px] text-center'>
+            <p>List NFTs</p>
+          </div>
+          :
+          <div className='w-full minlg:w-[200px] bg-[#F8F8F8] text-[#6F6F6F] font-grotesk font-bold p-2 rounded-[20px] text-center'>
             <p>Select NFTs</p>
           </div>
-        
-          <table className="min-w-full border-x-0 mt-9">
-            <thead>
-              <tr className="text-body leading-body font-body text-[#7F7F7F]">
-                <th scope="col" className="flex items-center minmd:text-body text-sm leading-body font-body"></th>
-                <th scope="col" className='text-left minmd:text-body text-sm leading-body font-body'>Name</th>
-                <th scope="col" className='text-left minmd:text-body text-sm leading-body font-body'>Status</th>
-                <th scope="col" className='text-left minmd:text-body text-sm leading-body font-body'>Purchased Price</th>
-                <th scope="col" className='text-left minmd:text-body text-sm leading-body font-body'>USD Value</th>
-                <th scope="col" className='text-left minmd:text-body text-sm leading-body font-body'>Profile</th>
-                <th scope="col" className="flex items-center minmd:text-body text-sm leading-body font-body"></th>
+        }
+        <div className="overflow-x-auto my-8 font-grotesk rounded-md p-4">
+          <table className="border-collapse table-auto w-full">
+            <thead className='text-[#6F6F6F] text-sm font-medium leading-6 p-4'>
+              <tr className='p-4 pt-0 pb-3 text-left ...'>
+                <th className='text-[#6F6F6F] text-sm font-medium leading-6  flex items-center h-[30px] w-full justify-center mr-4'>
+                  <input checked={allOwnerNfts?.length && selectedAssets?.length === allOwnerNfts?.length} onChange={() => selectAllHandler()} className='border-2 border-[#6F6F6F]' type='checkbox' />
+                </th>
+                <th className='text-[#6F6F6F] text-sm font-medium leading-6 pb-4'>Name</th>
+                <th className='text-[#6F6F6F] text-sm font-medium leading-6 pb-4'>Status</th>
+                <th className='text-[#6F6F6F] text-sm font-medium leading-6 pb-4'>Purchased Price</th>
+                <th className='text-[#6F6F6F] text-sm font-medium leading-6 pb-4'>USD Value</th>
+                <th className='text-[#6F6F6F] text-sm font-medium leading-6 pb-4'>Profile</th>
               </tr>
             </thead>
-            <tbody className="bg-always-white">
+            <tbody className='p-4'>
               {allOwnerNfts?.map((item, i) => (
-                <AssetTableRow key={i} item={item} index={i} />
+                <AssetTableRow isChecked={selectedAssets.some((i) => i.tokenId === item.tokenId)} onChange={onChangeHandler} key={item.id} item={item} index={i} isVisibile={publiclyVisibleNfts.some((i) => i.id === item.id)} profile={selectedProfile} />
               ))}
             </tbody>
           </table>
+
         </div>
       </div>
-    </ProfileContextProvider>
+    </div>
   );
 }
 
