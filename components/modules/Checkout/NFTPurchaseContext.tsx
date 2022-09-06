9386107219d5991620af2a38d18e4d8b717dd83e
just-toby <ejd4@protonmail.com>
2022-09-06 14:53:26 -0700

    wip fix contract integration

--- components/modules/Checkout/NFTPurchaseContext.tsx
+++ components/modules/Checkout/NFTPurchaseContext.tsx
@@ -105,74 +105,104 @@ export function NFTPurchaseContextProvider(
   }, [toBuy]);
 
   const buyAll = useCallback(async () => {
-    if (toBuy.every(purchase => purchase.currency === NULL_ADDRESS)) {
-      const orderDetailsWitEth = filterNulls([
-        // Looksrare orders are given individually
-        ...(toBuy?.filter(purchase => purchase?.protocol === ExternalProtocol.LooksRare)?.map(looksrarePurchase => {
-          return getLooksrareHex(
-            aggregator.address,
-            looksrarePurchase?.protocolData as LooksrareProtocolData,
-            looksrareExchange,
-            BigNumber.from(looksrarePurchase?.price).toString()
-          );
-        }) ?? []),
-        // Seaport orders are combined
-        toBuy?.find(purchase => purchase.protocol === ExternalProtocol.Seaport) != null ?
-          getSeaportHex(
-            ethers.utils.getAddress(currentAddress),
-            toBuy?.filter(purchase => purchase?.protocol === ExternalProtocol.Seaport)?.map(purchase => purchase?.protocolData as SeaportProtocolData),
-            toBuy?.filter(purchase => purchase?.protocol === ExternalProtocol.Seaport)?.map(purchase => BigNumber.from(purchase?.price)), // wei value
-          ) :
-          null
-      ]);
-      const totalWei = toBuy.reduce((sum, purchase) => sum.add(purchase.price), BigNumber.from(0));
-      const tx = await aggregator.batchTradeWithETH(
-        orderDetailsWitEth,
-        [],
-        { value: totalWei }
-      ).catch(() => {
-        return null;
-      });
-      if (tx) {
-        return await tx.wait(1).then(() => true).catch(() => false);
-      }
-      return false;
-    } else if (toBuy.every(purchase => getByContractAddress(purchase.currency) != null)) {
-      const tokenAmounts = {
-        tokenAddrs: toBuy?.map(purchase => purchase?.currency),
-        amounts: toBuy?.map(purchase => BigNumber.from(purchase?.price ?? 0))
-      };
-  
-      const orderDetails = filterNulls([
-        // Looksrare orders are given individually
-        ...(toBuy?.filter(purchase => purchase?.protocol === ExternalProtocol.LooksRare)?.map(looksrarePurchase => {
-          return getLooksrareHex(aggregator.address, looksrarePurchase?.protocolData as LooksrareProtocolData, looksrareExchange, '0');
-        }) ?? []),
-        // Seaport orders are combined
-        toBuy?.find(purchase => purchase.protocol === ExternalProtocol.Seaport) != null ?
-          getSeaportHex(
-            ethers.utils.getAddress(currentAddress),
-            toBuy?.filter(purchase => purchase?.protocol === ExternalProtocol.Seaport)?.map(purchase => purchase?.protocolData as SeaportProtocolData),
-            toBuy?.filter(purchase => purchase?.protocol === ExternalProtocol.Seaport)?.map(() => BigNumber.from(0)), // wei values
-          ) :
-          null
-      ]);
-      
-      const tx = await aggregator.batchTrade(
-        tokenAmounts,
-        orderDetails,
-        [] // dustTokens
-      ).catch(() => {
-        return null;
-      });
-      if (tx) {
-        return await tx.wait(1).then(() => true).catch(() => false);
-      }
-      return false;
-    } else {
-      // todo: support combinations of ETH and ERC20, or show error message if a currency is not supported
-      return false;
+    const tokenAmounts = {
+      tokenAddrs: toBuy?.map(purchase => purchase?.currency),
+      amounts: toBuy?.map(purchase => BigNumber.from(purchase?.price ?? 0))
+    };
+    const orderDetails = filterNulls([
+      // Looksrare orders are given individually
+      ...(toBuy?.filter(purchase => purchase?.protocol === ExternalProtocol.LooksRare)?.map(looksrarePurchase => {
+        return getLooksrareHex(aggregator.address, looksrarePurchase?.protocolData as LooksrareProtocolData, looksrareExchange, '0');
+      }) ?? []),
+      // Seaport orders are combined
+      toBuy?.find(purchase => purchase.protocol === ExternalProtocol.Seaport) != null ?
+        getSeaportHex(
+          ethers.utils.getAddress(currentAddress),
+          toBuy?.filter(purchase => purchase?.protocol === ExternalProtocol.Seaport)?.map(purchase => purchase?.protocolData as SeaportProtocolData),
+          toBuy?.filter(purchase => purchase?.protocol === ExternalProtocol.Seaport)?.map(() => BigNumber.from(0)), // eth values
+        ) :
+        null
+    ]);
+
+    // todo: update to supporte new batchTrade API
+    const tx = await aggregator.batchTrade(
+      tokenAmounts,
+      orderDetails,
+      [] // dustTokens
+    ).catch(() => {
+      return null;
+    });
+    if (tx) {
+      return await tx.wait(1).then(() => true).catch(() => false);
     }
+    // if (toBuy.every(purchase => purchase.currency === NULL_ADDRESS)) {
+    //   const orderDetailsWitEth = filterNulls([
+    //     // Looksrare orders are given individually
+    //     ...(toBuy?.filter(purchase => purchase?.protocol === ExternalProtocol.LooksRare)?.map(looksrarePurchase => {
+    //       return getLooksrareHex(
+    //         aggregator.address,
+    //         looksrarePurchase?.protocolData as LooksrareProtocolData,
+    //         looksrareExchange,
+    //         BigNumber.from(looksrarePurchase?.price).toString()
+    //       );
+    //     }) ?? []),
+    //     // Seaport orders are combined
+    //     toBuy?.find(purchase => purchase.protocol === ExternalProtocol.Seaport) != null ?
+    //       getSeaportHex(
+    //         ethers.utils.getAddress(currentAddress),
+    //         toBuy?.filter(purchase => purchase?.protocol === ExternalProtocol.Seaport)?.map(purchase => purchase?.protocolData as SeaportProtocolData),
+    //         toBuy?.filter(purchase => purchase?.protocol === ExternalProtocol.Seaport)?.map(purchase => BigNumber.from(purchase?.price)), // wei value
+    //       ) :
+    //       null
+    //   ]);
+    //   const totalWei = toBuy.reduce((sum, purchase) => sum.add(purchase.price), BigNumber.from(0));
+    //   const tx = await aggregator.batchTradeWithETH(
+    //     orderDetailsWitEth,
+    //     [],
+    //     { value: totalWei }
+    //   ).catch(() => {
+    //     return null;
+    //   });
+    //   if (tx) {
+    //     return await tx.wait(1).then(() => true).catch(() => false);
+    //   }
+    //   return false;
+    // } else if (toBuy.every(purchase => getByContractAddress(purchase.currency) != null)) {
+    //   const tokenAmounts = {
+    //     tokenAddrs: toBuy?.map(purchase => purchase?.currency),
+    //     amounts: toBuy?.map(purchase => BigNumber.from(purchase?.price ?? 0))
+    //   };
+  
+    //   const orderDetails = filterNulls([
+    //     // Looksrare orders are given individually
+    //     ...(toBuy?.filter(purchase => purchase?.protocol === ExternalProtocol.LooksRare)?.map(looksrarePurchase => {
+    //       return getLooksrareHex(aggregator.address, looksrarePurchase?.protocolData as LooksrareProtocolData, looksrareExchange, '0');
+    //     }) ?? []),
+    //     // Seaport orders are combined
+    //     toBuy?.find(purchase => purchase.protocol === ExternalProtocol.Seaport) != null ?
+    //       getSeaportHex(
+    //         ethers.utils.getAddress(currentAddress),
+    //         toBuy?.filter(purchase => purchase?.protocol === ExternalProtocol.Seaport)?.map(purchase => purchase?.protocolData as SeaportProtocolData),
+    //         toBuy?.filter(purchase => purchase?.protocol === ExternalProtocol.Seaport)?.map(() => BigNumber.from(0)), // wei values
+    //       ) :
+    //       null
+    //   ]);
+      
+    //   const tx = await aggregator.batchTrade(
+    //     tokenAmounts,
+    //     orderDetails,
+    //     [] // dustTokens
+    //   ).catch(() => {
+    //     return null;
+    //   });
+    //   if (tx) {
+    //     return await tx.wait(1).then(() => true).catch(() => false);
+    //   }
+    //   return false;
+    // } else {
+    //   // todo: support combinations of ETH and ERC20, or show error message if a currency is not supported
+    //   return false;
+    // }
   }, [aggregator, currentAddress, getByContractAddress, looksrareExchange, toBuy]);
 
   return <NFTPurchasesContext.Provider value={{
