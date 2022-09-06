8a23575083d4448a2060edcf23e25cd4dfb10259
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-09-06 14:05:08 -0700

    added activity page

---
+++ components/modules/Activity/ActivityPage.tsx
@@ -0,0 +1,94 @@
+import { useActivitiesForAddressQuery } from 'graphql/hooks/useActivitiesForAddressQuery';
+import { useDefaultChainId } from 'hooks/useDefaultChainId';
+import { usePaginator } from 'hooks/usePaginator';
+import { filterNulls } from 'utils/helpers';
+
+import ActivityTableRow from './ActivityTableRow';
+
+import { useCallback, useEffect, useState } from 'react';
+import { useAccount } from 'wagmi';
+
+const ACTIVITY_LOAD_COUNT = 50;
+
+export default function ActivityPages() {
+  const {
+    nextPage,
+    afterCursor,
+    setTotalCount,
+    cachedTotalCount
+  } = usePaginator(ACTIVITY_LOAD_COUNT);
+  const [lastAddedPage, setLastAddedPage] = useState('');
+
+  const [activityData, setActivityData] = useState([]);
+  const { address: currentAddress } = useAccount();
+  const defaultChainId = useDefaultChainId();
+
+  const { data: loadedActivitiesNextPage } = useActivitiesForAddressQuery(
+    currentAddress,
+    defaultChainId,
+    {
+      first: ACTIVITY_LOAD_COUNT,
+      afterCursor
+    });
+
+  const loadMoreActivities = useCallback(() => {
+    nextPage(loadedActivitiesNextPage?.getActivities?.pageInfo?.lastCursor);
+  }, [loadedActivitiesNextPage?.getActivities?.pageInfo?.lastCursor, nextPage]);
+
+  useEffect(() => {
+    if (
+      loadedActivitiesNextPage?.getActivities?.items?.length > 0 &&
+      lastAddedPage !== loadedActivitiesNextPage?.getActivities?.pageInfo?.firstCursor
+    ) {
+      setActivityData([
+        ...activityData,
+        ...filterNulls(loadedActivitiesNextPage?.getActivities?.items)
+      ]);
+      setLastAddedPage(loadedActivitiesNextPage?.getActivities?.pageInfo?.firstCursor);
+      setTotalCount(loadedActivitiesNextPage?.getActivities?.totalItems);
+    } else {
+      setTotalCount(loadedActivitiesNextPage?.getActivities?.totalItems);
+    }
+  }, [lastAddedPage, setTotalCount, activityData, loadedActivitiesNextPage, afterCursor, loadedActivitiesNextPage?.getActivities?.items, currentAddress]);
+  
+  return (
+    <div className='flex flex-col justify-between pt-28 px-4 font-grotesk'>
+      <div className='w-full max-w-nftcom mx-auto relative'>
+        <h2 className='font-bold text-black text-[40px] mb-6'>
+          <span className='text-[#F9D963]'>/</span>
+          My Activity
+        </h2>
+        <div className="my-8 font-grotesk rounded-md pt-4 pb-8 overflow-x-auto">
+          <table className="border-collapse table-fixed w-full">
+            <thead className='text-[#6F6F6F] text-sm font-medium leading-6 p-4'>
+              <tr className='pt-0 pb-3 text-left ...'>
+                <th className='text-[#6F6F6F] text-sm font-medium leading-6 pl-8 pt-0 pb-3 minmd:pr-4 w-[200px]'>Name</th>
+                <th className='text-[#6F6F6F] text-sm font-medium leading-6 pb-4 pr-8 minmd:pr-4 w-[80px]'>Status</th>
+                <th className='text-[#6F6F6F] text-sm font-medium leading-6 pb-4 pr-8 minmd:pr-4 w-[100px]'>Marketplace</th>
+                <th className='text-[#6F6F6F] text-sm font-medium leading-6 pb-4 pr-8 minmd:pr-4 w-[100px]'>Price</th>
+                <th className='text-[#6F6F6F] text-sm font-medium leading-6 pb-4 pr-8 minmd:pr-4 w-[100px]'>USD Value</th>
+                <th className='text-[#6F6F6F] text-sm font-medium leading-6 pb-4 pr-8 minmd:pr-4 w-[100px]'>Time</th>
+                <th className='text-[#6F6F6F] text-sm font-medium leading-6 pb-4 pr-8 minmd:pr-4 w-[130px] minlg:w-[100px]'>From</th>
+                <th className='text-[#6F6F6F] text-sm font-medium leading-6 pb-4 pr-8 minmd:pr-4 w-[130px] minlg:w-[100px]'>To</th>
+              </tr>
+            </thead>
+            <tbody className='p-4'>
+              {activityData?.map((item, i) => (
+                <ActivityTableRow key={item.id} item={item} index={i} />
+              ))}
+            </tbody>
+          </table>
+          {!activityData || !activityData?.length && <p className='text-[#6F6F6F] flex items-center justify-center border h-[400px] rounded-[10px]'>No activity available</p>}
+
+          {cachedTotalCount > activityData?.length &&
+          <div className='w-full flex justify-center'>
+            <button onClick={() => loadMoreActivities()} className="bg-[#F9D963] font-bold tracking-normal hover:bg-[#fcd034] text-base text-black py-2 px-4 rounded-full focus:outline-none focus:shadow-outline w-full minlg:w-[250px] mt-6" type="button">
+            Load More
+            </button>
+          </div>
+          }
+        </div>
+      </div>
+    </div>
+  );
+}
