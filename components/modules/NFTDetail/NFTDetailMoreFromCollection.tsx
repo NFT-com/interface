9007d84cf2ebccdc7d85546c2e822ff86a5a216b
Anthony <anthony@immutableholdings.com>
2022-09-09 09:49:19 -0600

    horizontal scrolling nft detail 'featured' sections (#425)
    * horizontal scrolling nft detail
    
    * remove package-lock

--- components/modules/NFTDetail/NFTDetailMoreFromCollection.tsx
+++ components/modules/NFTDetail/NFTDetailMoreFromCollection.tsx
@@ -1,12 +1,23 @@
+/* eslint-disable import/no-unresolved */
+// Import Swiper styles
+import 'swiper/swiper.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+
 import { NFTCard } from 'components/elements/NFTCard';
 import { useFetchCollectionNFTs } from 'graphql/hooks/useFetchCollectionNFTs';
+import useWindowDimensions from 'hooks/useWindowDimensions';
 import { Doppler, getEnv } from 'utils/env';
 import { getChainIdString } from 'utils/helpers';
 import { tw } from 'utils/tw';
 
 import { useRouter } from 'next/router';
+import SwiperCore, { Autoplay, Navigation } from 'swiper/core';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import useSWR from 'swr';
 import { useNetwork } from 'wagmi';
+
+SwiperCore.use([Autoplay, Navigation]);
 
 export interface NFTDetailMoreFromCollectionProps {
   contract: string;
@@ -14,6 +25,7 @@ export interface NFTDetailMoreFromCollectionProps {
 }
 
 export function NFTDetailMoreFromCollection(props: NFTDetailMoreFromCollectionProps) {
+  const { width: screenWidth } = useWindowDimensions();
   const { fetchCollectionsNFTs } = useFetchCollectionNFTs();
   const router = useRouter();
   const { chain } = useNetwork();
@@ -32,30 +44,41 @@ export function NFTDetailMoreFromCollection(props: NFTDetailMoreFromCollectionPr
     return null;
   }
 
-  return <div className='flex flex-col w-full'>
+  return <div className='flex flex-col w-full px-[16px]'>
     <span className="text-2xl font-bold font-grotesk mb-2">More from collection</span>
-    <div className='flex py-2 snap-x overflow-x-auto sm:no-scrollbar h-full items-stretch'>
-      {data?.map((nft, index) => {
-        return (
-          <div className={tw(
-            'NftCollectionItem flex flex-col snap-always snap-center sm:no-scrollbar w-72 shrink-0 cursor-pointer mr-4 self-stretch',
-          )} key={index}>
-            <NFTCard
-              contractAddress={props.contract}
-              tokenId={nft.tokenId}
-              title={nft.metadata.name}
-              images={[nft.metadata.imageURL]}
-              collectionName={props.collectionName}
-              onClick={() => {
-                if (nft.metadata.name) {
-                  router.push(`/app/nft/${props.contract}/${nft.tokenId}`);
-                }
-              }}
-              customBorderRadius={'rounded-tl rounded-tr'}
-            />
-          </div>
-        );
-      })}
+    <div className='flex py-2 h-full items-stretch'>
+      <Swiper
+        slidesPerView={screenWidth < 600 ? 1 : screenWidth >= 600 && screenWidth < 900 ? 3 : 4}
+        centeredSlides={false}
+        loop={true}
+        autoplay={{
+          'delay': 3000,
+          'disableOnInteraction': false
+        }}
+        className="flex drop-shadow-2xl"
+      >
+        {data?.map((nft, index) => {
+          return (
+            <SwiperSlide className={tw(
+              'NftCollectionItem flex flex-col w-72 shrink-0 cursor-pointer self-stretch',
+            )} key={nft?.id ?? index}>
+              <NFTCard
+                contractAddress={props.contract}
+                tokenId={nft.tokenId}
+                title={nft.metadata.name}
+                images={[nft.metadata.imageURL]}
+                collectionName={props.collectionName}
+                onClick={() => {
+                  if (nft.metadata.name) {
+                    router.push(`/app/nft/${props.contract}/${nft.tokenId}`);
+                  }
+                }}
+                customBorderRadius={'rounded-tl rounded-tr'}
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   </div>;
 }
