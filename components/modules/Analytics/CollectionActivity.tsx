4779506f45be2ebd47a4430f1fe0a6c34b5d5998
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-09-14 09:16:00 -0700

    consolidated duplicate logic for detail pages table rows

--- components/modules/Analytics/CollectionActivity.tsx
+++ components/modules/Analytics/CollectionActivity.tsx
@@ -1,10 +1,9 @@
 
 import { useGetTxByContractQuery } from 'graphql/hooks/useGetTxByContractQuery';
 import { useDefaultChainId } from 'hooks/useDefaultChainId';
-import { shorten, shortenAddress } from 'utils/helpers';
-import { tw } from 'utils/tw';
 
-import moment from 'moment';
+import DetailPageTableRow from './DetailPageTableRow';
+
 import { useEffect, useState } from 'react';
 
 export type CollectionActivityProps = {
@@ -24,16 +23,6 @@ export const CollectionActivity = ({ contract }: CollectionActivityProps) => {
       }
     }}, [defaultChainId, collectionData, txs]);
 
-  const formatMarketplaceName = (name) => {
-    if(name === 'opensea'){
-      return 'OpenSea';
-    } else if (name === 'looksrare') {
-      return 'LooksRare';
-    } else {
-      return name;
-    }
-  };
-
   return (
     <div className="overflow-x-auto my-8 font-grotesk rounded-md p-4">
       {!collectionData && <p className='text-[#6F6F6F] flex items-center justify-center border h-[200px] rounded-[10px]'>No activity available</p>}
@@ -52,30 +41,7 @@ export const CollectionActivity = ({ contract }: CollectionActivityProps) => {
         </thead>
         <tbody className='p-4'>
           {collectionData?.map((tx , index) => (
-            <tr key={index} className={tw(
-              'font-normal text-base leading-6 text-[#1F2127] overflow-auto',
-              index % 2 === 0 && 'bg-[#F8F8F8]'
-            )}
-            >
-              <td className="font-normal text-base leading-6 text-[#1F2127] p-4 capitalize">{tx?.type || '—'}</td>
-              <td className="font-normal text-base leading-6 text-[#1F2127] p-4">{tx?.token_id?.length > 10 ? shorten(tx?.token_id, true) || '—' : tx?.token_id || '—'}</td>
-              <td className="font-normal text-base leading-6 text-[#1F2127] p-4">{shortenAddress(tx.transfer_from, 4) || '—'}</td>
-              <td className="font-normal text-base leading-6 text-[#1F2127] p-4">{shortenAddress(tx.transfer_to, 4) || '—'}</td>
-              <td className="font-normal text-base leading-6 text-[#1F2127] p-4 capitalize">{
-                formatMarketplaceName(tx.marketplace) || '—'
-              }
-              </td>
-              <td className="font-normal text-base leading-6 text-[#1F2127] p-4">
-                {moment.utc(tx.transaction_date).format('lll').toString() || '—'}
-              </td>
-              <td className="font-normal text-base leading-6 text-[#B59007] p-4">
-                <a
-                  target="_blank"
-                  rel="noreferrer" href={`https://etherscan.io/tx/${tx.transaction_hash}`} className='font-bold tracking-wide'>
-                  {shorten(tx?.transaction_hash, true) || '—'}
-                </a>
-              </td>
-            </tr>
+            <DetailPageTableRow key={tx?.id} tx={tx} index={index} />
           ))}
         </tbody>
       </table>
