78681639147104631f1c8bc849026ab7f30e3529
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-09-22 18:15:57 -0400

    fix: correct replacement

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -100,8 +100,8 @@ export const processIPFSURL = (image: Maybe<string>): Maybe<string> => {
     return prefix + image.slice(7);
   } else if (image.indexOf('https://ipfs.io/ipfs/') === 0) {
     return prefix + image.slice(21);
-  } else if (image.indexOf('https://ipfs.infura.io/') === 0) {
-    return prefix + image.slice(23);
+  } else if (image.indexOf('https://ipfs.infura.io/ipfs/') === 0) {
+    return prefix + image.slice(28);
   } else if (image.indexOf('https://gateway.pinata.cloud/ipfs/') === 0) {
     return prefix + image.slice(34);
   } else if (image.indexOf('https://infura-ipfs.io/ipfs/') === 0) {
