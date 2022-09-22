9d05770da3f53cb0a45b21046d1c4232e032f321
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-09-22 12:13:45 -0400

    fix: new

--- next.config.js
+++ next.config.js
@@ -87,7 +87,7 @@ const moduleExports = withTM({
       'images.ctfassets.net',
       'sandbox-nvf2t.netlify.app',
       'staging-raccoon.netlify.app',
-      'nft.com',
+      'www.nft.com',
       '5hi24d3w2gny6zrfhekqk6mv4e0cfois.lambda-url.us-east-1.on.aws', // image proxy layer 2
       'nftcom-dev-assets.s3.amazonaws.com'
     ],
