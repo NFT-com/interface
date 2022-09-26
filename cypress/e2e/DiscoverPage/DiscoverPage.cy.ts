fa94c6e02690b3ca73986f998e06942e6152df3c
just-toby <ejd4@protonmail.com>
2022-09-26 12:10:19 -0700

    update tests more

--- cypress/e2e/DiscoverPage/DiscoverPage.cy.ts
+++ cypress/e2e/DiscoverPage/DiscoverPage.cy.ts
@@ -3,26 +3,19 @@
 describe('discover page tests mobile', () => {
   beforeEach(() => {
     cy.viewport(550, 750);
-    cy.intercept('GET', '*search*').as('sercharResults');
-    cy.fixture('discover_results').then((json) => {
-      cy.visit('/app/discover/');
-
-      cy.wait('@sercharResults').its('response.statusCode').should('eq', 200);
-      cy.wait(1000);
-    });
-  });
-  xit('renders first 2 items', () => {
-    cy.fixture('discover_results').then(() => {
-      cy.contains('Discover').should('exist');
-      cy.get('.DiscoverCollectionItem').should('have.length', 2);
-    });
+    cy.visit('/app/discover/');
   });
 
+  // TODO (fernando): complete test
+  it('renders first 2 items', () => {
+    cy.contains('Discover').should('exist');
+    // cy.get('.DiscoverCollectionItem').should('have.length', 2);
+  });
+
+  // TODO (fernando): complete test
   xit('renders next 2 items after clicking load more button', () => {
-    cy.fixture('discover_results').then(() => {
-      cy.findByText('Load More').should('exist').click().then(() =>{
-        cy.get('.DiscoverCollectionItem').should('have.length', 4);
-      });
+    cy.findByText('Load More').should('exist').click().then(() =>{
+      cy.get('.DiscoverCollectionItem').should('have.length', 4);
     });
   });
 });
@@ -30,26 +23,19 @@ describe('discover page tests mobile', () => {
 describe('discover page tests tablet', () => {
   beforeEach(() => {
     cy.viewport(820, 1180);
-    cy.intercept('GET', '*search*').as('sercharResults');
-    cy.fixture('discover_results').then((json) => {
-      cy.visit('/app/discover/');
-
-      cy.wait('@sercharResults').its('response.statusCode').should('eq', 200);
-      cy.wait(1000);
-    });
-  });
-  xit('renders first 4 items', () => {
-    cy.fixture('discover_results').then(() => {
-      cy.contains('Discover').should('exist');
-      cy.get('.DiscoverCollectionItem').should('have.length', 4);
-    });
+    cy.visit('/app/discover/');
   });
 
+  // TODO (fernando): complete test
+  it('renders first 4 items', () => {
+    cy.contains('Discover').should('exist');
+    // cy.get('.DiscoverCollectionItem').should('have.length', 4);
+  });
+
+  // TODO (fernando): complete test
   xit('renders next 4 items after clicking load more button', () => {
-    cy.fixture('discover_results').then(() => {
-      cy.findByText('Load More').should('exist').click().then(() =>{
-        cy.get('.DiscoverCollectionItem').should('have.length', 8);
-      });
+    cy.findByText('Load More').should('exist').click().then(() =>{
+      cy.get('.DiscoverCollectionItem').should('have.length', 8);
     });
   });
 });
@@ -57,26 +43,19 @@ describe('discover page tests tablet', () => {
 describe('discover page tests laptop', () => {
   beforeEach(() => {
     cy.viewport(1024, 1915);
-    cy.intercept('GET', '*search*').as('sercharResults');
-    cy.fixture('discover_results').then((json) => {
-      cy.visit('/app/discover/');
-
-      cy.wait('@sercharResults').its('response.statusCode').should('eq', 200);
-      cy.wait(1000);
-    });
-  });
-  xit('renders first 6 items', () => {
-    cy.fixture('discover_results').then(() => {
-      cy.contains('Discover').should('exist');
-      cy.get('.DiscoverCollectionItem').should('have.length', 6);
-    });
+    cy.visit('/app/discover/');
   });
 
+  // TODO (fernando): complete test
+  it('renders first 6 items', () => {
+    cy.contains('Discover').should('exist');
+    // cy.get('.DiscoverCollectionItem').should('have.length', 6);
+  });
+
+  // TODO (fernando): complete test
   xit('renders next 6 items after clicking load more button', () => {
-    cy.fixture('discover_results').then(() => {
-      cy.findByText('Load More').should('exist').click().then(() =>{
-        cy.get('.DiscoverCollectionItem').should('have.length', 12);
-      });
+    cy.findByText('Load More').should('exist').click().then(() =>{
+      cy.get('.DiscoverCollectionItem').should('have.length', 12);
     });
   });
 });
