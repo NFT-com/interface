859c427a45cf65e3de891284241b4da0bf2d4816
just-toby <ejd4@protonmail.com>
2022-09-26 16:28:54 -0700

    more tests

--- cypress/component/Checkout/ListingCheckoutNftTableRow.cy.tsx
+++ cypress/component/Checkout/ListingCheckoutNftTableRow.cy.tsx
@@ -21,4 +21,52 @@ describe('ListingCheckoutNftTableRow', () => {
       </WagmiConfig>
     );
   });
+
+  it('expands and collapses', () => {
+    const callback = cy.stub();
+    cy.mount(
+      <WagmiConfig client={client}>
+        <ListingCheckoutNftTableRow
+          listing={{ nft: {} }}
+          onPriceChange={callback}
+        />
+      </WagmiConfig>
+    );
+    cy.get('.caretToggle').first().click().then(() => {
+      cy.get('.caretToggle').first().click();
+    });
+  });
+
+  it('accepts numerical input collapsed', () => {
+    const callback = cy.stub();
+    cy.mount(
+      <WagmiConfig client={client}>
+        <ListingCheckoutNftTableRow
+          listing={{ nft: {} }}
+          onPriceChange={callback}
+        />
+      </WagmiConfig>
+    );
+    cy.get('input').type('1').then(() => {
+      expect(callback).to.be.called;
+    });
+  });
+
+  it('accepts numerical input expanded', () => {
+    const callback = cy.stub();
+    cy.mount(
+      <WagmiConfig client={client}>
+        <ListingCheckoutNftTableRow
+          listing={{ nft: {} }}
+          onPriceChange={callback}
+        />
+      </WagmiConfig>
+    );
+    cy.get('.caretToggle').first().click().then(() => {
+      cy.get('input').first().type('1').then(() => {
+        expect(callback).to.be.called;
+      });
+      cy.get('input').last().type('2');
+    });
+  });
 });
