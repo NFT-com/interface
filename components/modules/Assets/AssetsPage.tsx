f4077f16ea23f0fe3dac50ef1a024682c4c47278
lucas <50972198+robitaille1@users.noreply.github.com>
2022-09-27 14:01:04 -0700

    Added pagination to assets page, created new query for only owned assets (#520)
    Co-authored-by: just toby <66155195+just-toby@users.noreply.github.com>

--- components/modules/Assets/AssetsPage.tsx
+++ components/modules/Assets/AssetsPage.tsx
@@ -1,22 +1,60 @@
 import Loader from 'components/elements/Loader';
 import { NFTListingsContext } from 'components/modules/Checkout/NFTListingsContext';
-import { ProfileContext } from 'components/modules/Profile/ProfileContext';
 import { Nft } from 'graphql/generated/types';
+import { useMyAssetsQuery } from 'graphql/hooks/useMyAssetsQuery';
 import { useFetchNftCollectionAllowance } from 'hooks/balances/useFetchNftCollectionAllowance';
 import { TransferProxyTarget } from 'hooks/balances/useNftCollectionAllowance';
+import { usePaginator } from 'hooks/usePaginator';
+import { filterNulls } from 'utils/helpers';
+import { tw } from 'utils/tw';
 
 import AssetTableRow from './AssetTableRow';
 
-import { useCallback, useContext, useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from 'react';
 import { PartialDeep } from 'type-fest';
+import { useAccount } from 'wagmi';
+
+const ASSET_LOAD_COUNT = 25;
 
 export default function AssetsPages() {
+  const { address: currentAddress } = useAccount();
+  const [lastAddedPage, setLastAddedPage] = useState('');
+  const [assetData, setAssetData] = useState([]);
+  const {
+    nextPage,
+    afterCursor,
+    cachedTotalCount,
+    setTotalCount
+  } = usePaginator(ASSET_LOAD_COUNT);
+
+  const { data: loadedAssetsNextPage, loading: loadingAssets } = useMyAssetsQuery(
+    ASSET_LOAD_COUNT,
+    afterCursor
+  );
+
+  const loadMoreAssets = useCallback(() => {
+    nextPage(loadedAssetsNextPage?.myNFTs?.pageInfo?.lastCursor);
+  }, [loadedAssetsNextPage?.myNFTs?.pageInfo?.lastCursor, nextPage]);
+
+  useEffect(() => {
+    if (
+      loadedAssetsNextPage?.myNFTs?.items?.length > 0 &&
+      lastAddedPage !== loadedAssetsNextPage?.myNFTs?.pageInfo?.firstCursor
+    ) {
+      setAssetData([
+        ...assetData,
+        ...filterNulls(loadedAssetsNextPage?.myNFTs?.items)
+      ]);
+      setLastAddedPage(loadedAssetsNextPage?.myNFTs?.pageInfo?.firstCursor);
+      setTotalCount(loadedAssetsNextPage?.myNFTs?.totalItems);
+    } else {
+      loadedAssetsNextPage?.myNFTs?.totalItems && setTotalCount(loadedAssetsNextPage?.myNFTs?.totalItems);
+    }
+  }, [lastAddedPage, setTotalCount, assetData, loadedAssetsNextPage, afterCursor, loadedAssetsNextPage?.myNFTs?.items, currentAddress]);
+
   const { stageListings, toggleCartSidebar } = useContext(NFTListingsContext);
   const [selectedAssets, setSelectedAssets] = useState<Array<PartialDeep<Nft>>>([]);
   const [loading, setLoading] = useState(false);
-  const {
-    allOwnerNfts,
-  } = useContext(ProfileContext);
   const { fetchAllowance } = useFetchNftCollectionAllowance();
 
   const onChangeHandler = useCallback(async (asset) => {
@@ -29,12 +67,12 @@ export default function AssetsPages() {
   }, [selectedAssets]);
 
   const selectAllHandler = useCallback(async () => {
-    if (selectedAssets.length === allOwnerNfts.length) {
+    if (selectedAssets.length === assetData.length) {
       setSelectedAssets([]);
     } else {
-      setSelectedAssets([...allOwnerNfts]);
+      setSelectedAssets([...assetData]);
     }
-  }, [allOwnerNfts, selectedAssets.length]);
+  }, [assetData, selectedAssets.length]);
 
   return (
     <div className='flex flex-col justify-between pt-28 px-4 font-grotesk'>
@@ -90,12 +128,12 @@ export default function AssetsPages() {
             : null
           }
         </div>
-        <div className="my-8 font-grotesk rounded-md px-4 pt-4 pb-24 overflow-x-auto">
+        <div className="my-8 font-grotesk rounded-md px-4 pt-4 pb-24 overflow-x-auto min-h-[370px]">
           <table className="border-collapse table-fixed w-full">
             <thead className='text-[#6F6F6F] text-sm font-medium leading-6 p-4'>
               <tr className='p-4 pt-0 pb-3 text-left ...'>
                 <th className='text-[#6F6F6F] text-sm font-medium leading-6 pb-4 pr-8 minmd:pr-4 w-[100px] text-center'>
-                  <input checked={selectedAssets.length === allOwnerNfts.length} onChange={() => selectAllHandler()} className='border-2 border-[#6F6F6F] text-[#6F6F6F] form-checkbox focus:ring-[#F9D963]' type='checkbox' />
+                  <input checked={selectedAssets.length === assetData.length} onChange={() => selectAllHandler()} className='border-2 border-[#6F6F6F] text-[#6F6F6F] form-checkbox focus:ring-[#F9D963]' type='checkbox' />
                 </th>
                 <th className='text-[#6F6F6F] text-sm font-medium leading-6 pb-4 pr-8 minmd:pr-4 w-[300px]'>NFT Name</th>
                 <th className='text-[#6F6F6F] text-sm font-medium leading-6 pb-4 pr-8 minmd:pr-4 w-[200px]'>Collection</th>
@@ -107,11 +145,28 @@ export default function AssetsPages() {
               </tr>
             </thead>
             <tbody className='p-4'>
-              {allOwnerNfts?.map((item, i) => (
-                item.isOwnedByMe && <AssetTableRow isChecked={selectedAssets.some((nft) => nft.tokenId === item.tokenId)} onChange={onChangeHandler} key={item.id} item={item} index={i} />
+              {assetData?.map((item, i) => (
+                <AssetTableRow isChecked={selectedAssets.some((nft) => nft.tokenId === item.tokenId)} onChange={onChangeHandler} key={item.id} item={item} index={i} />
               ))}
             </tbody>
           </table>
+          {loadingAssets && !assetData.length && <p className={tw(
+            'h-20 w-full',
+            'bg-[#F8F8F8]',
+            'pl-8 flex items-center',
+          )}>Loading...</p>}
+          {!loadingAssets && !assetData.length && <p className={tw(
+            'h-20 w-full',
+            'bg-[#F8F8F8]',
+            'pl-8 flex items-center',
+          )}>No assets</p>}
+          {cachedTotalCount !== 1 && cachedTotalCount > assetData?.length &&
+            <div className='w-full flex justify-center'>
+              <button onClick={() => loadMoreAssets()} className="bg-[#F9D963] font-bold tracking-normal hover:bg-[#fcd034] text-base text-black py-2 px-4 rounded-full focus:outline-none focus:shadow-outline w-full minlg:w-[250px] mt-6" type="button">
+                Load More
+              </button>
+            </div>
+          }
         </div>
       </div>
     </div>
