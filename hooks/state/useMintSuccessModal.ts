2cb7def2096b120d54270cc63e2b81db1538dcbc
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-10-17 12:07:43 -0700

    updated context to hooks

---
+++ hooks/state/useMintSuccessModal.ts
@@ -0,0 +1,22 @@
+import { useCallback } from 'react';
+import useSWR from 'swr';
+
+export function useMintSuccessModal() {
+  const { data, mutate } = useSWR('mintSuccessModal', { fallbackData: false });
+
+  const loading = !data;
+  const useToggleMintSuccessModal = useCallback(() => {
+    mutate(!data);
+  }, [data, mutate]);
+
+  const setMintSuccessModalOpen = useCallback((open: boolean) => {
+    mutate(open);
+  }, [mutate]);
+
+  return {
+    loading,
+    profileSelectModal: data,
+    useToggleMintSuccessModal,
+    setMintSuccessModalOpen,
+  };
+}
---
+++ hooks/state/useProfileSelectModal.ts
@@ -0,0 +1,22 @@
+import { useCallback } from 'react';
+import useSWR from 'swr';
+
+export function useProfileSelectModal() {
+  const { data, mutate } = useSWR('profileSelectModal', { fallbackData: false });
+
+  const loading = !data;
+  const useToggleProfileSelectModal = useCallback(() => {
+    mutate(!data);
+  }, [data, mutate]);
+
+  const setProfileSelectModalOpen = useCallback((open: boolean) => {
+    mutate(open);
+  }, [mutate]);
+
+  return {
+    loading,
+    profileSelectModal: data,
+    useToggleProfileSelectModal,
+    setProfileSelectModalOpen,
+  };
+}
