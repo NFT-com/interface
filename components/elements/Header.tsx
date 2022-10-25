d23cd70c3038ea42a7b012e67d34378963e83125
lucas <50972198+robitaille1@users.noreply.github.com>
2022-10-25 16:36:56 -0700

    Fix/homepage header fixes (#599)
    * Created new layout for homepage, and implemented
    
    * updates header items
    
    * Update mobile sidebar for homepage, remove search icon for homepage header

--- components/elements/Header.tsx
+++ components/elements/Header.tsx
@@ -37,12 +37,13 @@ import { useAccount } from 'wagmi';
 const DynamicNotificationBadge = dynamic<React.ComponentProps<typeof StaticNotificationBadge>>(() => import('components/modules/Notifications/NotificationBadge').then(mod => mod.NotificationBadge));
 
 type HeaderProps = {
-  removeBg?: boolean
+  removeBg?: boolean;
+  homepageHeader?: boolean;
 }
 
 gsap.registerPlugin(ScrollTrigger);
 
-export const Header = ({ removeBg }: HeaderProps) => {
+export const Header = ({ removeBg, homepageHeader }: HeaderProps) => {
   const router = useRouter();
   const { address: currentAddress } = useAccount();
   const { setSearchModalOpen, setModalType } = useSearchModal();
@@ -69,56 +70,90 @@ export const Header = ({ removeBg }: HeaderProps) => {
   });
 
   if (getEnvBool(Doppler.NEXT_PUBLIC_HOMEPAGE_V3_ENABLED)) {
-    if (router.pathname === '/') {
+    if (homepageHeader) {
       return (
         <header id='header' className={tw(
           'font-noi-grotesk',
           'fixed inset-x-5 minlg:inset-x-8 minxl:inset-x-14 top-6 minlg:top-7',
-          'h-[5rem] minlg:h-[5.5rem] z-[11]',
+          'h-[5rem] minlg:h-[5.5rem] z-[110]',
         )}>
           <div className="w-full h-full mx-auto pr-5 pl-[1.5rem] min:px-11">
             <div className="flex items-center justify-between h-full">
-              <div id='header-left' data-aos="fade-right" data-aos-delay="100" className={tw(
-                'flex items-center h-full w-full',
-                'minlg:max-w-[50%]'
-              )}>
+              <div className="flex items-center h-full minlg:w-[60%]">
+                <button
+                  className='block minlg:hidden cursor-pointer z-[51] relative mr-4'
+                  onClick={() => {
+                    toggleMobileSidebar();
+                  }}
+                >
+                  {mobileSidebarOpen ? <X size={28} color='black' /> : <List size={28} color='black' />}
+
+                </button>
                 <div className="flex items-center">
-                  <div className="flex-shrink-0 flex items-center hover:cursor-pointer minlg:mr-9 minxxl:mr-11">
-                    <Link href='/' passHref>
-                      <div className='w-10 h-10'>
-                        {useDarkMode ? <LightNavLogo className='justify-start' /> : <NavLogo className='justify-start' />}
-                      </div>
-                    </Link>
-                    <svg className='ml-3' width="63" height="18" viewBox="0 0 63 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M16.3704 0.875H19.9551V17.1235H16.3704L3.51321 5.26179L3.58454 7.83843V17.1235H-6.10352e-05V0.875H3.58454L16.4418 12.7603L16.3704 10.1601V0.875Z" fill="black" />
-                      <path d="M26.3558 10.393V17.125H22.7712V0.875H40.9113V4.12529H26.3574V7.14268H39.4787V10.393H26.3558Z" fill="black" />
-                      <path d="M54.6126 4.12529V17.125H51.028V4.12529H43.1409V0.875H62.4996V4.12529H54.6126Z" fill="black" />
-                    </svg>
-                  </div>
-                  <div className="minlg:hidden block">
-                    <div className="ml-10 flex items-baseline space-x-4">
-                      <a
-                        href="#"
-                        className="hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
-                      >
-                      </a>
+                  <Link href='/' passHref>
+                    <div className="flex-shrink-0 flex items-center hover:cursor-pointer minlg:mr-8">
+                      {mobileSidebarOpen ?
+                        <p className='font-medium'>MENU</p>
+                        :
+                        <div className='w-10 h-10'>
+                          {useDarkMode ? <LightNavLogo className='justify-start' /> : <NavLogo className='justify-start' />}
+                        </div>
+                      }
+                      <svg className='ml-3 hidden minlg:block' width="63" height="18" viewBox="0 0 63 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M16.3704 0.875H19.9551V17.1235H16.3704L3.51321 5.26179L3.58454 7.83843V17.1235H-6.10352e-05V0.875H3.58454L16.4418 12.7603L16.3704 10.1601V0.875Z" fill="black" />
+                        <path d="M26.3558 10.393V17.125H22.7712V0.875H40.9113V4.12529H26.3574V7.14268H39.4787V10.393H26.3558Z" fill="black" />
+                        <path d="M54.6126 4.12529V17.125H51.028V4.12529H43.1409V0.875H62.4996V4.12529H54.6126Z" fill="black" />
+                      </svg>
                     </div>
-                  </div>
+                  </Link>
                 </div>
 
                 <nav
                   className={tw(
-                    'hidden minlg:flex items-center',
+                    'hidden minlg:flex space-x-3',
                     'h-max flex-shrink-0',
                     'text-[#6F6F6F] font-medium tracking-wide',
-                    'pr-4 py-[2px] mr-8'
+                    'flex items-center',
+                    'pr-4 py-[2px]'
                   )}
                 >
-                  <Link href='/app/discover'>
-                    <a className='text-black text-[2.5rem] minlg:text-lg minxxl:text-2xl minlg:mr-8 hover:text-[#6A6A6A]'>Discover</a>
-                  </Link>
+                  {getEnvBool(Doppler.NEXT_PUBLIC_GA_ENABLED) ?
+                    <DropdownPickerModal
+                      pointer
+                      centered
+                      constrain
+                      selectedIndex={0}
+                      options={filterNulls([
+                        {
+                          label: 'NFTs',
+                          onSelect: () => router.push('/discover'),
+                          icon: null,
+                        },
+                        {
+                          label: 'Collections',
+                          onSelect: () => router.push('/discover'),
+                          icon: null,
+                        },
+                        {
+                          label: 'Profiles',
+                          onSelect: () => router.push('/discover'),
+                          icon: null,
+                        }
+                      ])
+                      }>
+                      <a className='text-black text-[2.5rem] minlg:text-lg hover:text-[#6A6A6A] flex items-center relative'>
+                      Discover
+                        <CaretDown size={20} color="black" weight="bold" className='ml-2' />
+                      </a>
+                    </DropdownPickerModal>
+                    :
+                    <Link href='/app/discover'>
+                      <p className='text-black text-[2.5rem] minlg:text-lg hover:text-[#6A6A6A] mr-2 hover:cursor-pointer'>Discover</p>
+                    </Link>
+                  }
+
                   <Link href='/app/gallery'>
-                    <a className='text-black text-[2.5rem] minlg:text-lg minxxl:text-2xl minlg:mr-8 hover:text-[#6A6A6A]'>Gallery</a>
+                    <a className='text-black text-[2.5rem] minlg:text-lg hover:text-[#6A6A6A]'>Gallery</a>
                   </Link>
 
                   <DropdownPickerModal
@@ -128,7 +163,7 @@ export const Header = ({ removeBg }: HeaderProps) => {
                     selectedIndex={0}
                     options={filterNulls([
                       {
-                        label: 'Documents',
+                        label: 'Docs',
                         onSelect: () => window.open ('https://docs.nft.com/', '_ blank'),
                         icon: null,
                       },
@@ -139,7 +174,7 @@ export const Header = ({ removeBg }: HeaderProps) => {
                       }
                     ])
                     }>
-                    <a className='text-black text-[2.5rem] minlg:text-lg minxxl:text-2xl hover:text-[#6A6A6A] flex items-center relative'>
+                    <a className='text-black text-[2.5rem] minlg:text-lg hover:text-[#6A6A6A] flex items-center relative'>
                     Learn
                       <CaretDown size={20} color="black" weight="bold" className='ml-2' />
                     </a>
@@ -147,37 +182,101 @@ export const Header = ({ removeBg }: HeaderProps) => {
                 </nav>
               </div>
 
-              <div data-aos="fade-left" data-aos-delay="200" id='header-right' className={tw(
-                'flex items-center ...'
+              <div className={tw(
+                'flex items-center ...',
+                'minlg:px-0 minlg:mr-0',
+                currentAddress && 'mr-3'
               )}>
-                <div className="hidden minlg:block mr-2">
+                <div className="hidden minlg:block mr-1">
                   <SearchBar />
                 </div>
 
-                <button
-                  className='block minlg:hidden cursor-pointer mr-4 minlg:mr-0 h-full w-7'
-                  onClick={() => {
-                    setModalType('search');
-                    setSearchModalOpen(true);
-                  }}
-                >
-                  <SearchIcon color='#0F0F0F' />
-                </button>
-                <WalletRainbowKitButton header bgLight={!useDarkMode} showWhenConnected signInButton={true} headerButtonColor />
+                {currentAddress &&
+              <div className='block minlg:hidden'>
+                <WalletDropdown count={count} constrain>
+                  <div
+                    className='h-full flex minlg:hidden items-center relative -mr-4 cursor-pointer '
+                  >
+                    {count
+                      ? (
+                        <div className='absolute -right-1 top-1'>
+                          <DynamicNotificationBadge variant='wallet' count={count} />
+                        </div>
+                      )
+                      : null}
+                    <button
+                      className={tw(
+                        'h-12 w-12 rounded-full hover:bg-[#FFF4CA] transition-colors',
+                        'flex items-center justify-center cursor-pointer'
+                      )}
+                    >
+                      <WalletSimple size={24} color={useDarkMode ? primaryIcon : 'black'} />
+                    </button>
+                  </div>
+                </WalletDropdown>
+              </div>
+                }
                 <div
-                  className='h-full flex items-center relative ml-3 cursor-pointer'
+                  className='h-full flex minlg:hidden items-center relative cursor-pointer'
                   onClick={() => {
                     toggleCartSidebar();
                   }}
                 >
                   {[...(toList ?? []), ...(toBuy ?? [])].length > 0 && (
-                    <div className='absolute right-0 -top-4'>
+                    <div className='absolute right-0 -top-2'>
                       <DynamicNotificationBadge count={[...(toList ?? []), ...(toBuy ?? [])].length} />
                     </div>
                   )}
                   <button
                     className={tw(
                       'mr-1 h-12 w-12 rounded-full hover:bg-[#FFF4CA] transition-colors',
+                      'flex items-center justify-center cursor-pointer'
+                    )}
+                  >
+                    <ShoppingCartSimple size={24} color={useDarkMode ? primaryIcon : 'black'} />
+                  </button>
+                </div>
+                <WalletRainbowKitButton header bgLight={!useDarkMode} showWhenConnected signInButton={true} headerButtonColor />
+                {currentAddress &&
+              <div className='hidden minlg:block'>
+                <WalletDropdown count={count} constrain>
+                  <div
+                    className='h-full flex items-center relative ml-3 -mr-5 cursor-pointer '
+                  >
+                    {count
+                      ? (
+                        <div className='absolute right-2 top-1'>
+                          <DynamicNotificationBadge variant='wallet' count={count} />
+                        </div>
+                      )
+                      : null}
+                    <button
+                      className={tw(
+                        'mr-1 h-12 w-12 rounded-full hover:bg-[#FFF4CA] transition-colors',
+                        'flex items-center justify-center cursor-pointer'
+                      )}
+                    >
+                      <WalletSimple size={24} color={useDarkMode ? primaryIcon : 'black'} />
+                    </button>
+                  </div>
+                </WalletDropdown>
+              </div>
+                }
+
+                <div
+                  className='h-full hidden minlg:flex items-center relative ml-3 -mr-5 cursor-pointer'
+                  onClick={() => {
+                    toggleCartSidebar();
+                  }}
+                >
+                  {[...(toList ?? []), ...(toBuy ?? [])].length > 0 && (
+                    <div className='absolute right-0 -top-2'>
+                      <DynamicNotificationBadge count={[...(toList ?? []), ...(toBuy ?? [])].length} />
+                    </div>
+                  )}
+                  <button
+                    className={tw(
+                      'mr-3 h-12 w-12 rounded-full hover:bg-[#FFF4CA] transition-colors',
                       'flex items-center justify-center cursor-pointer'
                     )}
                   >
@@ -268,7 +367,7 @@ export const Header = ({ removeBg }: HeaderProps) => {
                     ])
                     }>
                     <a className='text-black text-[2.5rem] minlg:text-lg hover:text-[#6A6A6A] flex items-center relative'>
-                  Discover
+                      Discover
                       <CaretDown size={20} color="black" weight="bold" className='ml-2' />
                     </a>
                   </DropdownPickerModal>
@@ -289,7 +388,7 @@ export const Header = ({ removeBg }: HeaderProps) => {
                   selectedIndex={0}
                   options={filterNulls([
                     {
-                      label: 'Documents',
+                      label: 'Docs',
                       onSelect: () => window.open ('https://docs.nft.com/', '_ blank'),
                       icon: null,
                     },
