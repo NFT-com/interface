a1becb59b8121d69f07d7a28e91bea3d53d3e8af
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-11-01 15:33:21 -0700

    updated 6 months to 180 days

--- utils/marketplaceUtils.ts
+++ utils/marketplaceUtils.ts
@@ -12,7 +12,7 @@ import { BigNumber, BigNumberish, ethers } from 'ethers';
 import { PartialDeep } from 'type-fest';
 
 export const MAX_UINT_256 = BigNumber.from(2).pow(256).sub(1);
-export type SaleDuration = '1 Hour' | '1 Day' | '7 Days' | '6 Months' | '30 Days' | '60 Days' | '90 Days' | '1 Year';
+export type SaleDuration = '1 Hour' | '1 Day' | '7 Days' | '30 Days' | '60 Days' | '90 Days' | '180 Days' | '1 Year';
 
 export const convertDurationToSec = (d: SaleDuration) => {
   const durationDays: {[s in SaleDuration]: number} = {
@@ -22,7 +22,7 @@ export const convertDurationToSec = (d: SaleDuration) => {
     '30 Days': 30,
     '60 Days': 60,
     '90 Days': 7 * 4 * 3,
-    '6 Months': 7 * 4 * 6,
+    '180 Days': 7 * 4 * 6,
     '1 Year': 7 * 4 * 12,
   };
   return 60 * 60 * 24 * durationDays[d];
