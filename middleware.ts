dc5f98a79dac7703ec632e4309caf299fb3d97e2
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-11-02 15:35:07 -0700

    added middleware to redirect to lowercase urls

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @next/next/no-server-import-in-page */
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+const Middleware = (req: NextRequest) => {
+  if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase() || req.nextUrl.pathname.includes('profileURI'))
+    return NextResponse.next();
+
+  return NextResponse.redirect(new URL(req.nextUrl.origin + req.nextUrl.pathname.toLowerCase()));
+};
+
+export default Middleware;
