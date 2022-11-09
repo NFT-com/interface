7e0b3ae99b1c3c4966e8adcd2d4b119fe1f774a8
Gavin Mai <gavinmai@alumni.stanford.edu>
2022-11-09 15:52:51 -0600

    fix: new handler

--- pages/api/segment.ts
+++ pages/api/segment.ts
@@ -2,17 +2,35 @@ import { withSentry } from '@sentry/nextjs';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 const segmentHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const requestUrl = 'https://cdn.segment.com/analytics.js/v1/' + process.env.SEGMENT_API_KEY + '/analytics.min.js';
-
   try {
-    const result = await fetch(requestUrl.toString(), {
-      method: 'GET',
-      redirect: 'follow',
-    }).then(segmentRes => segmentRes.json());
-    res.status(200).json( result );
-    return;
+    const action = req.query['action'];
+
+    const requestUrl = `https://api.segment.io/v1/${action}`;
+
+    // allowed actions = track, identify, page
+    if (action == 'track' || action == 'identify' || action == 'page') {
+      const body = req.body;
+      try {
+        const result = await fetch(requestUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Basic ${Buffer.from(process.env.NEXT_PUBLIC_SEGMENT_API_KEY + ':' + '').toString('base64')}`, // base64 encoding of the write key
+          },
+          body: JSON.stringify(body),
+        }).then(segmentRes => segmentRes.json());
+        res.status(200).json(result);
+        return;
+      } catch (e) {
+        res.status(500).json(JSON.stringify({ message: 'error processing inner Segment result' }));
+        return;
+      }
+    } else {
+      res.status(400).json({ message: 'Invalid action' });
+      return;
+    }
   } catch (e) {
-    res.status(500).json(JSON.stringify({ message: 'Error processing segmento.io result' }));
+    res.status(500).json(JSON.stringify({ message: 'error processing outer Segment result' }));
     return;
   }
 };
