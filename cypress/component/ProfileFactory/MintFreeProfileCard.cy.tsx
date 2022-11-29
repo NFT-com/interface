c0c54e46fa47be84d93adcea9689d568ce80e060
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-11-29 12:07:50 -0800

    update test

--- cypress/component/ProfileFactory/MintFreeProfileCard.cy.tsx
+++ cypress/component/ProfileFactory/MintFreeProfileCard.cy.tsx
@@ -9,28 +9,16 @@ describe('MintFreeProfileCard', () => {
   const client = setupWagmiClient();
 
   it('mounts and renders when free profile available', () => {
-    const onOpen = cy.stub();
     cy.mount(
       <WagmiConfig client={client}>
-        <MintFreeProfileCard minting={false} setMintingState={onOpen} setModalOpen={onOpen} type='Free' />
+        <MintFreeProfileCard />
       </WagmiConfig>
     );
     cy.findByText('Claim your free NFT Profile').should('exist');
-    cy.findByText('Every wallet receives one').should('exist');
+    cy.findByText('Every wallet receives').should('exist');
     cy.get('input').type('testProfile').then(() => {
       cy.findByText('Great! Profile name is available :)').should('exist');
       cy.get('button').should('not.be.disabled');
     });
-  });
-
-  it('mounts and renders when free profile not available', () => {
-    const onOpen = cy.stub();
-    cy.mount(
-      <WagmiConfig client={client}>
-        <MintFreeProfileCard minting={false} setMintingState={onOpen} setModalOpen={onOpen} type='Paid' />
-      </WagmiConfig>
-    );
-    cy.findByText('You have already received one free mint').should('exist');
-    cy.findByText('Transaction fee').should('exist');
   });
 });
