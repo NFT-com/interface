e27a41dbc8d57cef7d4d0cbbddb3830af7e10b3b
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2022-12-16 14:55:36 -0800

    add x2y2 abi

--- hooks/contracts/useX2Y2ExchangeContract.ts
+++ hooks/contracts/useX2Y2ExchangeContract.ts
@@ -1,5 +1,5 @@
-import { X2y2_exchange__factory } from 'constants/typechain/factories/X2y2_exchange__factory';
-import { X2y2_exchange } from 'constants/typechain/X2y2_exchange';
+import { X2y2_exchange__factory } from 'constants/typechain';
+import { X2y2_exchange } from 'constants/typechain';
 import { X2Y2_EXCHANGE_CONTRACT } from 'types';
 
 import { Provider } from '@ethersproject/providers';
