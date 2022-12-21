5d1113e8ccbd92243f8b5af7bad3536f932cafbf
gmaijoe <gmaijoe@users.noreply.github.com>
2022-12-21 07:28:16 -0600

    Add some edge fixes for NFT details (#775)
    * fix: new
    
    * fix: listings
    
    * fix: new
    
    * fix: ok

--- components/modules/Analytics/LineVis.tsx
+++ components/modules/Analytics/LineVis.tsx
@@ -118,7 +118,7 @@ export const LineVis = ({ data, showMarketplaceOptions, selectedTimeFrame }: Lin
       </div>
       }
       <ResponsiveContainer height={320} width={'100%'} >
-        <LineChart data={data} margin={{ top: 10, right: 30, bottom: 10, left: -5 }} height={320}>
+        <LineChart data={data} margin={{ top: 10, right: 30, bottom: 10, left: 0 }} height={320}>
           <defs>
             <linearGradient id="colorvalue" x1="0" y1="0" x2="0" y2="1">
               <stop offset="5%" stopColor={'#FAC213'} stopOpacity={0.2}/>
