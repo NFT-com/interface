613f8b1d144d54c038fd1b77e76df8c1fcbb538d
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-01-11 16:40:09 -0600

    fix: wao

--- components/modules/Checkout/CheckoutSuccessView.tsx
+++ components/modules/Checkout/CheckoutSuccessView.tsx
@@ -5,6 +5,7 @@ import { processIPFSURL } from 'utils/helpers';
 import { NFTListingsContext } from './NFTListingsContext';
 
 import { useRouter } from 'next/router';
+import { Check } from 'phosphor-react';
 import DesktopSuccessProfile from 'public/images/desktop_success_profile.svg';
 import NullProfile from 'public/images/null_profile.svg';
 import { useContext } from 'react';
@@ -23,7 +24,43 @@ export function CheckoutSuccessView(props: CheckoutSuccessViewProps) {
     toList,
   } = useContext(NFTListingsContext);
 
-  console.log('toList: ', toList);
+  const images = () => {
+    if (toList.length == 1) {
+      return <div className='relative'>
+        <RoundedCornerMedia
+          containerClasses='w-[130px] h-[130px] mb-10 z-10'
+          src={processIPFSURL(toList[0]?.nft?.metadata?.imageURL)}
+          variant={RoundedCornerVariant.Success}
+        />
+        <Check className={'absolute top-[-12px] right-[-12px] z-20 h-7 w-7 p-1 text-white font-medium aspect-square shrink-0 rounded-full -ml-4 bg-[#26AA73]'} />
+      </div>;
+    } else if (toList.length > 1) {
+      return <div className='relative'>
+        <div className='flex flex-row justify-center items-center ml-16 mb-5'>
+          {toList.filter((_, index) => index < 3).map((listing, i) =>
+            <RoundedCornerMedia
+              key={i}
+              containerClasses={`w-[${130 - (i * 10)}px] h-[${130 - (i * 10)}px] z-${50 - i * 10} -ml-16 shadow-xl`}
+              src={processIPFSURL(listing?.nft?.metadata?.imageURL)}
+              variant={RoundedCornerVariant.Success}
+            />
+          )}
+          {toList.length > 3 &&
+            <div className='flex items-center justify-end w-[90px] h-[90px] z-10 -ml-16 pr-2 bg-[#F9D54C] rounded-[18px]'>
+              +{toList.length - 3}
+            </div>
+          }
+        </div>
+        <Check className={`absolute ${toList.length >= 3 ? 'top-0' : 'top-[-10px]'} right-[-13px] z-[100] h-7 w-7 p-1 text-white font-medium aspect-square shrink-0 rounded-full -ml-4 bg-[#26AA73]`} />
+      </div>;
+    }
+  };
+
+  const listingMessage = () => {
+    return toList.length > 1 ?
+      `You have successfully listed ${toList.length} NFTs` :
+      'You have successfully listed your NFT';
+  };
 
   return myOwnedProfileTokens?.length > 0 ?
     <div className="flex items-center h-[596px] font-noi-grotesk">
@@ -35,13 +72,9 @@ export function CheckoutSuccessView(props: CheckoutSuccessViewProps) {
       </div>
       <div className="relative h-full w-3/5 right-0">
         <div className='flex flex-col items-center justify-center h-full w-full px-10'>
-          <RoundedCornerMedia
-            containerClasses='w-[130px] h-[130px] mb-10'
-            src={processIPFSURL(toList[0]?.nft?.metadata?.imageURL)}
-            variant={RoundedCornerVariant.Success}
-          />
+          {images()}
           <div className='text-[34px] font-medium'>Congratulations!</div>
-          <div className='text-[18px] font-medium mt-4'>Your listing is now live on nft.com</div>
+          <div className='text-[18px] font-medium mt-4'>{listingMessage()}</div>
           <div className='text-[16px] mt-10'>Let&apos;s continue your web3 journey</div>
           <button onClick={() => alert('ok')} className="bg-[#F9D963] w-[277px] my-8 font-medium hover:bg-[#fcd034] text-base text-black text-[14px] p-4 rounded-[12px] focus:outline-none focus:shadow-outline" type="button">
             Share your Listing
