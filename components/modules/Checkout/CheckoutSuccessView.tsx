d8fb313c5ea39c94b2a849fe972f677c0fa10529
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-01-11 15:55:03 -0600

    fix: revert

--- components/modules/Checkout/CheckoutSuccessView.tsx
+++ components/modules/Checkout/CheckoutSuccessView.tsx
@@ -1,8 +1,13 @@
+import { RoundedCornerMedia, RoundedCornerVariant } from 'components/elements/RoundedCornerMedia';
 import { useMyNftProfileTokens } from 'hooks/useMyNftProfileTokens';
+import { processIPFSURL } from 'utils/helpers';
+
+import { NFTListingsContext } from './NFTListingsContext';
 
 import { useRouter } from 'next/router';
 import DesktopSuccessProfile from 'public/images/desktop_success_profile.svg';
 import NullProfile from 'public/images/null_profile.svg';
+import { useContext } from 'react';
 
 export interface CheckoutSuccessViewProps {
   userAddress: string;
@@ -14,6 +19,12 @@ export function CheckoutSuccessView(props: CheckoutSuccessViewProps) {
   const { profileTokens: myOwnedProfileTokens } = useMyNftProfileTokens();
   const router = useRouter();
 
+  const {
+    toList,
+  } = useContext(NFTListingsContext);
+
+  console.log('toList: ', toList);
+
   return myOwnedProfileTokens?.length > 0 ?
     <div className="flex items-center h-[596px] font-noi-grotesk">
       <div onClick={() => router.push('/app/mint-profiles')} className='absolute bottom-5 left-7 hover:cursor-pointer underline text-[16px] z-50 font-medium flex items-center'>
@@ -24,13 +35,18 @@ export function CheckoutSuccessView(props: CheckoutSuccessViewProps) {
       </div>
       <div className="relative h-full w-3/5 right-0">
         <div className='flex flex-col items-center justify-center h-full w-full px-10'>
-          <div className='text-[38px] font-medium'>Congratulations!</div>
-          <div className='text-[20px] font-medium mt-4'>Your listing is now live on nft.com</div>
-          <div className='text-[18px] mt-10'>Let&apos;s continue your web3 journey</div>
-          <button onClick={() => alert('ok')} className="bg-[#F9D963] w-[277px] my-8 font-medium hover:bg-[#fcd034] text-base text-black text-[16px] p-4 rounded-[12px] focus:outline-none focus:shadow-outline" type="button">
+          <RoundedCornerMedia
+            containerClasses='w-[130px] h-[130px] mb-10'
+            src={processIPFSURL(toList[0]?.nft?.metadata?.imageURL)}
+            variant={RoundedCornerVariant.Success}
+          />
+          <div className='text-[34px] font-medium'>Congratulations!</div>
+          <div className='text-[18px] font-medium mt-4'>Your listing is now live on nft.com</div>
+          <div className='text-[16px] mt-10'>Let&apos;s continue your web3 journey</div>
+          <button onClick={() => alert('ok')} className="bg-[#F9D963] w-[277px] my-8 font-medium hover:bg-[#fcd034] text-base text-black text-[14px] p-4 rounded-[12px] focus:outline-none focus:shadow-outline" type="button">
             Share your Listing
           </button>
-          <div onClick={() => router.push('/app/assets')} className='text-[#E4BA18] font-medium underline text-[16px] cursor-pointer'>List another NFT</div>
+          <div onClick={() => router.push('/app/assets')} className='text-[#E4BA18] font-medium underline text-[14px] cursor-pointer'>List another NFT</div>
         </div>
       </div>
     </div> :
