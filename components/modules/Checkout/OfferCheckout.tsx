b6d547f7f0b0ce52fb9489f2bdc92ddac8ddb836
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-01-12 16:46:04 -0600

    fix: latest

--- components/modules/Checkout/OfferCheckout.tsx
+++ components/modules/Checkout/OfferCheckout.tsx
@@ -57,19 +57,6 @@ export function OfferCheckout() {
   const profileOwnerToShow: PartialDeep<Profile> = toList[0]?.nft?.wallet?.preferredProfile ?? profileData?.profile;
   const [showSummary, setShowSummary] = useState(false);
 
-  const openseaFullyEnabled = !isNullOrEmpty(toList) && toList.find(nft => {
-    const hasTarget = nft?.targets?.find(target => target?.protocol === ExternalProtocol.Seaport) != null;
-    return !hasTarget; // return true if missing the desired target.
-  }) == null; // target is fully enabled if we didn't find an NFT that was missing it.
-  const looksrareFullyEnabled = !isNullOrEmpty(toList) && toList.find(nft => {
-    const hasTarget = nft?.targets?.find(target => target?.protocol === ExternalProtocol.LooksRare) != null;
-    return !hasTarget; // return true if missing the desired target.
-  }) == null; // target is fully enabled if we didn't find an NFT that was missing it.
-  const X2Y2FullyEnabled = !isNullOrEmpty(toList) && toList.find(nft => {
-    const hasTarget = nft?.targets?.find(target => target?.protocol === ExternalProtocol.X2Y2) != null;
-    return !hasTarget; // return true if missing the desired target.
-  }) == null; // target is fully enabled if we didn't find an NFT that was missing it.
-
   const openseaAtLeastOneEnabled = !isNullOrEmpty(toList) && toList.find(nft => {
     return nft?.targets?.find(target => target?.protocol === ExternalProtocol.Seaport) != null;
   }) != null;
@@ -285,148 +272,32 @@ export function OfferCheckout() {
     </div>;
   };
   
-  return !getEnvBool(Doppler.NEXT_PUBLIC_TX_ROUTER_RESKIN_ENABLED)
-    ? (
-      <div className="flex flex-col items-center w-full mt-10">
-        <div className="flex flex-col items-center w-full">
-          <div className='w-full flex flex-col px-8 items-center'>
-            <span className='text-2xl w-full flex font-bold'>Select Marketplace</span>
-            <div className='flex flex-col minlg:flex-row items-center justify-around w-full '>
-              <div
-                onClick={() => {
-                  toggleTargetMarketplace(ExternalProtocol.Seaport);
-                  setShowSummary(false);
-                }}
-                className={tw(
-                  'border border-[#D5D5D5] rounded-xl text-lg',
-                  'px-4 py-6 cursor-pointer w-full mt-4 mx-4',
-                  openseaFullyEnabled ? 'border-2 border-primary-yellow font-bold' : ''
-                )}
-              >
-                <span>Opensea</span>
-                <span className='ml-2 font-medium text-sm text-[#6F6F6F]'>(2.5% fee)</span>
-              </div>
-              <div
-                onClick={() => {
-                  toggleTargetMarketplace(ExternalProtocol.LooksRare);
-                  setShowSummary(false);
-                }}
-                className={tw(
-                  'border border-[#D5D5D5] rounded-xl text-lg',
-                  'px-4 py-6 cursor-pointer w-full mt-4',
-                  looksrareFullyEnabled ? 'border-2 border-primary-yellow font-bold' : ''
-                )}
-              >
-                <span>Looksrare</span>
-                <span className='ml-2 font-medium text-sm text-[#6F6F6F]'>(2% fee)</span>
-              </div>
-              {getEnvBool(Doppler.NEXT_PUBLIC_X2Y2_ENABLED) &&
-              <div
-                onClick={() => {
-                  toggleTargetMarketplace(ExternalProtocol.X2Y2);
-                  setShowSummary(false);
-                }}
-                className={tw(
-                  'border border-[#D5D5D5] rounded-xl text-lg',
-                  'px-4 py-6 cursor-pointer w-full mt-4 mx-4',
-                  X2Y2FullyEnabled ? 'border-2 border-primary-yellow font-bold' : ''
-                )}
-              >
-                <span>X2Y2</span>
-                <span className='ml-2 font-medium text-sm text-[#6F6F6F]'>(0.5% fee)</span>
-              </div>
-              }
-            </div>
-          </div>
-          {/* <div className='w-full flex flex-col px-8 mt-8 items-center'>
-            <span className='text-2xl w-full flex font-bold'>Set Duration</span>
-            <div className='flex flex-row items-center justify-around mt-4 w-full max-w-lg'>
-              {
-                ['1 Hour', '1 Day', '7 Days','30 Days', '60 Days', '90 Days', '180 Days'].map(duration => {
-                  return <div
-                    key={duration}
-                    onClick={() => {
-                      setDuration(duration as SaleDuration);
-                    }}
-                    className={tw(
-                      'rounded-full py-2.5 px-2',
-                      'border border-[#D5D5D5] text-black',
-                      'cursor-pointer hover:opacity-80',
-                    )}
-                  >
-                    {duration}
-                  </div>;
-                })
-              }
-            </div>
-          </div> */}
-          <div className='my-8 overflow-x-scroll w-full flex flex-col'>
-            <div className="border-t border-[#D5D5D5] mx-8">
-              <span className='text-2xl w-full flex font-bold mt-10 mb-8 mx-8'>Your Listings</span>
-            </div>
-            <table className="w-full mx-8 text-sm table-auto">
-              <thead>
-                <tr>
-                  <th className="font-medium pb-3 text-blog-text-reskin text-left">NFT</th>
-                  <th className="font-medium pb-3 text-blog-text-reskin text-left">Set Price</th>
-                  <th className="font-medium pb-3 text-blog-text-reskin text-left">Marketplaces</th>
-                </tr>
-              </thead>
-              <tbody>
-                {filterNulls(toList).map((listing, index) => {
-                  return (<ListingCheckoutNftTableRow key={index} listing={listing} onPriceChange={() => {
-                    setShowSummary(false);
-                  }} />);
-                })}
-              </tbody>
-            </table>
-          </div>
-          {
-            (isNullOrEmpty(toList) || toList.length === 0) && <div className='flex flex-col items-center justify-center my-12'>
-          No NFTs staged for listing
-            </div>
-          }
-          {!showSummary && toList.length > 0 && <Button
-            label={'Start Listing'}
-            disabled={!allListingsConfigured()}
-            onClick={async () => {
-              await prepareListings();
-              setShowSummary(true);
-            }}
-            type={ButtonType.PRIMARY}
-          />}
-        </div>
-        <NFTListingsCartSummaryModal visible={showSummary && toList.length > 0} onClose={() => setShowSummary(false)} />
+  return <div className='flex w-full justify-between h-full'>
+    {toList.length === 1 && ListingOneNFT()}
+    {(toList.length === 0 || toList.length > 1) && <div className='hidden minlg:block w-1/5 mt-20'>
+      <h1
+        className='text-xl font-semibold font-noi-grotesk cursor-pointer ml-28'
+        onClick={() => {
+          router.back();
+        }}>
+        Back
+      </h1>
+    </div>}
+    <div className={tw(
+      toList.length === 1 ? 'minlg:px-[5%]' : 'minlg:px-2 minxl:px-4',
+      'w-full flex flex-col justify-start items-center minlg:w-3/5 minxxl:px-28')}>
+      <div className='w-full minlg:mt-20 flex minlg:block justify-start items-end minlg:items-center minlg:mx-auto'>
+        <span
+          className='minlg:hidden text-lg font-semibold font-noi-grotesk cursor-pointer minlg:ml-28'
+          onClick={() => {
+            router.back();
+          }}>
+          Back
+        </span>
+        <h1 className='text-2xl minlg:text-3xl pl-12 minlg:pl-0 font-semibold font-noi-grotesk'>Your Offers</h1>
       </div>
-    ) :
-    (
-      <div className='flex w-full justify-between h-full'>
-        {toList.length === 1 && ListingOneNFT()}
-        {(toList.length === 0 || toList.length > 1) && <div className='hidden minlg:block w-1/5 mt-20'>
-          <h1
-            className='text-xl font-semibold font-noi-grotesk cursor-pointer ml-28'
-            onClick={() => {
-              router.back();
-            }}>
-            Back
-          </h1>
-        </div>}
-        <div className={tw(
-          toList.length === 1 ? 'minlg:px-[5%]' : 'minlg:px-2 minxl:px-4',
-          'w-full flex flex-col justify-start items-center minlg:w-3/5 minxxl:px-28')}>
-          <div className='w-full minlg:mt-20 flex minlg:block justify-start items-end minlg:items-center minlg:mx-auto'>
-            <span
-              className='minlg:hidden text-lg font-semibold font-noi-grotesk cursor-pointer minlg:ml-28'
-              onClick={() => {
-                router.back();
-              }}>
-              Back
-            </span>
-            <h1 className='text-2xl minlg:text-3xl pl-12 minlg:pl-0 font-semibold font-noi-grotesk'>Create Listings</h1>
-          </div>
-          {ListingCheckoutInfo()}
-        </div>
-        {(toList.length === 0 || toList.length > 1) && <div className='hidden minlg:block w-1/5 mt-20'></div>}
-      </div>
-    );
+      {ListingCheckoutInfo()}
+    </div>
+    {(toList.length === 0 || toList.length > 1) && <div className='hidden minlg:block w-1/5 mt-20'></div>}
+  </div>
 }
