4d9d18718d28dcc3464a27e054365bdec11c010f
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-01-12 11:31:56 -0600

    fix: latest

--- pages/api/imageFetcher.ts
+++ pages/api/imageFetcher.ts
@@ -5,6 +5,10 @@ import fetch from 'node-fetch';
 
 const imageFetcher = async (req: NextApiRequest, res: NextApiResponse) => {
   const gcp_enabled = process.env.GCP_IMG_PROXY_ENABLED;
+  if (gcp_enabled) {
+    return 'https://clonex-assets.rtfkt.com/images/6471.png?width=600';
+  }
+
   const r = await axios.get(`https://5hi24d3w2gny6zrfhekqk6mv4e0cfois.lambda-url.us-east-1.on.aws?gcp=${gcp_enabled}&url=${encodeURIComponent(`${req.query.url}`)}&width=${Number(req.query.width) || 1000}&height=${Number(req.query.height) || 1000}`);
   const optimizedUrl = r.data.data;
   const result = await fetch(optimizedUrl);
