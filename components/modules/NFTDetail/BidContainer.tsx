1983de8416396beadfc96b0d60c2d5f69f9984af
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-01-12 15:47:06 -0600

    fix: formats

--- components/modules/NFTDetail/BidContainer.tsx
+++ components/modules/NFTDetail/BidContainer.tsx
@@ -1,11 +1,10 @@
 import { RoundedCornerMedia, RoundedCornerVariant } from 'components/elements/RoundedCornerMedia';
 import { Nft } from 'graphql/generated/types';
-
-import moment from 'moment';
-import Link from 'next/link';
-import { PartialDeep } from 'type-fest';
-import { useEffect, useState } from 'react';
 import { tw } from 'utils/tw';
+
+import ShopIcon from 'public/shop-icon.svg';
+import { useEffect, useState } from 'react';
+import { PartialDeep } from 'type-fest';
 
 export type BidContainerProps = {
   data: PartialDeep<Nft>;
@@ -48,39 +47,39 @@ export const BidContainer = ({ data }: BidContainerProps) => {
           'flex flex-col bg-white rounded-[18px] shadow-xl border border-gray-200 mb-5 w-full max-w-nftcom h-fit justify-between relative font-noi-grotesk',
         )}>
           <div className='w-full flex items-center'>
-            <div className="relative h-full w-1/2 left-0">
-              <div className="h-8 px-6 pb-6 pt-10 w-full flex items-center">
+            <div className="relative h-full w-1/2 my-6 left-0 border-r border-[#ECECEC]">
+              <div className="h-8 px-6 w-full flex items-center">
                 <span className='text-[28px] font-semibold text-black'>
                   Current Bid
                 </span>
               </div>
-              <div className='flex font-noi-grotesk text-black leading-6 items-center my-8 md:my-6 px-6 justify-between'>
+              <div className='flex font-noi-grotesk text-black leading-6 items-center mt-8 md:my-6 px-6 justify-between'>
                 <div className='flex md:flex-col md:items-start items-end leading-6'>
                   <div className='flex items-end'>
                     <div className='flex items-end'>
                       Icon
-                      <span className='text-[37px] font-semibold'>Something</span>
+                      <span className='text-[37px] font-semibold'>1.5</span>
                     </div>
                     <span className='mx-1.5 text-[15px] uppercase font-semibold'>WETH</span>
                   </div>
                   <span className="md:ml-0 md:mt-2 ml-2 text-[15px] uppercase font-medium text-[#6A6A6A]">
-                    $123.11
+                    $123.11 USD
                   </span>
                 </div>
               </div>
             </div>
-            <div className="relative h-full w-1/2 right-0">
-              <div className="h-8 px-6 pb-6 pt-10 w-full flex items-center">
+            <div className="relative h-full w-1/2 my-6 right-0">
+              <div className="h-8 px-6 w-full flex items-center">
                 <span className='text-[28px] font-semibold text-black'>
-                  Current Bid
+                  Highest Bid
                 </span>
               </div>
-              <div className='flex font-noi-grotesk text-black leading-6 items-center my-8 md:my-6 px-6 justify-between'>
+              <div className='flex font-noi-grotesk text-black leading-6 items-center mt-8 md:my-6 px-6 justify-between'>
                 <div className='flex md:flex-col md:items-start items-end leading-6'>
                   <div className='flex items-end'>
                     <div className='flex items-end'>
                       Icon
-                      <span className='text-[37px] font-semibold'>Something</span>
+                      <span className='text-[37px] font-semibold'>1.5</span>
                     </div>
                     <span className='mx-1.5 text-[15px] uppercase font-semibold'>WETH</span>
                   </div>
@@ -92,7 +91,12 @@ export const BidContainer = ({ data }: BidContainerProps) => {
             </div>
           </div>
           <div className='flex w-full h-full px-6 py-4 rounded-br-[18px] rounded-bl-[18px] bg-[#F2F2F2]'>
-            Button
+            <button onClick={() => alert('ok')} className="bg-[#F9D963] hover:bg-[#fcd034] text-base text-black py-3 px-4 rounded-[12px] focus:outline-none focus:shadow-outline w-full" type="button">
+              Buy Now
+            </button>
+            <button onClick={() => alert('ok')} className="bg-[#FFFFFF] flex whitespace-nowrap items-center ml-4 justify-center text-base border border-[#B2B2B2] text-black py-3 px-7 rounded-[12px] focus:outline-none focus:shadow-outline w-fit" type="button">
+              <ShopIcon />&nbsp;Add to Cart
+            </button>
           </div>
         </div>
       </div>
