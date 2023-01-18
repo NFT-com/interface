01d98cd3199baa9334fc402708120126be5b6f63
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-01-18 11:52:56 -0400

    fix: loading screen

--- components/modules/Checkout/OfferCheckout.tsx
+++ components/modules/Checkout/OfferCheckout.tsx
@@ -53,9 +53,6 @@ export function OfferCheckout() {
   const [showSummary, setShowSummary] = useState(false);
   const [selectedExpirationOption, setSelectedExpirationOption] = useState(2);
 
-  console.log('profileOwnerToShow: ', profileOwnerToShow);
-  console.log('nft: ', nft);
-
   const BiddingOneNFT = () => {
     return(
       <div className='hidden minlg:flex flex-col justify-start items-center bg-gray-200 w-2/5 min-h-[100vh]'>
@@ -70,60 +67,73 @@ export function OfferCheckout() {
             Back
           </h1>
         </div>
-        <div className='mt-20 w-1/2'>
-          <video
-            autoPlay
-            muted
-            loop
-            key={nft?.metadata?.imageURL}
-            src={processIPFSURL(nft?.metadata?.imageURL)}
-            poster={processIPFSURL(nft?.metadata?.imageURL)}
-            className={tw(
-              'rounded-md w-full',
-            )}
-          />
-          <div className='flex font-noi-grotesk mt-6'>
-            <div className='flex flex-col justify-between w-3/5'>
-              <span className='font-semibold text-ellipsis overflow-hidden'>{nft?.metadata?.name}</span>
-              <span className='text-[#6F6F6F] whitespace-nowrap text-ellipsis overflow-hidden'>{collection?.collection?.name || '-'}</span>
-            </div>
-            <div className='flex justify-start h-full w-2/5'>
-              <div className='flex flex-col h-[42px] w-[42px] '>
-                {profileOwnerToShow?.photoURL ?
-                  <div className="relative object-cover aspect-square rounded-md w-full">
-                    <Image
-                      layout='fill'
-                      alt="NFT Profile Image"
-                      src={processIPFSURL(profileOwnerToShow?.photoURL)}
-                      className="object-cover absolute w-full h-full justify-center rounded-[50%]" />
-                  </div>
-                  :
-                  <LoggedInIdenticon round border />
-                }
+        {nft?.metadata?.imageURL ?
+          <div className='mt-20 w-1/2'>
+            <video
+              autoPlay
+              muted
+              loop
+              key={nft?.metadata?.imageURL}
+              src={processIPFSURL(nft?.metadata?.imageURL)}
+              poster={processIPFSURL(nft?.metadata?.imageURL)}
+              className={tw(
+                'rounded-md w-full',
+              )}
+            />
+            <div className='flex font-noi-grotesk mt-6'>
+              <div className='flex flex-col justify-between w-3/5'>
+                <span className='font-semibold text-ellipsis overflow-hidden'>{nft?.metadata?.name}</span>
+                <span className='text-[#6F6F6F] whitespace-nowrap text-ellipsis overflow-hidden'>{collection?.collection?.name || '-'}</span>
               </div>
-              <div
-                className={tw(
-                  'flex px-3 items-center',
-                  profileOwnerToShow?.url && 'cursor-pointer'
-                )}
-                onClick={() => {
-                  if (profileOwnerToShow?.url) {
-                    router.push('/' + profileOwnerToShow?.url);
-                  } else {
-                    window.open(getEtherscanLink(Number(defaultChainId), nft?.wallet?.address, 'address'), '_blank');
+              <div className='flex justify-start h-full w-2/5'>
+                <div className='flex flex-col h-[42px] w-[42px] '>
+                  {profileOwnerToShow?.photoURL ?
+                    <div className="relative object-cover aspect-square rounded-md w-full">
+                      <Image
+                        layout='fill'
+                        alt="NFT Profile Image"
+                        src={processIPFSURL(profileOwnerToShow?.photoURL)}
+                        className="object-cover absolute w-full h-full justify-center rounded-[50%]" />
+                    </div>
+                    :
+                    <LoggedInIdenticon round border />
                   }
-                }}
-              >
-                <span className="text-base whitespace-nowrap text-ellipsis overflow-hidden ml-2leading-5 font-noi-grotesk">
-                  {profileOwnerToShow?.url ?
-                    `@${profileOwnerToShow?.url}` :
-                    shortenAddress(nft?.wallet?.address, isMobile ? 4 : 6) ?? 'Unknown'
-                  }
-                </span>
+                </div>
+                <div
+                  className={tw(
+                    'flex px-3 items-center',
+                    'cursor-pointer hover:underline'
+                  )}
+                  onClick={() => {
+                    if (profileOwnerToShow?.url) {
+                      router.push('/' + profileOwnerToShow?.url);
+                    } else {
+                      window.open(getEtherscanLink(Number(defaultChainId), nft?.wallet?.address, 'address'), '_blank');
+                    }
+                  }}
+                >
+                  <span className="text-base whitespace-nowrap text-ellipsis overflow-hidden ml-2leading-5 font-noi-grotesk">
+                    {profileOwnerToShow?.url ?
+                      `@${profileOwnerToShow?.url}` :
+                      shortenAddress(nft?.wallet?.address, isMobile ? 4 : 6) ?? 'Unknown'
+                    }
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+          :
+          <div className='m-auto w-1/2 flex flex-col justify-center items-center'>
+            <div role="status">
+              <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-[#FDCC00]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+              </svg>
+              <span className="sr-only">Loading...</span>
+            </div>
+            <span className='text-[#6F6F6F] mt-4'>Loading NFT...</span>
+          </div>
+        }
       </div>
     );
   };
