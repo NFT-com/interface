00e938eaa860b2d8736ae6ef0c87cb23bc089415
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-01-18 12:00:35 -0400

    fix: loading screen

--- components/modules/Checkout/OfferCheckout.tsx
+++ components/modules/Checkout/OfferCheckout.tsx
@@ -123,7 +123,7 @@ export function OfferCheckout() {
             </div>
           </div>
           :
-          <div className='m-auto w-1/2 flex flex-col justify-center items-center'>
+          <div className='mt-[30vh] w-1/2 flex flex-col justify-center items-center'>
             <div role="status">
               <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-[#FDCC00]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
@@ -182,21 +182,26 @@ export function OfferCheckout() {
         <div className='w-full flex flex-col items-center mb-4'>
           <span className='text-[18px] w-full flex text-black'>Once your bid is placed, you will be the highest bidder in the auction.</span>
           <div className='w-full text-[16px] font-medium flex text-[#6A6A6A] mb-1 mt-3'>Bid</div>
-          <div className='font-bold w-full'>
-            <PriceInput
-              key={'BidInput'}
-              offer={true}
-              currencyAddress={getAddress('weth', defaultChainId)}
-              currencyOptions={['ETH', 'WETH']}
-              onPriceChange={(val: BigNumber) => {
-                console.log('val', val);
-              }}
-              onCurrencyChange={(currency: SupportedCurrency) => {
-                console.log('currency', currency);
-              }}
-              error={false}
-            />
-          </div>
+          {nft ?
+            <div className='font-bold w-full'>
+              <PriceInput
+                key={'BidInput'}
+                offer={true}
+                currencyAddress={getAddress('weth', defaultChainId)}
+                currencyOptions={['ETH', 'WETH']}
+                onPriceChange={(val: BigNumber) => {
+                  console.log('val', val);
+                }}
+                onCurrencyChange={(currency: SupportedCurrency) => {
+                  console.log('currency', currency);
+                }}
+                error={false}
+              />
+            </div> :
+            <div className="animate-pulse flex h-5 my-2 w-full space-x-4">
+              <div className="h-5 w-full bg-slate-200 rounded" />
+            </div>
+          }
 
           <div className='flex items-center justify-between w-full'>
             <div className='text-[16px] flex text-[#B2B2B2] mb-1 mt-3'>Your Balance</div>
@@ -204,15 +209,20 @@ export function OfferCheckout() {
           </div>
 
           <div className='w-full text-[16px] font-medium flex text-[#6A6A6A] mb-1 mt-5'>Set Bid Expiration</div>
-          <div className='mb-2 rounded-md h-12 w-full font-bold'>
-            <DropdownPicker
-              v2
-              offer={true}
-              options={expirationOptions}
-              selectedIndex={selectedExpirationOption}
-              placeholder={'Select Expiration'}
-            />
-          </div>
+          {nft ?
+            <div className='mb-2 rounded-md h-12 w-full font-bold'>
+              <DropdownPicker
+                v2
+                offer={true}
+                options={expirationOptions}
+                selectedIndex={selectedExpirationOption}
+                placeholder={'Select Expiration'}
+              />
+            </div> :
+            <div className="animate-pulse flex h-5 my-2 w-full space-x-4">
+              <div className="h-5 w-full bg-slate-200 rounded" />
+            </div>
+          }
 
           <div className='border-b-[1px] border-dashed border-[#ECECEC] w-full my-3' />
 
