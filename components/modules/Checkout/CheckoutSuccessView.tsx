d211074fc1b87535666735b1d0a857d250af73e6
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-01-23 10:13:44 -0400

    fix: update styling

--- components/modules/Checkout/CheckoutSuccessView.tsx
+++ components/modules/Checkout/CheckoutSuccessView.tsx
@@ -114,7 +114,7 @@ export function CheckoutSuccessView(props: CheckoutSuccessViewProps) {
         </div>
       </div>
     </div> :
-    <div className="flex flex-col items-center h-[596px] md:h-screen font-noi-grotesk">
+    <div className="flex flex-col items-center h-[646px] md:h-screen font-noi-grotesk">
       <div className="relative z-10 bg-gradient-to-br from-[#FAC213] to-[#FF9B37] w-full h-2/5 top-0">
         <div className='w-full flex items-center justify-center'>
           <DesktopNoProfile className="h-full w-full shrink-0 object-cover" layout="fill" />
