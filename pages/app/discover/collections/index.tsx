5cc74bc3a94027dfbb647e452152e0a87bb97082
dimamankov <dimamankov@gmail.com>
2023-01-26 23:54:35 +0100

    Remove gates variables (#929)
    remove gate variables
    
    Co-authored-by: gmaijoe <gmaijoe@users.noreply.github.com>

--- pages/app/discover/collections/index.tsx
+++ pages/app/discover/collections/index.tsx
@@ -8,8 +8,6 @@ import { SideNav } from 'components/modules/Search/SideNav';
 import { useCollectionQueryLeaderBoard } from 'graphql/hooks/useCollectionLeaderBoardQuery';
 import { useFetchTypesenseSearch } from 'graphql/hooks/useFetchTypesenseSearch';
 import { useSearchModal } from 'hooks/state/useSearchModal';
-import NotFoundPage from 'pages/404';
-import { Doppler, getEnvBool } from 'utils/env';
 import { tw } from 'utils/tw';
 
 import { SlidersHorizontal, X } from 'phosphor-react';
@@ -25,9 +23,6 @@ function usePrevious(value) {
 }
 
 export default function CollectionsPage() {
-  const newFiltersEnabled = getEnvBool(Doppler.NEXT_PUBLIC_DISCOVER2_PHASE3_ENABLED);
-  const newFiltersEnabledNew = getEnvBool(Doppler.NEXT_PUBLIC_DISCOVER2_PHASE4_ENABLED);
-
   const [page, setPage] = useState(1);
   const { sideNavOpen, activePeriod, setSideNavOpen, collectionsResultsFilterBy, isLeaderBoard, toggleLeaderBoardState, changeTimePeriod, setSearchModalOpen, setClearedFilters } = useSearchModal();
   const { data: collectionData } = useCollectionQueryLeaderBoard(activePeriod);
@@ -120,126 +115,122 @@ export default function CollectionsPage() {
       );
     }
   };
-  if (!newFiltersEnabled) {
-    return <NotFoundPage />;
-  }else{
-    return(
-      <>
-        <div className="p-2 minmd:p-4 minlg:p-8 minhd:p-16 minmd:m-0 mb-10 minlg:mb-10 minlg:mt-20 minmd:max-w-full self-center minmd:self-stretch minxl:mx-auto min-h-screen ">
-          <div className="flex">
-            <div className=" w-full min-h-disc">
-              <div>
-                <div className='flex justify-between mb-10'>
-                  <div className='flex justify-between items-center'>
-                    {
-                      !isLeaderBoard && (
-                        <div className='flex items-center'>
-                          <div
-                            className={`hidden minlg:block max-w-[112px] overflow-hidden cursor-pointer ${sideNavOpen ? 'mr-[206px]' : 'mr-4'}`}
-                            onClick={() => setSideNavOpen(!sideNavOpen)}>
-                            {sideNavOpen ?
-                              <div className="flex items-center justify-center bg-[#F2F2F2] text-[#6A6A6A] py-3 px-5 text-lg rounded-[48px]">
-                                Filters
-                                <X size={22} className="text-[#6A6A6A] ml-2" />
-                              </div> :
-                              <div className="flex items-center justify-center bg-black text-white py-3 px-5 text-lg rounded-[48px]">
-                                <SlidersHorizontal size={22} className="mr-2"/>
-                                <p>Filter</p>
-                              </div>
-                            }
-                          </div>
-                          <div className="px-0 flex mt-0 mr-4 justify-between minlg:hidden">
-                            <div onClick={() => setSearchModalOpen(true, 'filters', filters )} className={'flex items-center justify-center bg-black text-white w-10 h-10 rounded-[50%] text-lg rounded-[48px] cursor-pointer'}>
-                              <SlidersHorizontal size={22}/>
+  return(
+    <>
+      <div className="p-2 minmd:p-4 minlg:p-8 minhd:p-16 minmd:m-0 mb-10 minlg:mb-10 minlg:mt-20 minmd:max-w-full self-center minmd:self-stretch minxl:mx-auto min-h-screen ">
+        <div className="flex">
+          <div className=" w-full min-h-disc">
+            <div>
+              <div className='flex justify-between mb-10'>
+                <div className='flex justify-between items-center'>
+                  {
+                    !isLeaderBoard && (
+                      <div className='flex items-center'>
+                        <div
+                          className={`hidden minlg:block max-w-[112px] overflow-hidden cursor-pointer ${sideNavOpen ? 'mr-[206px]' : 'mr-4'}`}
+                          onClick={() => setSideNavOpen(!sideNavOpen)}>
+                          {sideNavOpen ?
+                            <div className="flex items-center justify-center bg-[#F2F2F2] text-[#6A6A6A] py-3 px-5 text-lg rounded-[48px]">
+                              Filters
+                              <X size={22} className="text-[#6A6A6A] ml-2" />
+                            </div> :
+                            <div className="flex items-center justify-center bg-black text-white py-3 px-5 text-lg rounded-[48px]">
+                              <SlidersHorizontal size={22} className="mr-2"/>
+                              <p>Filter</p>
                             </div>
-                            <div className={'hidden relative flex items-center justify-center bg-white border-[#ECECEC] border-[1px] text-white w-10 h-10 rounded-[50%] text-lg rounded-[48px] cursor-pointer z-5'}>
+                          }
+                        </div>
+                        <div className="px-0 flex mt-0 mr-4 justify-between minlg:hidden">
+                          <div onClick={() => setSearchModalOpen(true, 'filters', filters )} className={'flex items-center justify-center bg-black text-white w-10 h-10 rounded-[50%] text-lg rounded-[48px] cursor-pointer'}>
+                            <SlidersHorizontal size={22}/>
+                          </div>
+                          <div className={'hidden relative flex items-center justify-center bg-white border-[#ECECEC] border-[1px] text-white w-10 h-10 rounded-[50%] text-lg rounded-[48px] cursor-pointer z-5'}>
 
-                            </div>
                           </div>
+                        </div>
+                      </div>
+                    )
+                  }
+                  <div className="flex flex-col minmd:flex-row minmd:items-center">
+                    {isLeaderBoard && <span className="text-[1.75rem] font-[500] mr-10">Leaderboard</span>}
+                    <button onClick={() => toggleLeaderBoardState(!isLeaderBoard)} className={`${isLeaderBoard ? 'text-[#6A6A6A]' : 'text-[#000]'} flex items-center underline`}>
+                      {!isLeaderBoard ? <LeaderBoardIcon className="mr-2"/> : null}
+                      {!isLeaderBoard ? 'Show leaderboard' : 'View Collections' }
+                    </button>
+                  </div>
+                </div>
+                <div className='flex minmd:hidden'>
+                  {
+                    isLeaderBoard && (
+                      <TimePeriodToggle
+                        onChange={(val) => changeTimePeriod(val)}
+                        activePeriod={activePeriod}/>
+                    )
+                  }
+
+                </div>
+              </div>
+              <div className="flex">
+                <div className="flex-auto">
+                  {
+                    isLeaderBoard
+                      ? (
+                        <div className="hidden minlg:flex px-6 text-sm text-[#B2B2B2] leading-6 font-[600] font-noi-grotesk">
+                          <div className="w-[35%] flex items-center  pl-[32px]">COLLECTION</div>
+                          <div className="w-[15%] flex items-center justify-center">VOLUME</div>
+                          <div className="w-[15%] flex items-center justify-center">% CHANGE</div>
+                          <div className="w-[15%] flex items-center justify-center">FLOOR PRICE</div>
+                          <div className="w-[10%] flex items-center justify-center">ITEMS</div>
+                          <div className="w-[10%] flex items-center justify-center">SALES</div>
+                        </div>
+                      )
+                      : null
+                  }
+                  <div className={!isLeaderBoard ? 'flex flex-row justify-between items-start' : ''}>
+                    {
+                      !isLeaderBoard && sideNavOpen && (
+                        <div className='hidden minlg:block'>
+                          <SideNav onSideNav={() => null} filtersData={filters}/>
                         </div>
                       )
                     }
-                    <div className="flex flex-col minmd:flex-row minmd:items-center">
-                      {isLeaderBoard && <span className="text-[1.75rem] font-[500] mr-10">Leaderboard</span>}
-                      <button onClick={() => toggleLeaderBoardState(!isLeaderBoard)} className={`${isLeaderBoard ? 'text-[#6A6A6A]' : 'text-[#000]'} flex items-center underline`}>
-                        {!isLeaderBoard ? <LeaderBoardIcon className="mr-2"/> : null}
-                        {!isLeaderBoard ? 'Show leaderboard' : 'View Collections' }
-                      </button>
-                    </div>
-                  </div>
-                  <div className='flex minmd:hidden'>
                     {
-                      isLeaderBoard && (
-                        <TimePeriodToggle
-                          onChange={(val) => changeTimePeriod(val)}
-                          activePeriod={activePeriod}/>
-                      )
-                    }
-
-                  </div>
-                </div>
-                <div className="flex">
-                  <div className="flex-auto">
-                    {
-                      isLeaderBoard
+                      !loading && (collections?.length === 0)
                         ? (
-                          <div className="hidden minlg:flex px-6 text-sm text-[#B2B2B2] leading-6 font-[600] font-noi-grotesk">
-                            <div className="w-[35%] flex items-center  pl-[32px]">COLLECTION</div>
-                            <div className="w-[15%] flex items-center justify-center">VOLUME</div>
-                            <div className="w-[15%] flex items-center justify-center">% CHANGE</div>
-                            <div className="w-[15%] flex items-center justify-center">FLOOR PRICE</div>
-                            <div className="w-[10%] flex items-center justify-center">ITEMS</div>
-                            <div className="w-[10%] flex items-center justify-center">SALES</div>
+                          <div className='grid-cols-1 w-full'>
+                            <NoActivityIcon className='m-auto mt-10' />
+                            <div className="md:text-[20px] text-[24px] font-semibold font-noi-grotesk mb-2 flex items-center justify-center mt-5 text-[#4D4D4D]">No Results Found</div>
                           </div>
                         )
                         : null
                     }
-                    <div className={!isLeaderBoard ? 'flex flex-row justify-between items-start' : ''}>
-                      {
-                        !isLeaderBoard && sideNavOpen && (
-                          <div className='hidden minlg:block'>
-                            <SideNav onSideNav={() => null} filtersData={filters}/>
-                          </div>
-                        )
-                      }
-                      {
-                        newFiltersEnabledNew && !loading && (collections?.length === 0)
-                          ? (
-                            <div className='grid-cols-1 w-full'>
-                              <NoActivityIcon className='m-auto mt-10' />
-                              <div className="md:text-[20px] text-[24px] font-semibold font-noi-grotesk mb-2 flex items-center justify-center mt-5 text-[#4D4D4D]">No Results Found</div>
-                            </div>
-                          )
-                          : null
-                      }
-                      {collections && collections?.length > 0 && leaderBoardOrCollectionView()}
-                    </div>
-                    {(loading) &&
-                      (<div className="flex items-center justify-center min-h-[16rem] w-full">
-                        <Loader />
-                      </div>)}
-                    { !isLeaderBoard && collections && collections.length < found && collections?.length > 0 &&
-                      <div className="mx-auto w-full minxl:w-1/4 flex justify-center mt-7 font-medium">
-                        <Button
-                          color={'black'}
-                          accent={AccentType.SCALE}
-                          stretch={true}
-                          label={'Load More'}
-                          onClick={() => {
-                            setPage(page + 1);
-                          }}
-                          type={ButtonType.PRIMARY}
-                        />
-                      </div>}
+                    {collections && collections?.length > 0 && leaderBoardOrCollectionView()}
                   </div>
+                  {(loading) &&
+                    (<div className="flex items-center justify-center min-h-[16rem] w-full">
+                      <Loader />
+                    </div>)}
+                  { !isLeaderBoard && collections && collections.length < found && collections?.length > 0 &&
+                    <div className="mx-auto w-full minxl:w-1/4 flex justify-center mt-7 font-medium">
+                      <Button
+                        color={'black'}
+                        accent={AccentType.SCALE}
+                        stretch={true}
+                        label={'Load More'}
+                        onClick={() => {
+                          setPage(page + 1);
+                        }}
+                        type={ButtonType.PRIMARY}
+                      />
+                    </div>}
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
 CollectionsPage.getLayout = function getLayout(page) {
