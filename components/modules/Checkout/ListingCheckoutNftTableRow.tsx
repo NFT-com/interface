ff7ea97d4f6688b6e13418ee324764a0eb801cc0
fernandoGalarcio <fernando.galarcio.m@gmail.com>
2023-01-26 18:10:55 -0500

    adjustment

--- components/modules/Checkout/ListingCheckoutNftTableRow.tsx
+++ components/modules/Checkout/ListingCheckoutNftTableRow.tsx
@@ -385,7 +385,7 @@ export function ListingCheckoutNftTableRow(props: ListingCheckoutNftTableRowProp
         {seaportEnabled && /*(selectedOptionDropdown0.current !== ExternalProtocol.Seaport && selectedOptionDropdown0.current !== 'Opensea') && */
           <div className='w-full flex flex-col minlg:flex-row border-b border-[#A6A6A6] minlg:border-0 pb-3 minlg:pb-0 mb-3 minlg:mb-0'>
             <div className='minlg:hidden w-full text-base font-normal flex text-[#A6A6A6] mb-3'>Marketplace</div>
-            <div className='mb-2 rounded-md h-12 md:w-full minlg:min-w-[140px] minglg:max-w-[140px]'>
+            <div className='mb-2 rounded-md h-12 md:w-full minlg:w-[22%]'>
               <DropdownPicker
                 options={generateMarketPlaceOptions(0, true)}
                 selectedIndex={0}
@@ -434,7 +434,7 @@ export function ListingCheckoutNftTableRow(props: ListingCheckoutNftTableRowProp
         {looksrareEnabled && /* && selectedOptionDropdown0.current !== ExternalProtocol.LooksRare &&*/
             <div className='w-full flex flex-col minlg:flex-row border-b border-[#A6A6A6] minlg:border-0 pb-3 minlg:pb-0 mb-3 minlg:mb-0'>
               <div className='minlg:hidden w-full text-base font-normal flex text-[#A6A6A6] mb-3'>Marketplace</div>
-              <div className='mb-2 rounded-md h-12 md:w-full minlg:min-w-[140px] minglg:max-w-[140px]'>
+              <div className='mb-2 rounded-md h-12 md:w-full minlg:w-[22%]'>
                 <DropdownPicker
                   options={generateMarketPlaceOptions(1, true)}
                   selectedIndex={1}
@@ -498,7 +498,7 @@ export function ListingCheckoutNftTableRow(props: ListingCheckoutNftTableRowProp
         {X2Y2Enabled && /* selectedOptionDropdown0.current !== ExternalProtocol.X2Y2 && */
           <div className='w-full flex flex-col minlg:flex-row border-b border-[#A6A6A6] minlg:border-0 pb-3 minlg:pb-0 mb-3 minlg:mb-0'>
             <div className='minlg:hidden w-full text-base font-normal flex text-[#A6A6A6] mb-3'>Marketplace</div>
-            <div className='mb-2 rounded-md h-12 md:w-full minlg:min-w-[140px] minglg:max-w-[140px]'>
+            <div className='mb-2 rounded-md h-12 md:w-full minlg:w-[22%]'>
               <DropdownPicker
                 options={generateMarketPlaceOptions(2, true)}
                 selectedIndex={2}
@@ -562,7 +562,7 @@ export function ListingCheckoutNftTableRow(props: ListingCheckoutNftTableRowProp
         {getEnvBool(Doppler.NEXT_PUBLIC_NATIVE_TRADING_TEST) && NFTCOMEnabled && /*(selectedOptionDropdown0.current !== ExternalProtocol.Seaport && selectedOptionDropdown0.current !== 'Opensea') && */
           <div className='w-full flex flex-col minlg:flex-row border-b border-[#A6A6A6] minlg:border-0 pb-3 minlg:pb-0 mb-3 minlg:mb-0'>
             <div className='minlg:hidden w-full text-base font-normal flex text-[#A6A6A6] mb-3'>Marketplace</div>
-            <div className='mb-2 rounded-md h-12 md:w-full minlg:min-w-[140px] minglg:max-w-[140px]'>
+            <div className='mb-2 rounded-md h-12 md:w-full minlg:w-[22%]'>
               <DropdownPicker
                 options={generateMarketPlaceOptions(3, true)}
                 selectedIndex={3}
