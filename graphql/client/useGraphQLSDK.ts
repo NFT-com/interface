b3763784d03f2b059eacfb228daa8438430cf70f
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-02-01 11:33:54 -0400

    fix: use callback

--- graphql/client/useGraphQLSDK.ts
+++ graphql/client/useGraphQLSDK.ts
@@ -1,12 +1,12 @@
 import { GraphQLContext } from 'graphql/client/GraphQLProvider';
 import { getSdk, Sdk } from 'graphql/generated/types';
 
-import { useContext, useEffect, useState } from 'react';
+import { useCallback, useContext, useState } from 'react';
 
 export function useGraphQLSDK(): Sdk {
   const { client } = useContext(GraphQLContext);
   const [sdk, setSdk] = useState(getSdk(client));
-  useEffect(() => {
+  useCallback(() => {
     setSdk(getSdk(client));
   }, [client]);
   return sdk;
