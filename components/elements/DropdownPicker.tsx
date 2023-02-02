6abb30b84cdc760348dbf6f2112fb649a8f43aae
fernandoGalarcio <fdowannabe@gmail.com>
2023-02-02 16:11:08 -0500

    adjustment

--- components/elements/DropdownPicker.tsx
+++ components/elements/DropdownPicker.tsx
@@ -3,7 +3,7 @@ import { tw } from 'utils/tw';
 
 import Image from 'next/image';
 import KeyIcon from 'public/mint-key.svg';
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { ReactElement, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { ChevronDown, ChevronUp } from 'react-feather';
 import { useThemeColors } from 'styles/theme/useThemeColors';
 
@@ -22,7 +22,7 @@ export interface DropdownPickerProps {
   selectedIndex?: number;
   constrain?: boolean;
   above?: boolean;
-  placeholder?: string | Element;
+  placeholder?: string | ReactElement;
   onChange?: (label: string) => void;
   showKeyIcon?: boolean;
   v2?: boolean;
