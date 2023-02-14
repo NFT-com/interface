d130dd140e05257bb16644c7acd7a8f0445c638e
gmaijoe <gmaijoe@users.noreply.github.com>
2023-02-14 15:42:18 -0400

    patch sentry bug related to absolute urls (#1025)
    fix: fix sentry

--- pages/api/imageFetcher.ts
+++ pages/api/imageFetcher.ts
@@ -4,16 +4,25 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'node-fetch';
 
 const imageFetcher = async (req: NextApiRequest, res: NextApiResponse) => {
-  const r = await axios.get(`https://5hi24d3w2gny6zrfhekqk6mv4e0cfois.lambda-url.us-east-1.on.aws?gcp=${req.query.gcp}&url=${encodeURIComponent(`${req.query.url}`)}&width=${Number(req.query.width) || 1000}&height=${Number(req.query.height) || 1000}`);
-  const optimizedUrl = r.data.data;
-  const result = await fetch(optimizedUrl);
-  if (result.statusText == 'Unprocessable Entity' || !process.env.IMAGE_PROXY_ENABLED) {
-    const originalResult = await fetch(decodeURIComponent(`${req.query.url}`));
+  const fallBackImage = 'https://cdn.nft.com/optimizedLoader2.webp';
+  try {
+    if (!req.query.url) throw new Error('no url provided');
+
+    const r = await axios.get(`https://5hi24d3w2gny6zrfhekqk6mv4e0cfois.lambda-url.us-east-1.on.aws?gcp=${req.query.gcp}&url=${encodeURIComponent(`${req.query.url}`)}&width=${Number(req.query.width) || 1000}&height=${Number(req.query.height) || 1000}`);
+    const optimizedUrl = r?.data?.data || fallBackImage;
+    const result = await fetch(optimizedUrl);
+    if (result.statusText == 'Unprocessable Entity' || !process.env.IMAGE_PROXY_ENABLED) {
+      const originalResult = await fetch(decodeURIComponent(`${req.query.url}`));
+      const originalBody = await originalResult.body;
+      originalBody.pipe(res);
+    } else {
+      const body = await result.body;
+      body.pipe(res);
+    }
+  } catch (err) {
+    const originalResult = await fetch(fallBackImage);
     const originalBody = await originalResult.body;
     originalBody.pipe(res);
-  } else {
-    const body = await result.body;
-    body.pipe(res);
   }
 };
 
