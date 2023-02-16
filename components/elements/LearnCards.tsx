d1361a7fb4bcaf6a3e596e0883b7492a404f76e4
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-02-16 11:59:20 -0400

    fix: remove gcp img env var

--- components/elements/LearnCards.tsx
+++ components/elements/LearnCards.tsx
@@ -41,7 +41,7 @@ export const LearnCards = (props: LearnCardsProps) => {
             )}
             onClick={() => router.push(card['linkTo'])}
             style={{
-              background: `url("${getBaseUrl('https://www.nft.com/')}api/imageFetcher?gcp=${getEnvBool(Doppler.NEXT_PUBLIC_GCP_IMG_PROXY_ENABLED)}&url=${encodeURIComponent(props.cardImages[index].url)}&height=${1084}&width=${1084}")`,
+              background: `url("${getBaseUrl('https://www.nft.com/')}api/imageFetcher?gcp=false&url=${encodeURIComponent(props.cardImages[index].url)}&height=${1084}&width=${1084}")`,
               backgroundPosition: 'center center',
               backgroundSize: 'cover',
               backgroundRepeat: 'no-repeat',
