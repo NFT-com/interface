263e0fb1baaa4755d067adbb5ab6d335d6e3d7c2
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-03-02 13:22:54 -0400

    fix: ravi internal

--- pages/api/message.ts
+++ pages/api/message.ts
@@ -2,8 +2,21 @@ import { withSentry } from '@sentry/nextjs';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'node-fetch';
 
+const INTERNAL_TESTING = [
+  '0x9b3bA8B2a6E570067927B74dED7c62717Dc5cAB6',
+  '0x0a383703969a6121A881b1a51CD421B72C881861'
+];
+
 const message = async (req: NextApiRequest, res: NextApiResponse) => {
   const url = 'https://slack.com/api/chat.postMessage';
+
+  for (let i = 0; i < INTERNAL_TESTING.length; i++) {
+    if ((req?.query?.text as string)?.toLowerCase().includes(INTERNAL_TESTING[i].toLowerCase())) {
+      res.status(200).json('ok');
+      return;
+    }
+  }
+  
   try {
     // only in prod
     fetch(url, {
