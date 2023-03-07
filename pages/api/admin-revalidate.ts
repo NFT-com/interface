57eb0fc8eadc47fde1dbba0835bddd4134a60c54
Gavin Mai <gavinmai@alumni.stanford.edu>
2023-03-07 17:45:33 -0400

    fix: fix admin revalidate

--- pages/api/admin-revalidate.ts
+++ pages/api/admin-revalidate.ts
@@ -3,13 +3,13 @@ export default async function handler(req, res) {
     await res.revalidate('/articles');
     await res.revalidate('/');
     
-    const postSlug = req.body.query.slug['en-US'];
+    const postSlug = req.query?.slug?.['en-US'];
     if (!postSlug) await res.validate(`/articles/${postSlug}`);
   
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    return res.status(500).send('Error revalidating');
+    return res.status(500).send(`Error revalidating, ${JSON.stringify(err)}`);
   }
 }
