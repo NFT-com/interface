f68f290cdd2aa329e76e812d93cc1bf871a5a771
fernandoGalarcio <fdowannabe@gmail.com>
2023-03-08 01:44:21 -0500

    Use different coundtdown

--- components/modules/NFTDetail/Countdown.tsx
+++ components/modules/NFTDetail/Countdown.tsx
@@ -1,36 +1,37 @@
 
-import moment from 'moment';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export interface CountdownProps {
   eventTime: number;
-  interval: number;
 }
 
-const calculateDuration = eventTime => moment.duration(Math.max(eventTime - (Math.floor(Date.now() / 1000)), 0), 'seconds');
+function Countdown({ eventTime }: CountdownProps) {
+  const countDownDate = new Date(eventTime*1000).getTime();
+  const [countDown, setCountDown] = useState(
+    countDownDate - new Date().getTime()
+  );
 
-function Countdown({ eventTime, interval }: CountdownProps) {
-  const [duration, setDuration] = useState(calculateDuration(eventTime));
-  const timerCallback = useCallback(() => {
-    setDuration(calculateDuration(eventTime));
-  }, [eventTime]);
-  
   useEffect(() => {
-    const timer = setInterval(() => {
-      setDuration(calculateDuration(eventTime));
+    const interval = setInterval(() => {
+      setCountDown(countDownDate - new Date().getTime());
     }, 1000);
-  
-    return () => clearInterval(timer);
-  }, [eventTime, interval, timerCallback]);
+
+    return () => clearInterval(interval);
+  }, [countDownDate]);
+
+  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
   
   return (
     <div>
       <p className='mb-1 text-[#B2B2B2]'>Ends in</p>
       <p className='font-noi-grotesk sm:text-xl text-2xl font-medium leading-6'>
-        {duration.days()}<span className='text-[15px] text-[#6A6A6A] mr-[10px]'>d</span>
-        {duration.hours()}<span className='text-[15px] text-[#6A6A6A] mr-[10px]'>h</span>
-        {duration.minutes()}<span className='text-[15px] text-[#6A6A6A] mr-[10px]'>m</span>
-        {duration.seconds()}<span className='text-[15px] text-[#6A6A6A]'>s</span>
+        {days}<span className='text-[15px] text-[#6A6A6A] mr-[10px]'>d</span>
+        {hours}<span className='text-[15px] text-[#6A6A6A] mr-[10px]'>h</span>
+        {minutes}<span className='text-[15px] text-[#6A6A6A] mr-[10px]'>m</span>
+        {seconds}<span className='text-[15px] text-[#6A6A6A]'>s</span>
       </p>
     </div>
   );
