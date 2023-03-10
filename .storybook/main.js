09294fa9206cdc9ca44655df405edd2e18e7e326
fernandoGalarcio <fdowannabe@gmail.com>
2023-03-10 14:27:35 -0500

    adjustment

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,7 +10,6 @@ module.exports = {
     "@storybook/addon-interactions",
     "@storybook/addon-postcss",
     "storybook-addon-next",
-    "storybook-addon-pseudo-states",
   ],
   framework: "@storybook/react",
   core: {
