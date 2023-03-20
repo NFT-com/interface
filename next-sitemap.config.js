a4db1d5b06594a30ebce976afad283f5a519a8b0
Ryo Lambert <ryo@immutableholdings.com>
2023-03-20 17:14:11 -0600

    NFT-1609 Dynamic Sitemap Generators (#1137)
    * [NFT-1609] NEW: Add dynamic sitemap generation.
    
    * NFT-1430 FIX: tweak robots.txt config
    
    * NFT-1609 FIX: Add updated gql types

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,15 +2,26 @@
 module.exports = {
   siteUrl: 'https://www.nft.com',
   generateRobotsTxt: true,
-  exclude: [
-
-  ],
+  sitemapSize: 5000, // Will support 250M urls 50K files w/5K urls
+  changefreq: 'daily', // Default
+  exclude: ['/server-sitemap-index.xml'],
   robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://www.nft.com/server-sitemap-index.xml',
+    ],
     policies: [
       {
         userAgent: '*',
         disallow: ['/*'],
-        allow: ['/$', '/articles', '/articles/*', '/*.xml'],
+        allow: [
+          '/$',
+          '/app/discover',
+          '/app/discover/*',
+          '/app/collection/*',
+          '/articles',
+          '/articles/*',
+          '/*.xml',
+        ],
       }
     ]
   },
