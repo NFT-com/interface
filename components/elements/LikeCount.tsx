c329d71f53d3531b2b7def35f36b4efaf29cfa63
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2023-03-21 15:52:41 -0700

    updated like count state, and added mutation

--- components/elements/LikeCount.tsx
+++ components/elements/LikeCount.tsx
@@ -2,33 +2,45 @@ import { tw } from 'utils/tw';
 
 import { Player } from '@lottiefiles/react-lottie-player';
 import Heart from 'public/heart.svg?svgr';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type LikeCountProps = {
-  count?: number;
-  isLiked?: boolean;
-  onClick: () => void;
+  count: number;
+  isLiked: boolean;
+  onClick: any;
+  mutate: () => void;
 }
 
-export default function LikeCount({ count = 0, isLiked = false, onClick }: LikeCountProps) {
+export default function LikeCount({ count, isLiked, onClick, mutate }: LikeCountProps) {
   const [clicked, setClicked] = useState(false);
-  const [liked, setLiked] = useState(isLiked);
+  const [liked, setLiked] = useState(null);
+  const [likeCount, setLikeCount] = useState(null);
+
+  useEffect(() => {
+    setLiked(isLiked);
+    setLikeCount(count);
+  }, [count, isLiked]);
+
+  function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
   return (
     <div
-      onClick={(e) => {
+      onClick={async(e) => {
         e.preventDefault();
         onClick && onClick();
         if(!liked) {
           setClicked(true);
-          //mutate
-          setTimeout(() => {
-            setLiked(true);
-            setClicked(false);
-          }, 700);
+          setLikeCount(likeCount + 1);
+          await timeout(700);
+          setLiked(true);
+          setClicked(false);
+          mutate();
         } else {
+          mutate();
           setLiked(false);
-          //mutate
+          setLikeCount(likeCount - 1);
         }
       }
       }
@@ -59,7 +71,7 @@ export default function LikeCount({ count = 0, isLiked = false, onClick }: LikeC
           </div>
         }
         
-        {count > 0 &&
+        {likeCount > 0 &&
             <p className={tw(
               'font-noi-grotesk font-medium tracking-normal',
               liked ? 'text-alert-red-light' : 'text-key-bg',
@@ -68,7 +80,7 @@ export default function LikeCount({ count = 0, isLiked = false, onClick }: LikeC
                 new Intl.NumberFormat('en-US', {
                   notation: 'compact',
                   compactDisplay: 'short'
-                }).format(count)
+                }).format(likeCount)
               }
             </p>
         }
