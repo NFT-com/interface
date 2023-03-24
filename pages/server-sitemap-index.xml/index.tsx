0e3c9649bdb37ef7fc45e844df172492ec5ff740
Ryo Lambert <ryo@immutableholdings.com>
2023-03-23 20:46:44 -0600

    fix: add urls back

--- pages/server-sitemap-index.xml/index.tsx
+++ pages/server-sitemap-index.xml/index.tsx
@@ -7,26 +7,13 @@ import { siteUrl } from 'next-sitemap.config';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   // Setup variables
-  const res = await fetch(`https://${ctx.req.headers.host}/api/sitemaps/server-sitemap-index?teamKey=${teamAuthToken}`, {
+  const { sitemapUrls } = await fetch(`https://${ctx.req.headers.host}/api/sitemaps/server-sitemap-index?teamKey=${teamAuthToken}`, {
     headers: {
       'Cache-Control': 's-maxage=86340, stale-while-revalidate'
     }
-  });
-  console.log('%c Line:11 🧀 sitemapUrls', 'color:#6ec1c2', {
-    url: `${siteUrl}/api/sitemaps/server-sitemap-index?teamKey=${teamAuthToken}`,
-    headers: res.headers,
+  }).then(res => res.json());
 
-    res: JSON.stringify(res, null, 2),
-  });
-  try {
-    const results = await res.json();
-    console.trace('%c Line:14 🥤 results', 'color:#fca650', results);
-  } catch (e) {
-    console.trace(e);
-    console.error(JSON.stringify(e, null, 2));
-  }
-
-  return getServerSideSitemapIndexLegacy(ctx, []);
+  return getServerSideSitemapIndexLegacy(ctx, sitemapUrls || []);
 };
 
 // Default export to prevent next.js errors
