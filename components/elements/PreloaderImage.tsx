45baf52d5b95d77be8835a60310cf5e2faf50f81
Ryo Lambert <ryo@immutableholdings.com>
2023-03-28 21:42:29 -0600

    fix: patch extraneous errors & cleanup

--- components/elements/PreloaderImage.tsx
+++ components/elements/PreloaderImage.tsx
@@ -1,22 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
-import { genereteRandomPreloader, getStaticAsset } from 'utils/helpers';
+import { generateRandomPreloader, getStaticAsset } from 'utils/helpers';
 
 export default function PreloaderImage() {
   const getPreloader = () => {
-    const index = genereteRandomPreloader();
+    const index = generateRandomPreloader();
     switch (index) {
     case 0:
-      return <img src={getStaticAsset('public/preloaderImg-1.svg')} alt='public/preloaderImg-1.svg'/>;
+      return <img src={getStaticAsset('public/preloaderImg-1.svg')} alt='public/preloaderImg-1.svg' />;
     case 1:
-      return <img src={getStaticAsset('public/preloaderImg-2.svg')} alt='public/preloaderImg-2.svg'/>;
+      return <img src={getStaticAsset('public/preloaderImg-2.svg')} alt='public/preloaderImg-2.svg' />;
     case 2:
-      return <img src={getStaticAsset('public/preloaderImg-3.svg')} alt='public/preloaderImg-3.svg'/>;
+      return <img src={getStaticAsset('public/preloaderImg-3.svg')} alt='public/preloaderImg-3.svg' />;
     case 3:
-      return <img src={getStaticAsset('public/preloaderImg-4.svg')} alt='public/preloaderImg-4.svg'/>;
+      return <img src={getStaticAsset('public/preloaderImg-4.svg')} alt='public/preloaderImg-4.svg' />;
     case 4:
-      return <img src={getStaticAsset('public/preloaderImg-5.svg')} alt='public/preloaderImg-5.svg'/>;
-    default:
-      return 0;
+      return <img src={getStaticAsset('public/preloaderImg-5.svg')} alt='public/preloaderImg-5.svg' />;
+   default:
+     return 0;
     }
   };
 
