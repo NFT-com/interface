acec4b1395ac8cf73652dbbbae9a9fdf82038598
luc <50972198+robitaille1@users.noreply.github.com>
2023-03-28 17:00:46 -0700

    Update components/elements/Footer.tsx
    Co-authored-by: Ryo Lambert <ryo@immutableholdings.com>

--- components/elements/Footer.tsx
+++ components/elements/Footer.tsx
@@ -5,8 +5,12 @@ import { FooterSmall as StaticFooterSmall } from './FooterSmall';
 import dynamic from 'next/dynamic';
 import React from'react';
 
-const DynamicFooterLarge = dynamic<React.ComponentProps<typeof StaticFooterLarge>>(() => import('components/elements/FooterLarge').then(mod => mod.FooterLarge));
-const DynamicFooterSmall = dynamic<React.ComponentProps<typeof StaticFooterSmall>>(() => import('components/elements/FooterSmall').then(mod => mod.FooterSmall));
+const DynamicFooterLarge = dynamic<React.ComponentProps<typeof StaticFooterLarge>>(() => import('components/elements/FooterLarge').then(mod => mod.FooterLarge), {
+  loading: () => <footer className="h-20 bg-black">Loading...</footer>
+});
+const DynamicFooterSmall = dynamic<React.ComponentProps<typeof StaticFooterSmall>>(() => import('components/elements/FooterSmall').then(mod => mod.FooterSmall), {
+  loading: () => <footer className="h-20 bg-black">Loading...</footer>
+});
 
 type FooterProps = {
   isLarge?: boolean;
