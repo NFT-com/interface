ac5f0dec3957f0915993902754aaef3228f61732
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2023-04-03 15:27:26 -0700

    updated links

--- components/modules/Profile/LinksToSection.tsx
+++ components/modules/Profile/LinksToSection.tsx
@@ -39,8 +39,8 @@ export function LinksToSection(props: LinksToSectionParams) {
       image: '/link-to-green-key.png',
       section: 'GALLERY',
       title: 'NFT.COM Gallery',
-      description: 'Explore all Genesis Keys and existing NFT.com Profiles in our Gallery.',
-      linkTo: '/app/gallery'
+      description: 'Explore all our Genesis Keys.',
+      linkTo: '/app/collection/0x8fB5a7894AB461a59ACdfab8918335768e411414'
     },
     {
       image: '/link-to-nftcom-2.png',
@@ -70,8 +70,8 @@ export function LinksToSection(props: LinksToSectionParams) {
       image: '/link-to-green-key.png',
       section: 'GALLERY',
       title: 'NFT.COM Gallery',
-      description: 'Explore all Genesis Keys and existing NFT.com Profiles in our Gallery.',
-      linkTo: '/app/gallery'
+      description: 'Explore all our Genesis Keys.',
+      linkTo: '/app/collection/0x8fB5a7894AB461a59ACdfab8918335768e411414'
     },
     {
       image: '/link-to-nftcom-2.png',
