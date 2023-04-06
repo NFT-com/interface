908be5af81d60f575741eb2d5df06203d4ebc643
Ryo Lambert <ryo@immutableholdings.com>
2023-04-06 15:30:24 -0600

    feat: add CSP & x-frame-options headers

---
+++ config/headers.js
@@ -0,0 +1,62 @@
+
+// TODO: Need to get inventory of domains to allow (frame-ancestors newer ver of x-frame-options)
+const ContentSecurityPolicy = `
+  child-src 'self;
+  default-src 'self' *.nft.com;
+  frame-ancestors 'none';
+  script-src 'self' https://www.googletagmanager.com https://connect.facebook.net;
+  style-src 'self';
+  font-src 'self' https://fonts.googleapis.com;
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'no-referrer'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY' // blocks all iframes, switch to 'SAMEORIGIN' if issues arise
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=(), interest-cohort=()'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains'
+  },
+];
+
+const manifestHeaders = [
+  {
+    key: 'Access-Control-Allow-Origin',
+    value: '*'
+  },
+  {
+    key: 'Access-Control-Allow-Methods',
+    value: 'GET'
+  },
+  {
+    key: 'Access-Control-Allow-Headers',
+    value: 'X-Requested-With, content-type, Authorization'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin'
+  }
+];
+
+module.exports = { manifestHeaders, securityHeaders };
