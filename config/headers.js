d4c113fba54824fbcca1c93ebbf3008fee839dec
Ryo Lambert <ryo@immutableholdings.com>
2023-04-07 09:55:41 -0600

    feat: add CSP & x-frame-options headers (#1190)
    * feat: add CSP & x-frame-options headers
    
    * fix: format
    
    * fix: pull default & child CSP.
    
    * fix: add nonce values to partytown scripts
    
    ---------
    
    Co-authored-by: luc <50972198+robitaille1@users.noreply.github.com>

---
+++ config/headers.js
@@ -0,0 +1,59 @@
+// TODO: Need to get inventory of domains to allow (frame-ancestors newer ver of x-frame-options)
+const ContentSecurityPolicy = `
+  frame-ancestors 'none';
+  script-src 'self' https://www.googletagmanager.com https://connect.facebook.net nonce-pb+/pfhRedphzqIYzlBxMA== nonce-gGkqzVy6zqm4Aoyp9I4H5g== nonce-375Pd+0smY3JyJkGZJLKnA==;
+  style-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;
+  font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'no-referrer'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY' // blocks all iframes, switch to 'SAMEORIGIN' if issues arise
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=(), interest-cohort=()'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains'
+  },
+];
+
+const manifestHeaders = [
+  {
+    key: 'Access-Control-Allow-Origin',
+    value: '*'
+  },
+  {
+    key: 'Access-Control-Allow-Methods',
+    value: 'GET'
+  },
+  {
+    key: 'Access-Control-Allow-Headers',
+    value: 'X-Requested-With, content-type, Authorization'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin'
+  }
+];
+
+module.exports = { manifestHeaders, securityHeaders };
