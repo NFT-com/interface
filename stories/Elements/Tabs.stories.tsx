3e5974c47808aee77d8f270bff22f08c16e0d365
Lucas Robitaille <lucas.robitaille@immutableholdings.com>
2023-04-13 15:28:58 -0700

    updated tab stories

---
+++ stories/Elements/Tabs.stories.tsx
@@ -0,0 +1,63 @@
+import { Tabs } from 'components/elements/Tabs';
+import { tw } from 'utils/tw';
+
+import { Meta, StoryFn } from '@storybook/react';
+import React from 'react';
+
+export default {
+  title: 'elements/Tabs',
+  component: Tabs,
+  args: {
+    tabOptions: [
+      {
+        label: 'Tab 1',
+        content: <p>This is the content of Tab 1</p>,
+      },
+      {
+        label: 'Tab 2',
+        content: <p>This is the content of Tab 2</p>,
+      }
+    ]
+  },
+
+} as Meta<typeof Tabs>;
+
+const Template: StoryFn<typeof Tabs> = (args) => <Tabs {...args} />;
+
+export const Base = Template.bind({});
+
+export const WithLabelChild = Template.bind({});
+WithLabelChild.args = {
+  tabOptions: [
+    {
+      label: 'Tab 1',
+      content: <p>This is the content of Tab 1</p>,
+      labelChild: <span
+        className={tw(
+          'rounded-full h-5 w-5 flex items-center justify-center text-sm ml-2 justify-self-center',
+          'text-teal-400'
+        )}>
+          test
+      </span>
+    },
+    {
+      label: 'Tab 2',
+      content: <p>This is the content of Tab 2</p>,
+      labelChild: <span
+        className={tw(
+          'rounded-full h-5 w-5 flex items-center justify-center text-sm ml-2 justify-self-center',
+          'bg-red-400 text-white'
+        )}>
+          22
+      </span>
+    }
+  ]
+  
+};
+
+export const FullWidth = Template.bind({});
+
+export const ConstrainedWidth = Template.bind({});
+ConstrainedWidth.args = {
+  constrainTabs: true
+};
