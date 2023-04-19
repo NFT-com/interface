9acb24aaa1d7138e39bf685d07b23cf2a3ee654b
Oleksii Hlovliuk <aglovlyuk@gmail.com>
2023-04-20 01:50:53 +0300

    Update SocialSection Layout

--- components/modules/HomePage/SocialSection.tsx
+++ components/modules/HomePage/SocialSection.tsx
@@ -13,18 +13,24 @@ export default function SocialSection({ data }: HomePageData) {
   const router = useRouter();
 
   return(
-    <div className="pt-6 pb-16 minlg:pb-0 minmd:pt-28 minxl:pt-32 minxxl:pt-40">
+    <div className="pt-6 pb-16 minmd:pb-0 minmd:pt-12 minlg:pt-28">
       {
         data && data?.items.map((item, i) => {
           return (
-            <div key={i} className={`grid minmd:grid-cols-2 items-center minmd:-mt-20 minxl:-mt-24
-              relative z-${i}`}>
+            <div
+              key={i}
+              className={`
+              grid minmd:grid-cols-2 minmd:-mt-10 minlg:-mt-28
+              minxxl:max-w-[100rem] minxxl:mx-auto
+              relative z-${i}`}
+            >
               <div className={tw(
-                'my-14 minlg:my-0 px-5 minmd:px-0 minmd:ml-[14.7vw]',
-                'minmd:max-w-[25rem] minxxl:max-w-[29vw] minlg:pb-[9.6rem]'
+                'my-14 minlg:my-0 px-5 minmd:px-0 minmd:ml-[5vw] minlg:ml-[14.7vw]',
+                'minmd:max-w-[25rem] minlg:pb-[9.6rem] minmd:pt-6 minlg:pt-40',
+                item.leftImage ? 'minmd:ml-[10vw]' : ''
               )}>
                 <h2 data-aos="fade-up" data-aos-delay="100" className={tw(
-                  'text-[2.9rem] minmd:text-[3.75rem] minxxl:text-[5rem] leading-[1.25] minxl:leading-none font-normal',
+                  'text-[2.9rem] minmd:text-[3.5rem] minxxl:text-[5rem] leading-[1.25] minmd:leading-none font-normal',
                   'tracking-tight mb-6 minxxl:mb-9 -mr-2'
                 )}>
                   {
@@ -40,7 +46,7 @@ export default function SocialSection({ data }: HomePageData) {
                   data-aos-delay='300'
                   className={tw(
                     'mb-9',
-                    'text-[1rem] leading-[1.556] minlg:text-lg minlg:!leading-[1.3] minxxl:text-[2rem]'
+                    'text-[1rem] leading-[1.556] minlg:text-lg minxxl:text-2xl minlg:!leading-[1.3]'
                   )}
                 >
                   {item.subTitle}
@@ -58,10 +64,19 @@ export default function SocialSection({ data }: HomePageData) {
               </div>
 
               <div
-                className={item.leftImage ? 'minmd:-order-1 minlg:-mr-20' : 'minlg:-ml-14'}
+                className={tw(
+                  'relative',
+                  item.leftImage ? 'minmd:-order-1' : 'minlg:-ml-14'
+                )}
                 data-aos="fade-up"
                 data-aos-delay="400"
               >
+                {i == 1 && <div
+                  className={tw(
+                    'bg-[rgba(24,26,34,.3)] blur-2xl h-[60%]',
+                    'absolute left-[10%] right-[3%] top-1/2 -translate-y-1/2 mt-[10%]'
+                  )}></div>}
+
                 <BlurImage
                   width={700}
                   height={700}
