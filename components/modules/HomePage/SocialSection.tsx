1a5d0aa16a19679d4e768ffbd31848278f357d57
Ryo Lambert <ryo@immutableholdings.com>
2023-04-20 15:29:52 -0600

    fix: add drop shadow

--- components/modules/HomePage/SocialSection.tsx
+++ components/modules/HomePage/SocialSection.tsx
@@ -77,6 +77,7 @@ export default function SocialSection({ data }: HomePageData) {
                 <BlurImage
                   width={700}
                   height={700}
+                  className='drop-shadow-xl'
                   loader={contentfulLoader}
                   src={item.image.url}
                   alt='NFT image'
